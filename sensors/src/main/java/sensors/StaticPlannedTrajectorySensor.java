4ed75f21e5fd0bcfb31dddfe54c5c52cbeb874c7
Alexander Ryndin <alexander.ryndin@inform-software.com>
2018-01-16 18:12:27 +0100

    add trajectory xy data

---
+++ sensors/src/main/java/sensors/StaticPlannedTrajectorySensor.java
@@ -0,0 +1,42 @@
+package sensors;
+
+import commons.controller.commons.BusEntry;
+import commons.simulation.Sensor;
+import org.apache.commons.lang3.Validate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class StaticPlannedTrajectorySensor implements Sensor {
+
+    private final BusEntry type;
+    private final List<Double> trajectory;
+
+    public StaticPlannedTrajectorySensor(BusEntry type, List<Double> trajectory) {
+        this.type = Validate.notNull(type);
+        Validate.notNull(trajectory);
+        List<Double> defensiveCopy = new ArrayList<>(trajectory);
+        Validate.noNullElements(defensiveCopy);
+        this.trajectory = Collections.unmodifiableList(defensiveCopy);
+    }
+
+    @Override
+    public Object getValue() {
+        return trajectory;
+    }
+
+    @Override
+    public BusEntry getType() {
+        return type;
+    }
+
+    @Override
+    public String getTypeName() {
+        return Collections.<Double>emptyList().getClass().getTypeName();
+    }
+
+    @Override
+    public void update() {
+    }
+}
---
+++ sensors/src/main/java/sensors/StaticPlannedTrajectoryYSensor.java
@@ -0,0 +1,12 @@
+package sensors;
+
+import commons.controller.commons.BusEntry;
+
+import java.util.List;
+
+public class StaticPlannedTrajectoryYSensor extends StaticPlannedTrajectorySensor {
+
+    public StaticPlannedTrajectoryYSensor(List<Double> trajectoryY) {
+        super(BusEntry.PLANNED_TRAJECTORY_Y, trajectoryY);
+    }
+}
