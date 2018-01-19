3de2031c2e83fe6b32cfc0d28a6dd63d1587a089
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2018-01-19 17:37:28 +0000

    Added basic yaml support

--- src/main/java/de/monticore/lang/monticar/generator/roscpp/yamlHelper.java
+++ src/main/java/de/monticore/lang/monticar/generator/roscpp/yamlHelper.java
@@ -2,21 +2,65 @@ package de.monticore.lang.monticar.generator.roscpp;
 
 import de.monticar.lang.monticar.generator.python.RosInterface;
 import de.monticar.lang.monticar.generator.python.RosTag;
-import de.monticore.lang.montiarc.montiarc._symboltable.ExpandedComponentInstanceSymbol;
-import de.monticore.lang.montiarc.montiarc._symboltable.PortSymbol;
+import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.ExpandedComponentInstanceSymbol;
+import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.PortSymbol;
 import de.monticore.symboltable.Scope;
 
+import java.util.HashMap;
+
+//TODO: better exceptions
 public class yamlHelper {
 
+    private static HashMap<String, RosTopic> uniqueTopics = new HashMap<>();
+
     public static void rosTagToDataHelper(Scope symtab, RosTag rosTag) {
-        ExpandedComponentInstanceSymbol componentInstanceSymbol = symtab.<ExpandedComponentInstanceSymbol>resolve(rosTag.component, ExpandedComponentInstanceSymbol.KIND).orElse(null);
+        uniqueTopics.clear();
+        ExpandedComponentInstanceSymbol componentInstanceSymbol = symtab.<ExpandedComponentInstanceSymbol>resolve(rosTag.component, ExpandedComponentInstanceSymbol.KIND)
+                .orElseThrow(() -> new RuntimeException("Component " + rosTag.component + " could not be found!"));
 
         for (RosInterface sub : rosTag.subscriber) {
             for (String portName : sub.ports.keySet()) {
-                PortSymbol currentPort = componentInstanceSymbol.getPort(portName).orElse(null);
+                PortSymbol currentPort = componentInstanceSymbol.getPort(portName)
+                        .orElseThrow(() -> new RuntimeException("Port " + componentInstanceSymbol.getName() + "." + portName + " not found"));
 
+                String msgField = sub.ports.get(portName);
+                if (currentPort.isOutgoing())
+                    throw new RuntimeException("Only incoming ports can be used for subscribers but " + currentPort.getName() + " is outgoing!");
+                DataHelper.addPortToTopic(currentPort, getRosTopicFromRosInterface(sub), msgField);
             }
         }
+
+        for (RosInterface pub : rosTag.publisher) {
+            for (String portName : pub.ports.keySet()) {
+                PortSymbol currentPort = componentInstanceSymbol.getPort(portName)
+                        .orElseThrow(() -> new RuntimeException("Port " + componentInstanceSymbol.getName() + "." + portName + " not found"));
+                String msgField = pub.ports.get(portName);
+                if (currentPort.isIncoming())
+                    throw new RuntimeException("Only outgoing ports can be used for publishers but " + currentPort.getName() + " is incoming!");
+                DataHelper.addPortToTopic(currentPort, getRosTopicFromRosInterface(pub), msgField);
+            }
+        }
+
+    }
+
+    private static RosTopic getRosTopicFromRosInterface(RosInterface rosInterface) {
+        if (uniqueTopics.containsKey(rosInterface.topic)) {
+            //TODO: refactor, add more checks for valid config file
+            if (!uniqueTopics.get(rosInterface.topic).getImportString().equals(rosInterface.type))
+                throw new IllegalArgumentException("ROS topic can only have one type.");
+
+            return uniqueTopics.get(rosInterface.topic);
+        } else {
+
+            String includeString = rosInterface.type;
+            if (!includeString.contains("/"))
+                throw new IllegalArgumentException("The ROS msg type has to be given in the form package/msgName!");
+            String topicType = includeString.substring(includeString.lastIndexOf("/") + 1);
+            RosTopic tmpTopic = new RosTopic(rosInterface.topic, topicType, includeString);
+            uniqueTopics.put(rosInterface.topic, tmpTopic);
+            return tmpTopic;
+        }
+
 
     }
 
--- src/test/java/de/monticore/lang/monticar/generator/roscpp/yamlTest.java
+++ src/test/java/de/monticore/lang/monticar/generator/roscpp/yamlTest.java
@@ -1,23 +1,36 @@
 package de.monticore.lang.monticar.generator.roscpp;
 
-import com.esotericsoftware.yamlbeans.YamlException;
 import de.monticar.lang.monticar.generator.python.RosTag;
 import de.monticar.lang.monticar.generator.python.TagReader;
+import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.ExpandedComponentInstanceSymbol;
+import de.monticore.lang.tagging._symboltable.TaggingResolver;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
-public class yamlTest {
+public class yamlTest extends AbstractSymtabTest {
 
     @Test
-    public void parseYamlTest() throws FileNotFoundException, YamlException {
+    public void parseYamlTest() throws IOException {
         TagReader<RosTag> reader = new TagReader<>();
-        List<RosTag> tags = reader.readYAML("src/test/resources/tests/config/config.yaml");
+        TaggingResolver symtab = createSymTabAndTaggingResolver("src/test/resources");
 
-        assertTrue(tags.size() == 2);
+
+        ExpandedComponentInstanceSymbol componentInstanceSymbol = symtab.<ExpandedComponentInstanceSymbol>resolve("tests.a.compA", ExpandedComponentInstanceSymbol.KIND).orElse(null);
+
+        GeneratorRosCpp generatorRosCpp = new GeneratorRosCpp();
+        generatorRosCpp.setGenerationTargetPath("./target/generated-sources-roscpp/testRos/");
+
+        List<RosTag> tags = reader.readYAML("src/test/resources/tests/config/config.yaml");
+        assertTrue(tags.size() == 1);
+
+        yamlHelper.rosTagToDataHelper(symtab, tags.get(0));
+
+        generatorRosCpp.generateFiles(componentInstanceSymbol, symtab);
+
     }
 
 }
