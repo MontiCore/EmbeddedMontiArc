16aba9b2f218fb7583b9d542b3dc2bcf83551d41
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2018-01-26 16:47:14 +0000

    Repaired YamlTest::convertYamlTest

--- src/test/java/de/monticore/lang/monticar/generator/roscpp/YamlTest.java
+++ src/test/java/de/monticore/lang/monticar/generator/roscpp/YamlTest.java
@@ -3,23 +3,24 @@ package de.monticore.lang.monticar.generator.roscpp;
 import com.google.common.collect.Lists;
 import de.monticar.lang.monticar.generator.python.RosTag;
 import de.monticar.lang.monticar.generator.python.TagReader;
+import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.PortSymbol;
+import de.monticore.lang.monticar.generator.roscpp.helper.ResolveHelper;
 import de.monticore.lang.monticar.generator.roscpp.helper.YamlHelper;
 import de.monticore.lang.tagging._symboltable.TaggingResolver;
-import org.junit.Ignore;
+import de.monticore.symboltable.CommonSymbol;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class YamlTest extends AbstractSymtabTest {
 
-    @Ignore
     @Test
     public void convertYamlTest() throws IOException {
         //Setup
@@ -27,53 +28,47 @@ public class YamlTest extends AbstractSymtabTest {
         TaggingResolver symtab = createSymTabAndTaggingResolver("src/test/resources");
 
         //Execute
-//        List<RosTag> tags = reader.readYAML("src/test/resources/config/config.yaml");
-//        YamlHelper.rosTagToDataHelper(symtab, tags.get(0));
-//
-//        //Check
-//        assertTrue(tags.size() == 1);
-//
-//        Set<RosTopic> topics = DataHelper.getTopics();
-//        assertTrue(topics.size() == 2);
-//
-//        HashMap<String, RosTopic> topicNameToTopic = new HashMap<>();
-//        topics.forEach(t -> topicNameToTopic.put(t.getName(), t));
-//
-//        assertTrue(topicNameToTopic.containsKey("test"));
-//        assertTrue(topicNameToTopic.containsKey("test2"));
-//
-//        RosTopic topicTest = topicNameToTopic.get("test");
-//        RosTopic topicTest2 = topicNameToTopic.get("test2");
-//
-//        assertTrue(topicTest.getRosType().equals("CarMessage"));
-//        assertTrue(topicTest2.getRosType().equals("StampedFloat64"));
-//
-//        assertTrue(topicTest.getImportString().equals("automated_driving_msgs/CarMessage"));
-//        assertTrue(topicTest2.getImportString().equals("automated_driving_msgs/StampedFloat64"));
-//
-//        HashMap<String, PortSymbol> portNameToPort = new HashMap<>();
-//        topics.forEach(t -> t.getPorts().forEach(p -> portNameToPort.put(p.getName(), p)));
-//
-//        assertTrue(portNameToPort.containsKey("rosIn"));
-//        assertTrue(portNameToPort.containsKey("noRosIn"));
-//        assertTrue(portNameToPort.containsKey("rosOut"));
-//
-//        PortSymbol port1 = portNameToPort.get("rosIn");
-//        PortSymbol port2 = portNameToPort.get("noRosIn");
-//        PortSymbol port3 = portNameToPort.get("rosOut");
-//
-//        assertTrue(topicTest.getPorts().contains(port1));
-//        assertTrue(topicTest.getPorts().contains(port2));
-//        assertFalse(topicTest.getPorts().contains(port3));
-//
-//        assertFalse(topicTest2.getPorts().contains(port1));
-//        assertFalse(topicTest2.getPorts().contains(port2));
-//        assertTrue(topicTest2.getPorts().contains(port3));
-//
-//        assertEquals(DataHelper.getMsgFieldFromPort(port1).orElse(null), "posX");
-//        assertEquals(DataHelper.getMsgFieldFromPort(port2).orElse(null), "posY");
-//        assertEquals(DataHelper.getMsgFieldFromPort(port3).orElse(null), "data");
+        List<RosTag> tags = reader.readYAML("src/test/resources/config/config.yaml");
 
+        //Check
+        assertTrue(tags.size() == 1);
+
+        ResolvedRosTag tag = ResolveHelper.resolveRosTag(tags.get(0), symtab);
+
+        assertNotNull(tag.getComponent());
+        assertEquals(tag.getComponent().getFullName(), "tests.a.compA");
+
+        assertTrue(tag.getPublisherInterfaces().size() == 1);
+        assertTrue(tag.getSubscriberInterfaces().size() == 1);
+
+        ResolvedRosInterface subInter = tag.getSubscriberInterfaces().stream().findFirst().get();
+
+        assertEquals(subInter.getTopic(), "test");
+        assertEquals(subInter.getInclude(), "automated_driving_msgs/CarMessage");
+
+        Map<String, PortSymbol> portNamesToPort = subInter.getPorts().stream().collect(Collectors.toMap(CommonSymbol::getName, p -> p));
+        assertTrue(portNamesToPort.size() == 2);
+        assertTrue(portNamesToPort.containsKey("rosIn"));
+        assertTrue(portNamesToPort.containsKey("noRosIn"));
+
+        PortSymbol rosIn = portNamesToPort.get("rosIn");
+        PortSymbol noRosIn = portNamesToPort.get("noRosIn");
+        assertEquals(subInter.getMsgFieldForPort(rosIn).getConversion(rosIn), "msg->posX");
+        assertEquals(subInter.getMsgFieldForPort(noRosIn).getConversion(noRosIn), "msg->posY");
+
+
+        ResolvedRosInterface pubInter = tag.getPublisherInterfaces().stream().findFirst().get();
+
+        assertEquals(pubInter.getTopic(), "test2");
+        assertEquals(pubInter.getInclude(), "automated_driving_msgs/StampedFloat64");
+
+        portNamesToPort = pubInter.getPorts().stream().collect(Collectors.toMap(CommonSymbol::getName, p -> p));
+
+        assertTrue(portNamesToPort.size() == 1);
+        assertTrue(portNamesToPort.containsKey("rosOut"));
+
+        PortSymbol rosOut = portNamesToPort.get("rosOut");
+        assertEquals(pubInter.getMsgFieldForPort(rosOut).getConversion(rosOut), ".data = component.rosOut");
     }
 
     @Test
