c4145de4f5e6e75dca20900bd1108a9124140def
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2018-01-31 13:37:15 +0000

    Added Context Conditions for RosConnections + tests

---
+++ src/main/java/de/monticore/lang/monticar/generator/roscpp/cocos/EmbeddedMontiArcMathSymtabCoCo.java
@@ -0,0 +1,8 @@
+package de.monticore.lang.monticar.generator.roscpp.cocos;
+
+import de.monticore.lang.tagging._symboltable.TaggingResolver;
+import de.monticore.symboltable.Symbol;
+
+public interface EmbeddedMontiArcMathSymtabCoCo {
+    void check(TaggingResolver taggingResolver, Symbol symbol);
+}
