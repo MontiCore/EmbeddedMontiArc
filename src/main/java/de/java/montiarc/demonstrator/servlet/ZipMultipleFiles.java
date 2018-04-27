3bcba40c6a16ace2df1e467cb15975ce0594b4fe
Ievgen Strepkov <eugen.strepkov@gmail.com>
2018-04-27 11:29:04 +0200

    Added the class to zip files

---
+++ src/main/java/de/java/montiarc/demonstrator/servlet/ZipMultipleFiles.java
@@ -0,0 +1,41 @@
+package de.java.montiarc.demonstrator.servlet;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipMultipleFiles {
+
+    public ByteArrayOutputStream zipIt(String... files) throws IOException {
+
+        List<String> srcFiles = Arrays.asList(files);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ZipOutputStream zipOut = new ZipOutputStream(baos);
+
+        for (String srcFile : srcFiles) {
+
+            File fileToZip = new File(srcFile);
+            ByteArrayInputStream fis = new ByteArrayInputStream(Files.readAllBytes(fileToZip.toPath()));
+            ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+
+            zipOut.putNextEntry(zipEntry);
+
+            byte[] bytes = new byte[1024];
+            int length;
+            while((length = fis.read(bytes)) >= 0) {
+                zipOut.write(bytes, 0, length);
+            }
+            fis.close();
+
+            System.out.println(srcFile + " has been packed");
+        }
+
+        zipOut.close();
+        baos.close();
+
+        return baos;
+    }
+}
