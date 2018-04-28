f87665fb7bac68b05816c4c31a4610137397a09d
Ievgen Strepkov <eugen.strepkov@gmail.com>
2018-04-28 12:15:54 +0200

    Added the function which writes zip file to the disk

--- src/main/java/de/java/montiarc/demonstrator/servlet/ZipMultipleFiles.java
+++ src/main/java/de/java/montiarc/demonstrator/servlet/ZipMultipleFiles.java
@@ -9,7 +9,7 @@ import java.util.zip.ZipOutputStream;
 
 public class ZipMultipleFiles {
 
-    public ByteArrayOutputStream zipIt(String... files) throws IOException {
+    public ByteArrayOutputStream zipItToStream(String... files) throws IOException {
 
         List<String> srcFiles = Arrays.asList(files);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -37,5 +37,28 @@ public class ZipMultipleFiles {
         baos.close();
 
         return baos;
+    }
+
+    public void zipItToFile(String... files) throws IOException {
+
+        List<String> srcFiles = Arrays.asList(files);
+        FileOutputStream fos = new FileOutputStream("multiCompressed.zip");
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+
+        for (String srcFile : srcFiles) {
+                File fileToZip = new File(srcFile);
+                FileInputStream fis = new FileInputStream(fileToZip);
+                ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+                zipOut.putNextEntry(zipEntry);
+
+                byte[] bytes = new byte[1024];
+                int length;
+                while((length = fis.read(bytes)) >= 0) {
+                    zipOut.write(bytes, 0, length);
+                }
+                fis.close();
+            }
+            zipOut.close();
+            fos.close();
     }
 }
