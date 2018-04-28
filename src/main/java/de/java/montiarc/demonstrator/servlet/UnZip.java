c1d03300a60f168de9f5e821aeff60bd1532c028
Ievgen Strepkov <eugen.strepkov@gmail.com>
2018-04-28 12:21:55 +0200

    Added another method for extracting zip for future use

--- src/main/java/de/java/montiarc/demonstrator/servlet/UnZip.java
+++ src/main/java/de/java/montiarc/demonstrator/servlet/UnZip.java
@@ -61,3 +61,26 @@ public class UnZip
         }
     }
 }
+
+// TODO: try to use this code to extract an archive
+//public class UnzipFile {
+//    public static void main(String[] args) throws IOException {
+//        String fileZip = "compressed.zip";
+//        byte[] buffer = new byte[1024];
+//        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+//        ZipEntry zipEntry = zis.getNextEntry();
+//        while(zipEntry != null){
+//            String fileName = zipEntry.getName();
+//            File newFile = new File("unzipTest/" + fileName);
+//            FileOutputStream fos = new FileOutputStream(newFile);
+//            int len;
+//            while ((len = zis.read(buffer)) > 0) {
+//                fos.write(buffer, 0, len);
+//            }
+//            fos.close();
+//            zipEntry = zis.getNextEntry();
+//        }
+//        zis.closeEntry();
+//        zis.close();
+//    }
+//}
