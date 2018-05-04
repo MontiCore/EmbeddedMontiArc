4926c74f5bfb2724dcdb5f0bbb064b2ea6fb81b7
pbilov <petyo.ilov@rwth-aachen.de>
2018-05-04 12:36:57 +0200

    running processes in background

--- EmbeddedMontiArcStudio/scripts/distr-sim/start-script.bat
+++ EmbeddedMontiArcStudio/scripts/distr-sim/start-script.bat
@@ -3,11 +3,11 @@ call ..\shared\variables.bat
 pushd %~dp0
 cd %RMI%
 call mvn clean install
-start java -Djava.library.path=%RMI_PATH% -Djava.rmi.server.codebase=.\target\rmi-model-server-1.0.1.jar -Djava.rmi.server.hostname=127.0.0.1 -cp .\target\rmi-model-server-1.0.1.jar rwth.rmi.model.server.RMIServer
+start /b java -Djava.library.path=%RMI_PATH% -Djava.rmi.server.codebase=.\target\rmi-model-server-1.0.1.jar -Djava.rmi.server.hostname=127.0.0.1 -cp .\target\rmi-model-server-1.0.1.jar rwth.rmi.model.server.RMIServer
 popd
 
-start %PSQL%\PostgreSQLPortable.exe
+start /b %PSQL%\PostgreSQLPortable.exe
 timeout 3 > NUL
-start %SFS%\sfs2x-standalone.exe
+start /b %SFS%\sfs2x-standalone.exe
 timeout 10 > NUL
 call %CHROME% http://localhost/visualization
