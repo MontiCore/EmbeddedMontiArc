8296e0143c169f29ad588e4b47a230f330732bb2
Thomas Michael Timmermanns <thomas.timmermanns@rwth-aachen.de>
2018-05-17 18:09:25 +0200

    Fixed generation tests for Windows.

--- src/test/java/de/monticore/lang/monticar/emadl/GenerationTest.java
+++ src/test/java/de/monticore/lang/monticar/emadl/GenerationTest.java
@@ -24,6 +24,7 @@ import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.Componen
 import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.ExpandedComponentInstanceSymbol;
 import de.monticore.lang.monticar.emadl.generator.AbstractSymtab;
 import de.monticore.lang.monticar.emadl.generator.EMADLGenerator;
+import de.monticore.lang.monticar.emadl.generator.EMADLGeneratorCli;
 import de.monticore.lang.monticar.generator.FileContent;
 import de.monticore.lang.tagging._symboltable.TaggingResolver;
 import de.se_rwth.commons.Splitters;
@@ -35,6 +36,8 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -42,7 +45,7 @@ import static de.monticore.lang.monticar.emadl.ParserTest.ENABLE_FAIL_QUICK;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
-public class GenerationTest {
+public class GenerationTest extends AbstractSymtabTest {
 
     @Before
     public void setUp() {
@@ -51,134 +54,88 @@ public class GenerationTest {
         Log.enableFailQuick(ENABLE_FAIL_QUICK);
     }
 
-    private void generate(String qualifiedName) throws IOException, TemplateException{
-        EMADLGenerator gen =  new EMADLGenerator();
-        gen.generate("src/test/resources/models/", qualifiedName);
-    }
-
-    private List<FileContent> generateStrings(String modelsDirPath, String qualifiedName) throws IOException, TemplateException {
-        EMADLGenerator gen = new EMADLGenerator();
-        gen.setModelsPath( modelsDirPath );
-        TaggingResolver symtab = AbstractSymtab.createSymTabAndTaggingResolver(gen.getModelsPath());
-        ComponentSymbol component = symtab.<ComponentSymbol>resolve(qualifiedName, ComponentSymbol.KIND).orElse(null);
-
-        List<String> splitName = Splitters.DOT.splitToList(qualifiedName);
-        String componentName = splitName.get(splitName.size() - 1);
-        String instanceName = componentName.substring(0, 1).toLowerCase() + componentName.substring(1);
-
-        if (component == null){
-            Log.error("Component with name '" + componentName + "' does not exist.");
-            System.exit(1);
-        }
-
-        ExpandedComponentInstanceSymbol instance = component.getEnclosingScope().<ExpandedComponentInstanceSymbol>resolve(instanceName, ExpandedComponentInstanceSymbol.KIND).get();
-
-        return gen.generateStrings(symtab, instance, symtab);
-    }
-
-    private String readFileFromResources(String relativePath) throws IOException{
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(relativePath).getFile());
-        Scanner scanner = new Scanner(file);
-        scanner.useDelimiter("\\Z");
-        String content = scanner.next() + "\n";
-        scanner.close();
-        return content;
-    }
-
     @Test
     public void testCifar10Generation() throws IOException, TemplateException {
-        //generate("cifar10.Cifar10Classifier");
-        Log.getFindings().clear();
-
-        List<FileContent> fileContents = generateStrings(
-                "src/test/resources/models/",
-                "cifar10.Cifar10Classifier");
+        Log.getFindings().clear();;
+        String[] args = {"-m", "src/test/resources/models/", "-r", "cifar10.Cifar10Classifier"};
+        EMADLGeneratorCli.main(args);
         assertTrue(Log.getFindings().isEmpty());
 
-        for (FileContent fileContent : fileContents){
-            switch (fileContent.getFileName()){
-                case "cifar10_cifar10Classifier.h":
-                    assertEquals(fileContent.getFileContent(),
-                            readFileFromResources("target_code/cifar10_cifar10Classifier.h"));
-                    break;
-                case "CNNCreator_cifar10_cifar10Classifier_net.py":
-                    assertEquals(fileContent.getFileContent(),
-                            readFileFromResources("target_code/CNNCreator_cifar10_cifar10Classifier_net.py"));
-                    break;
-                case "CNNBufferFile.h":
-                    assertEquals(fileContent.getFileContent(),
-                            readFileFromResources("target_code/CNNBufferFile.h"));
-                    break;
-                case "CNNPredictor_cifar10_cifar10Classifier_net.h":
-                    assertEquals(fileContent.getFileContent(),
-                            readFileFromResources("target_code/CNNPredictor_cifar10_cifar10Classifier_net.h"));
-                    break;
-                case "cifar10_cifar10Classifier_net.h":
-                    assertEquals(fileContent.getFileContent(),
-                            readFileFromResources("target_code/cifar10_cifar10Classifier_net.h"));
-                    break;
-                case "CNNTranslator.h":
-                    assertEquals(fileContent.getFileContent(),
-                            readFileFromResources("target_code/CNNTranslator.h"));
-                    break;
-                case "cifar10_cifar10Classifier_calculateClass.h":
-                    assertEquals(fileContent.getFileContent(),
-                            readFileFromResources("target_code/cifar10_cifar10Classifier_calculateClass.h"));
-                    break;
-                case "CNNTrainer_cifar10_Cifar10Classifier.py":
-                    assertEquals(fileContent.getFileContent(),
-                            readFileFromResources("target_code/CNNTrainer_cifar10_Cifar10Classifier.py"));
-                    break;
-            }
-        }
+        checkFilesAreEqual(
+                Paths.get("./target/generated-sources-emadl"),
+                Paths.get("./src/test/resources/target_code"),
+                Arrays.asList(
+                        "cifar10_cifar10Classifier.h",
+                        "CNNCreator_cifar10_cifar10Classifier_net.py",
+                        "CNNBufferFile.h",
+                        "CNNPredictor_cifar10_cifar10Classifier_net.h",
+                        "cifar10_cifar10Classifier_net.h",
+                        "CNNTranslator.h",
+                        "cifar10_cifar10Classifier_calculateClass.h",
+                        "CNNTrainer_cifar10_Cifar10Classifier.py"));
     }
 
     @Test
     public void testSimulatorGeneration() throws IOException, TemplateException {
-        generate("simulator.MainController");
+        Log.getFindings().clear();;
+        String[] args = {"-m", "src/test/resources/models/", "-r", "simulator.MainController"};
+        EMADLGeneratorCli.main(args);
         assertTrue(Log.getFindings().isEmpty());
     }
 
     @Test
     public void testAddGeneration() throws IOException, TemplateException {
-        generate("Add");
+        Log.getFindings().clear();;
+        String[] args = {"-m", "src/test/resources/models/", "-r", "Add"};
+        EMADLGeneratorCli.main(args);
         assertTrue(Log.getFindings().isEmpty());
     }
 
     @Test
     public void testAlexnetGeneration() throws IOException, TemplateException {
-        generate("Alexnet");
+        Log.getFindings().clear();;
+        String[] args = {"-m", "src/test/resources/models/", "-r", "Alexnet"};
+        EMADLGeneratorCli.main(args);
         assertTrue(Log.getFindings().isEmpty());
     }
 
     @Test
     public void testResNeXtGeneration() throws IOException, TemplateException {
-        generate("ResNeXt50");
+        Log.getFindings().clear();;
+        String[] args = {"-m", "src/test/resources/models/", "-r", "ResNeXt50"};
+        EMADLGeneratorCli.main(args);
         assertTrue(Log.getFindings().isEmpty());
     }
 
     @Test
     public void testThreeInputGeneration() throws IOException, TemplateException {
-        generate("ThreeInputCNN_M14");
+        Log.getFindings().clear();;
+        String[] args = {"-m", "src/test/resources/models/", "-r", "ThreeInputCNN_M14"};
+        EMADLGeneratorCli.main(args);
         assertTrue(Log.getFindings().size() == 1);
     }
 
     @Test
     public void testMultipleOutputsGeneration() throws IOException, TemplateException {
-        generate("MultipleOutputs");
+        Log.getFindings().clear();;
+        String[] args = {"-m", "src/test/resources/models/", "-r", "MultipleOutputs"};
+        EMADLGeneratorCli.main(args);
         assertTrue(Log.getFindings().size() == 1);
     }
 
     @Test
     public void tesVGGGeneration() throws IOException, TemplateException {
-        generate("VGG16");
+        Log.getFindings().clear();;
+        String[] args = {"-m", "src/test/resources/models/", "-r", "VGG16"};
+        EMADLGeneratorCli.main(args);
         assertTrue(Log.getFindings().isEmpty());
     }
 
     @Test
     public void testMultipleInstances() throws IOException, TemplateException {
-        generate("InstanceTest.MainB");
+        Log.getFindings().clear();;
+        String[] args = {"-m", "src/test/resources/models/", "-r", "InstanceTest.MainB"};
+        EMADLGeneratorCli.main(args);
         assertTrue(Log.getFindings().isEmpty());
     }
 }
