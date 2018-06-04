b1b6838ac908e58247d63af558a537e269a75c1b
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2018-06-04 14:51:04 +0100

    batch -> bash autopilot simulation

---
+++ EmbeddedMontiArcStudio/scripts/autopilot/generate.sh
@@ -0,0 +1,9 @@
+rm -rf ${CPP_DIR}
+mkdir ${CPP_DIR}
+${JAVA_HOME}/bin/java -jar "${HOME}/emam2cpp.jar" \
+   --models-dir="${HOME}/model/autopilot" \
+   --root-model=de.rwth.armin.modeling.autopilot.autopilot \
+   --output-dir="${CPP_DIR}" \
+   --flag-generate-autopilot-adapter \
+   --flag-use-armadillo-backend
+cp -r ${HOME}/precompiled ${CPP_DIR}
---
+++ EmbeddedMontiArcStudio/scripts/autopilot/simulate.start.sh
@@ -0,0 +1,6 @@
+cd ../shared
+source variables.sh
+cd ../autopilot
+source generate.sh
+source compile.sh
+source start-simulation.sh
