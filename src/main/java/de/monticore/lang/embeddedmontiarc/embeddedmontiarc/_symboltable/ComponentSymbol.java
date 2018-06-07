a95a77f1233be8e806367ddda12960257f8e16f8
ferdinand.mehlan <ferdinand.mehlan@rwth-aachen.de>
2018-06-07 11:41:29 +0200

    Restore ConnectorSymbol, which had gone missing

--- src/main/java/de/monticore/lang/embeddedmontiarc/embeddedmontiarc/_symboltable/ComponentSymbol.java
+++ src/main/java/de/monticore/lang/embeddedmontiarc/embeddedmontiarc/_symboltable/ComponentSymbol.java
@@ -53,7 +53,7 @@ import static de.monticore.symboltable.Symbols.sortSymbolsByPosition;
  *
  * @author Robert Heim
  */
-public class ComponentSymbol extends CommonScopeSpanningSymbol implements ElementInstance{
+public class ComponentSymbol extends CommonScopeSpanningSymbol {
   
   public static final ComponentKind KIND = new ComponentKind();
   
