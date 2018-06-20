c2015d1ff7cc5fd30f76ef918008152b173b29c5
Christoph Richter <christoph.richter@rwth-aachen.de>
2018-06-20 17:19:14 +0200

    MathOptLanguage: Added resolving filters

--- src/main/java/de/monticore/lang/mathopt/_symboltable/MathOptLanguage.java
+++ src/main/java/de/monticore/lang/mathopt/_symboltable/MathOptLanguage.java
@@ -22,10 +22,16 @@ package de.monticore.lang.mathopt._symboltable;
 
 import de.monticore.lang.math.LogConfig;
 import de.monticore.lang.math._symboltable.MathLanguage;
+import de.monticore.lang.math._symboltable.MathModelNameCalculator;
+import de.monticore.lang.math._symboltable.MathStatementsSymbol;
+import de.monticore.lang.math._symboltable.expression.MathExpressionSymbol;
 import de.monticore.symboltable.MutableScope;
 import de.monticore.symboltable.ResolvingConfiguration;
-import de.monticore.symboltable.SymbolTableCreator;
+import de.monticore.symboltable.Symbol;
+import de.monticore.symboltable.resolving.CommonResolvingFilter;
+import de.monticore.symboltable.resolving.ResolvingFilter;
 
+import java.util.Collection;
 import java.util.Optional;
 
 /**
@@ -33,8 +39,11 @@ import java.util.Optional;
  */
 public class MathOptLanguage extends MathOptLanguageTOP {
 
+    private MathLanguage parentLanguage;
+
     public MathOptLanguage() {
         super("Math Optimization Language", MathLanguage.FILE_ENDING);
+        parentLanguage = new MathLanguage();
         LogConfig.init();
     }
 
@@ -46,5 +55,14 @@ public class MathOptLanguage extends MathOptLanguageTOP {
     @Override
     public Optional<MathOptSymbolTableCreator> getSymbolTableCreator(ResolvingConfiguration resolvingConfiguration, MutableScope mutableScope) {
         return Optional.of(new MathOptSymbolTableCreator(resolvingConfiguration, mutableScope));
+    }
+
+    @Override
+    protected void initResolvingFilters() {
+        super.initResolvingFilters();
+        addResolvingFilter(CommonResolvingFilter.create(MathExpressionSymbol.KIND));
+        addResolvingFilter(CommonResolvingFilter.create(MathStatementsSymbol.KIND));
+
+        setModelNameCalculator(new MathModelNameCalculator());
     }
 }
