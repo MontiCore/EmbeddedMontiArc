33aaf7f74189a33b735398628a57df84e6ff5321
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2018-06-21 00:09:30 +0200

    Fix for new sciebo links Moved dependencies into new sciebo

--- build/download.sh
+++ build/download.sh
@@ -11,6 +11,11 @@ while read line; do
 
 		# get filename
 		filename=${link##*/}
+		#fix sciebo format
+		if [[ $filename = *"files="* ]]; then
+			filename=$(echo $filename | sed -e 's/.*files=\(.*\)/\1/')
+		fi
+
 		# if dependencies.txt does not contain name, use curl to get it
 		if [[ ! $filename = *".zip" ]]; then
 			filename=$(curl -sI  $link | grep -o -E 'filename=.*$' | sed -e 's/filename=//' | tr -d "\"\r")
