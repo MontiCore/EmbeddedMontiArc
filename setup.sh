cc5e653557e29b6d3e61b4bf2526359eeb375c33
Thomas Michael Timmermanns <thomas.timmermanns@rwth-aachen.de>
2018-06-23 01:16:29 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 echo "Installing dependencies!"
 read -p "This script will download multiple GB of packages. Press [CTRL-C] to stop or  [ENTER] to continue." tmp
 
+sudo -v
 EmbeddedMontiArcStudio/scripts/classifier/installMxNet.sh
-EmbeddedMontiArcStudio/scripts/intersection/setup.sh
+sudo EmbeddedMontiArcStudio/scripts/intersection/setup.sh
