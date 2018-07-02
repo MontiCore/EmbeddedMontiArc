6590a98f440aa2aff22d57eeb8fcad95b1012c01
Christoph Richter <christoph.richter@rwth-aachen.de>
2018-07-02 14:42:05 +0200

    StringIndexHelper: Added function to modify multiple bracket entries
    StringIndexHelper: Extended replace function for all bracket entries

--- src/main/java/de/monticore/lang/monticar/generator/cpp/converter/StringIndexHelper.java
+++ src/main/java/de/monticore/lang/monticar/generator/cpp/converter/StringIndexHelper.java
@@ -2,28 +2,36 @@ package de.monticore.lang.monticar.generator.cpp.converter;
 
 /**
  * @author Sascha Schneiders
+ * @author Christoph Richter
  */
 public class StringIndexHelper {
 
     public static String modifyContentBetweenBracketsByAdding(String input, String modifier) {
+        return modifyContentBetweenBracketsByAdding(input, modifier, "(", ",", ")");
+    }
+
+    /**
+     * Only adds modifier in the first occurring bracket
+     */
+    public static String modifyContentBetweenBracketsByAdding(String input, String modifier, String bracketStart, String bracketSep, String bracketEnd) {
         String result = "";
         boolean done = false;
-        int indexFirst = input.indexOf("(", 0);
+        int indexFirst = input.indexOf(bracketStart, 0);
         if (indexFirst == -1)
             return input;
         result += input.substring(0, indexFirst);
-        int indexSecond = input.indexOf(",", indexFirst + 1);
+        int indexSecond = input.indexOf(bracketSep, indexFirst + bracketSep.length());
         if (indexSecond == -1) {
-            indexSecond = input.indexOf(")", indexFirst + 1);
+            indexSecond = input.indexOf(bracketEnd, indexFirst + bracketEnd.length());
             done = true;
         }
         while (!done) {
             result += input.substring(indexFirst, indexSecond) + modifier;
             indexFirst = indexSecond;
-            indexSecond = input.indexOf(",", indexSecond + 1);
+            indexSecond = input.indexOf(bracketSep, indexSecond + bracketSep.length());
 
             if (indexSecond == -1) {
-                indexSecond = input.indexOf(")", indexFirst + 1);
+                indexSecond = input.indexOf(bracketEnd, indexFirst + bracketEnd.length());
                 done = true;
             }
         }
@@ -37,9 +45,22 @@ public class StringIndexHelper {
         if (input.endsWith(endPart)) {
             int indexFirst = input.lastIndexOf(endPart);
             result = input.substring(0, indexFirst);
-            result+=newEndPart;
+            result += newEndPart;
         }
         return result;
+    }
+
+    public static String modifyContentBetweenBracketsByAddingForAllBrackets(String input, String modifier, String bracketStart, String bracketSep, String bracketEnd) {
+        String[] parts = input.split("(?<=\\))");
+        StringBuilder sb = new StringBuilder();
+        for (String s : parts) {
+            sb.append(modifyContentBetweenBracketsByAdding(s, modifier, bracketStart, bracketSep, bracketEnd));
+        }
+        return sb.toString();
+    }
+
+    public static String modifyContentBetweenBracketsByAddingForAllBrackets(String input, String modifier) {
+        return modifyContentBetweenBracketsByAddingForAllBrackets(input, modifier, "(", ",", ")");
     }
 
 }
