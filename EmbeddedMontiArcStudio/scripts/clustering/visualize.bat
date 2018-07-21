4dd7eda7344ef2ecb63f42f3dfadf2fd1a83b0c0
0xJMR <25683724+0xjmr@users.noreply.github.com>
2018-07-21 15:58:23 +0200

    Fixes small issue with Clusterer.

--- EmbeddedMontiArcStudio/scripts/clustering/visualize.bat
+++ EmbeddedMontiArcStudio/scripts/clustering/visualize.bat
@@ -2,6 +2,6 @@ call "..\shared\variables.bat"
 if exist "%SVG_HOME%\SVG" rmdir "%SVG_HOME%\SVG" /s /q
 mkdir "%SVG_HOME%\SVG"
 "%JAVA_HOME%\bin\java.exe" -jar "%VISUALIZATION_EMAM_HOME%\visualization-emam.jar" ^
-   --model "detection.spectralClusterer" ^
+   --model "detection.objectDetector1" ^
    --modelPath "%HOME%\model\clustering\\" ^
    --outputPath "%SVG_HOME%\SVG\\"
