5b4a3017c69c40b383f047d36f2fb5e5aa6c74bb
Sascha Schneiders <sascha.schneiders@rwth-aachen.de>
2018-08-12 19:27:57 +0200

    updated log path

--- src/main/java/de/monticore/lang/monticar/generator/testing/StreamTestModifier.java
+++ src/main/java/de/monticore/lang/monticar/generator/testing/StreamTestModifier.java
@@ -1,21 +1,20 @@
 /**
- *
- *  ******************************************************************************
- *  MontiCAR Modeling Family, www.se-rwth.de
- *  Copyright (c) 2017, Software Engineering Group at RWTH Aachen,
- *  All rights reserved.
- *
- *  This project is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 3.0 of the License, or (at your option) any later version.
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this project. If not, see <http://www.gnu.org/licenses/>.
+ * ******************************************************************************
+ * MontiCAR Modeling Family, www.se-rwth.de
+ * Copyright (c) 2017, Software Engineering Group at RWTH Aachen,
+ * All rights reserved.
+ * <p>
+ * This project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this project. If not, see <http://www.gnu.org/licenses/>.
  * *******************************************************************************
  */
 package de.monticore.lang.monticar.generator.testing;
@@ -93,7 +92,7 @@ public class StreamTestModifier {
         File streamTest = new File(filePath);
         if (!streamTest.exists()) {
 
-            File curDir = new File("N:\\");
+            File curDir = new File("N:\\target\\generated-sources-cpp\\streamtest\\exec\\");
             getAllFiles(curDir);
         }
         List<String> lines = new ArrayList<>();
@@ -113,9 +112,7 @@ public class StreamTestModifier {
     public static void getAllFiles(File curDir) {
         File[] filesList = curDir.listFiles();
         for (File f : filesList) {
-            if (f.isFile()) {
-                System.out.println(f.getName());
-            }
+            System.out.println(f.getName());
         }
     }
 }
