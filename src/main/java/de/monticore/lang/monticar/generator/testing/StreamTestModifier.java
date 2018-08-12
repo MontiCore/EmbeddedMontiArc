44d2741ce51daca504acc812ee2ab41703f369dd
Sascha Schneiders <sascha.schneiders@rwth-aachen.de>
2018-08-12 19:23:50 +0200

    updated path log

--- src/main/java/de/monticore/lang/monticar/generator/testing/StreamTestModifier.java
+++ src/main/java/de/monticore/lang/monticar/generator/testing/StreamTestModifier.java
@@ -113,8 +113,6 @@ public class StreamTestModifier {
     public static void getAllFiles(File curDir) {
         File[] filesList = curDir.listFiles();
         for (File f : filesList) {
-            if (f.isDirectory())
-                getAllFiles(f);
             if (f.isFile()) {
                 System.out.println(f.getName());
             }
