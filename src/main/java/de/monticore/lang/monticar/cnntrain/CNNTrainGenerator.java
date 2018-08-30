d084d16c4c8c20e54b1047a5d779f62d345d6d9b
Svetlana Pavlitskaya <svetlana.pavlitskaya@rwth-aachen.de>
2018-08-30 15:20:30 +0200

    added generator interface

---
+++ src/main/java/de/monticore/lang/monticar/cnntrain/CNNTrainGenerator.java
@@ -0,0 +1,49 @@
+/**
+ *
+ *  ******************************************************************************
+ *  MontiCAR Modeling Family, www.se-rwth.de
+ *  Copyright (c) 2017, Software Engineering Group at RWTH Aachen,
+ *  All rights reserved.
+ *
+ *  This project is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3.0 of the License, or (at your option) any later version.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this project. If not, see <http://www.gnu.org/licenses/>.
+ * *******************************************************************************
+ */
+package de.monticore.lang.monticar.cnntrain;
+
+import de.monticore.lang.monticar.cnntrain._symboltable.ConfigurationSymbol;
+import de.monticore.symboltable.Scope;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+
+public interface CNNTrainGenerator {
+
+    String getInstanceName();
+
+    void setInstanceName(String instanceName);
+
+    String getGenerationTargetPath();
+
+    void setGenerationTargetPath(String generationTargetPath);
+
+    void generate(Path modelsDirPath, String rootModelNames);
+
+    void generate(Scope scope, String rootModelName);
+
+    //check cocos with CNNTrainCocos.checkAll(configuration) before calling this method.
+    void generateFile(ConfigurationSymbol configuration) throws IOException;
+
+    //check cocos with CNNTrainCocos.checkAll(configuration) before calling this method.
+    Map<String, String> generateStrings(ConfigurationSymbol configuration);
+}
