22241084a0bf489746c5ca89ebf3bcfbc8718ad4
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2018-11-14 15:11:28 +0100

    Remove KMeans algo; unfit for task

--- src/main/java/de/monticore/lang/monticar/generator/middleware/clustering/ClusteringAlgorithmFactory.java
+++ src/main/java/de/monticore/lang/monticar/generator/middleware/clustering/ClusteringAlgorithmFactory.java
@@ -1,6 +1,5 @@
 package de.monticore.lang.monticar.generator.middleware.clustering;
 
-import de.monticore.lang.monticar.generator.middleware.clustering.algorithms.KMeansAlgorithm;
 import de.monticore.lang.monticar.generator.middleware.clustering.algorithms.SpectralClusteringAlgorithm;
 import de.se_rwth.commons.logging.Log;
 
@@ -13,7 +12,6 @@ public class ClusteringAlgorithmFactory {
     public static ClusteringAlgorithm getFromKind(ClusteringKind kind){
         switch (kind){
             case SPECTRAL_CLUSTERER: return new SpectralClusteringAlgorithm();
-            case KMEANS: return new KMeansAlgorithm();
             default: Log.error("0x1D54C: No clustering algorithm found for ClusteringKind " + kind);
         }
         return null;
--- src/main/java/de/monticore/lang/monticar/generator/middleware/clustering/ClusteringKind.java
+++ src/main/java/de/monticore/lang/monticar/generator/middleware/clustering/ClusteringKind.java
@@ -2,5 +2,4 @@ package de.monticore.lang.monticar.generator.middleware.clustering;
 
 public enum ClusteringKind {
     SPECTRAL_CLUSTERER,
-    KMEANS;
 }
