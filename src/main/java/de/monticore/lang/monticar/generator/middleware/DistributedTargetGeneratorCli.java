8ad9b272cff7c5928aa1db7c3866372745de9366
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2019-01-07 18:31:42 +0100

    Transform Cli from options(e.g. -g=cpp) to json

--- src/main/java/de/monticore/lang/monticar/generator/middleware/DistributedTargetGeneratorCli.java
+++ src/main/java/de/monticore/lang/monticar/generator/middleware/DistributedTargetGeneratorCli.java
@@ -1,5 +1,7 @@
 package de.monticore.lang.monticar.generator.middleware;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.ExpandedComponentInstanceSymbol;
 import de.monticore.lang.embeddedmontiarc.tagging.middleware.ros.RosToEmamTagSchema;
 import de.monticore.lang.monticar.emadl.generator.EMADLAbstractSymtab;
@@ -14,6 +16,8 @@ import de.monticore.lang.tagging._symboltable.TaggingResolver;
 import de.se_rwth.commons.logging.Log;
 import org.apache.commons.cli.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -22,47 +26,23 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-/**
- * java -cp target/embedded-montiarc-math-middleware-generator-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
- * de.monticore.lang.monticar.generator.middleware.DistributedTargetGeneratorCli \
- * --models-dir=src/test/resources/ \
- * --root-model=tests.a.addComp \
- * --output-dir=target/cli-test/src \
- * --generators=cpp,roscpp
- */
 public final class DistributedTargetGeneratorCli {
 
-    private static final Option OPTION_MODELS_PATH = Option.builder("m")
-            .longOpt("models-dir")
-            .desc("full path to directory with EMAM models")
-            .hasArg(true)
-            .required(true)
-            .build();
-
-    private static final Option OPTION_ROOT_MODEL = Option.builder("r")
-            .longOpt("root-model")
-            .desc("fully qualified name of the root model")
-            .hasArg(true)
-            .required(true)
-            .build();
-
-    private static final Option OPTION_OUTPUT_PATH = Option.builder("o")
-            .longOpt("output-dir")
-            .desc("full path to output directory for generated files")
-            .hasArg(true)
-            .required(true)
-            .build();
-
-    private static final Option OPTION_GENERATORS = Option.builder("g")
-            .longOpt("generators")
-            .desc("identifiers for the generators that should be used")
-            .hasArg(true)
-            .required(true)
-            .numberOfArgs(10)
-            .valueSeparator(',')
-            .build();
-
-    private static final Option OPTION_EMADL_BACKEND = EMADLGeneratorCli.OPTION_BACKEND;
+    private static final String helpString = "Parameters: <file path to config json> OR -r <raw json config string>\n\n" +
+            "Schema of config json:\n" +
+            "{\n" +
+            "    'modelsDir':'<path to directory with EMAM models>',\n" +
+            "    'outputDir':'<path to output directory for generated files>',\n" +
+            "    'rootModel':'<fully qualified name of the root model>',\n" +
+            "    'generators':['<identifier for first generator>', '<identifier for second generator>',...],\n" +
+            "    'emadlBackend':'<deep-learning-framework backend. Options: MXNET, CAFFE2>'\n" +
+            "}\n\n" +
+            "Generator Options:\n" +
+            "Behaviour generators:\n" +
+            "    'cpp': EMAM2CPP\n" +
+            "    'emadlcpp': EMADL2CPP\n" +
+            "Middleware generators:\n" +
+            "    'roscpp': EMAM2Roscpp";
 
     public static final String GENERATOR_CPP = "cpp";
     public static final String GENERATOR_EMADL = "emadlcpp";
@@ -73,23 +53,30 @@ public final class DistributedTargetGeneratorCli {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(args));
+        Gson gson = new Gson();
+        CliParameters parameters = null;
+        //File name only
+        if(args.length == 1){
 
-        Options options = getOptions();
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cliArgs = parseArgs(options, parser, args);
-        if (cliArgs != null) {
-            runGenerator(cliArgs);
+            String filePath = expandHomeDir(args[0]);
+            try {
+                JsonReader jsonReader = new JsonReader(new FileReader(filePath));
+                parameters = gson.fromJson(jsonReader, CliParameters.class);
+            } catch (FileNotFoundException e) {
+                Log.error("0x49E6A: Can not find specified config file: " + args[0] + "!");
+            }
+
         }
-    }
+        //raw mode
+        else if(args.length == 2 && args[0].equals("-r")){
+            parameters = gson.fromJson(args[1], CliParameters.class);
+        }else{
+            System.out.println(helpString);
+        }
 
-    private static Options getOptions() {
-        Options options = new Options();
-        options.addOption(OPTION_MODELS_PATH);
-        options.addOption(OPTION_ROOT_MODEL);
-        options.addOption(OPTION_OUTPUT_PATH);
-        options.addOption(OPTION_GENERATORS);
-        options.addOption(OPTION_EMADL_BACKEND);
-        return options;
+        if(parameters != null){
+            runGenerator(parameters);
+        }
     }
 
     private static Set<String> getGeneratorNames() {
@@ -101,70 +88,72 @@ public final class DistributedTargetGeneratorCli {
         return res;
     }
 
-    private static CommandLine parseArgs(Options options, CommandLineParser parser, String[] args) {
-        CommandLine cliArgs;
-        try {
-            cliArgs = parser.parse(options, args);
-        } catch (ParseException e) {
-            Log.error("0x9A1AC: Argument parsing exception", e);
-            return null;
+    public static void runGenerator(CliParameters cliParameters) {
+        if(cliParameters.getModelsDir() == null){
+            Log.error("0xEB6FE: No models dir specified!");
+            return;
         }
-        return cliArgs;
-    }
+        String fullModelsDirPath = expandHomeDir(cliParameters.getModelsDir());
 
-    private static void runGenerator(CommandLine cliArgs) {
-        String modelsDirPath = expandHomeDir(cliArgs.getOptionValue(OPTION_MODELS_PATH.getOpt()));
-        if(!Files.isDirectory(Paths.get(modelsDirPath))){
-            Log.error("0x6444B: Models dir does not exist: " + modelsDirPath);
+        if(cliParameters.getGenerators().size() == 0){
+            Log.error("0x6178E: No generator was specified!");
             return;
         }
 
-        String outputPath = expandHomeDir(cliArgs.getOptionValue(OPTION_OUTPUT_PATH.getOpt()));
-        String rootModelName = cliArgs.getOptionValue(OPTION_ROOT_MODEL.getOpt());
-        Set<String> generators = Arrays.stream(cliArgs.getOptionValues(OPTION_GENERATORS.getOpt())).collect(Collectors.toSet());
+        if(!Files.isDirectory(Paths.get(fullModelsDirPath))){
+            Log.error("0x6444B: Models dir does not exist: " + fullModelsDirPath);
+            return;
+        }
+
         TaggingResolver taggingResolver;
-        if (generators.contains(GENERATOR_EMADL)) {
-            taggingResolver = EMADLAbstractSymtab.createSymTabAndTaggingResolver(modelsDirPath);
+        if (cliParameters.getGenerators().contains(GENERATOR_EMADL)) {
+            taggingResolver = EMADLAbstractSymtab.createSymTabAndTaggingResolver(fullModelsDirPath);
         }
         else{
-            taggingResolver = AbstractSymtab.createSymTabAndTaggingResolver(modelsDirPath);
+            taggingResolver = AbstractSymtab.createSymTabAndTaggingResolver(fullModelsDirPath);
         }
 
         DistributedTargetGenerator generator = new DistributedTargetGenerator();
-        generator.setGenerationTargetPath(outputPath);
+        generator.setGenerationTargetPath(cliParameters.getOutputDir());
 
         Set<String> validGenNames = getGeneratorNames();
-        generators.forEach(genName -> {
+
+        cliParameters.getGenerators().forEach(genName -> {
             if (validGenNames.contains(genName)) {
                 Log.warn("Using generator " + genName);
             } else {
                 Log.error("0xE28B6: Not a valid generator Name:" + genName  +".");
+                return;
             }
         });
 
-        ExpandedComponentInstanceSymbol componentInstanceSymbol = taggingResolver.<ExpandedComponentInstanceSymbol>resolve(rootModelName, ExpandedComponentInstanceSymbol.KIND).orElse(null);
+        ExpandedComponentInstanceSymbol componentInstanceSymbol = taggingResolver.<ExpandedComponentInstanceSymbol>resolve(cliParameters.getRootModel(), ExpandedComponentInstanceSymbol.KIND).orElse(null);
 
         if (componentInstanceSymbol == null) {
             Log.error("0x5FFAE: The given component cannot be resolved.");
             return;
         }
 
-        if (generators.contains(GENERATOR_CPP)) {
+        if (cliParameters.getGenerators().contains(GENERATOR_CPP)) {
             generator.add(new CPPGenImpl(), "cpp");
         }
 
-        if (generators.contains(GENERATOR_EMADL)) {
-            String backendString = cliArgs.getOptionValue(OPTION_EMADL_BACKEND.getOpt());
-            generator.add(new EMADLGeneratorImpl(cliArgs.getOptionValue(OPTION_MODELS_PATH.getOpt()), backendString), "cpp");
+        if (cliParameters.getGenerators().contains(GENERATOR_EMADL)) {
+            if(cliParameters.getEmadlBackend() != null && !cliParameters.getEmadlBackend().equals("")) {
+                generator.add(new EMADLGeneratorImpl(fullModelsDirPath, cliParameters.getEmadlBackend()), "cpp");
+            }else{
+                Log.error("0x0D839: Generator EMADL was specified but no backend was selected!");
+                return;
+            }
         }
 
-        if (generators.contains(GENERATOR_ROSCPP)) {
+        if (cliParameters.getGenerators().contains(GENERATOR_ROSCPP)) {
             generator.add(new RosCppGenImpl(), "roscpp");
             RosToEmamTagSchema.registerTagTypes(taggingResolver);
             TagHelper.resolveTags(taggingResolver, componentInstanceSymbol);
         }
 
-        if (generators.contains(GENERATOR_ODV)) {
+        if (cliParameters.getGenerators().contains(GENERATOR_ODV)) {
             generator.add(new ODVGenImpl(), "odv");
         }
 
@@ -173,7 +162,6 @@ public final class DistributedTargetGeneratorCli {
         } catch (IOException e) {
             Log.error("Error generating files", e);
         }
-
     }
 
     private static String expandHomeDir(String path) {
