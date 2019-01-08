1c1e3d89145483690fdb4cd866b6680d8c194c45
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2019-01-08 20:50:26 +0100

    Remove hardcoded results for generation test. Check integration test instead.

--- src/test/resources/results/system/src/ba_system_collisionDetection/coordinator/IAdapter_ba_system_collisionDetection.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ba_system_collisionDetection.h"
-
-class IAdapter_ba_system_collisionDetection{
-	public:
-		virtual ~IAdapter_ba_system_collisionDetection(){}
-		virtual void init(ba_system_collisionDetection* comp) = 0;
-		virtual void tick() = 0;
-};
