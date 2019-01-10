7f791d02821bc5cb5e8be8b2a63a1e1316e9ec8b
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2019-01-10 16:43:18 +0100

    Remove filesAreEquals tests, use integration test instead

--- src/test/java/de/monticore/lang/monticar/generator/middleware/GenerationTest.java
+++ src/test/java/de/monticore/lang/monticar/generator/middleware/GenerationTest.java
@@ -76,8 +76,6 @@ public class GenerationTest extends AbstractSymtabTest {
         middlewareGenerator.add(new CPPGenImpl(), "cpp");
 
         List<File> files = middlewareGenerator.generate(componentInstanceSymbol, taggingResolver);
-        testFilesAreEqual(files, "CMakeCppOnly/src/", generationTargetPath);
-
     }
 
     @Test
@@ -102,8 +100,6 @@ public class GenerationTest extends AbstractSymtabTest {
         distributedTargetGenerator.add(new RosCppGenImpl(), "roscpp");
 
         List<File> files = distributedTargetGenerator.generate(componentInstanceSymbol, taggingResolver);
-
-        testFilesAreEqual(files, "system/src/", generationTargetPath);
     }
 
     @Test
--- src/test/resources/results/system/src/ba_system_intersectionController/coordinator/IAdapter_ba_system_intersectionController.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ba_system_intersectionController.h"
-
-class IAdapter_ba_system_intersectionController{
-	public:
-		virtual ~IAdapter_ba_system_intersectionController(){}
-		virtual void init(ba_system_intersectionController* comp) = 0;
-		virtual void tick() = 0;
-};
