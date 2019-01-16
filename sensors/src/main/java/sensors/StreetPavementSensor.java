68a5687817260d411c7bfbe8a5efb87c1573dc2b
Theresa Täuber <taeuber.theresa@web.de>
2019-01-16 13:05:24 +0100

    fehler behoben

--- sensors/src/main/java/sensors/StreetPavementSensor.java
+++ sensors/src/main/java/sensors/StreetPavementSensor.java
@@ -1,3 +1,23 @@
+/**
+ *
+ * ******************************************************************************
+ *  MontiCAR Modeling Family, www.se-rwth.de
+ *  Copyright (c) 2017, Software Engineering Group at RWTH Aachen,
+ *  All rights reserved.
+ *
+ *  This project is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3.0 of the License, or (at your option) any later version.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this project. If not, see <http://www.gnu.org/licenses/>.
+ * *******************************************************************************
+ */
 package sensors;
 import commons.controller.commons.BusEntry;
 import commons.simulation.Sensor;
@@ -7,10 +27,10 @@ import simulation.environment.WorldModel;
 import simulation.environment.geometry.osmadapter.GeomStreet;
 import simulation.environment.visualisationadapter.implementation.Street2D;
 import simulation.environment.visualisationadapter.interfaces.EnvStreet;
-import sensors.factory.SensorFactory;
+//import sensors.factory.SensorFactory;
 import simulation.vehicle.PhysicalVehicle;
-import simulation.vehicle.Vehicle;
-import static commons.controller.commons.BusEntry.SENSOR_STREETPAVEMENT;
+//import simulation.vehicle.Vehicle;
+//import static commons.controller.commons.BusEntry.SENSOR_STREETPAVEMENT;
 
 import java.util.Optional;
 
