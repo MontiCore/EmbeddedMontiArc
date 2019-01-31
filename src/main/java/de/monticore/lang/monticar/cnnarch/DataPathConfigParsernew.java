cbd0822624034976b6b2a494cc4422d94ca07995
Hoda Anvari Kazemabad <hoda.kazemabad@rwth-aachen.de>
2019-01-31 10:10:40 +0100

    New DataPathConfigParser

---
+++ src/main/java/de/monticore/lang/monticar/cnnarch/DataPathConfigParsernew.java
@@ -0,0 +1,51 @@
+package de.monticore.lang.monticar.cnnarch.mxnetgenerator;
+
+import de.monticore.lang.monticar.cnntrain._symboltable.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataPathConfigParser{
+
+    private String configTargetPath;
+	ConfigurationSymbol configuration;
+    private String configFileName;
+
+    public DataPathConfigParser(ConfigurationSymbol configuration, String configPath) {
+        this.configuration = configuration;
+		setConfigPath(configPath); 
+		setConfigFileName("ConfigChainAutomation");
+    }	
+	
+	public String getConfigFileName() {
+        return configFileName;
+    }
+	
+	public void setConfigFileName(String configFileName){
+		this.configFileName = configFileName + ".cnnt";
+	}
+	
+	public String getConfigPath() {
+        if (configTargetPath.charAt(configTargetPath.length() - 1) != '/') {
+            this.configTargetPath = configTargetPath + "/";
+        }
+        return configTargetPath;
+    }
+	
+	public void setConfigPath(String configTargetPath){
+		this.configTargetPath = configTargetPath + getConfigFileName;
+	}
+	
+    public ConfigurationSymbol getConfiguration() {
+        return configuration;
+    }
+
+	public String getDataPath(String modelName) {
+        if (!getConfiguration().getEntryMap().containsKey(modelName)) {
+            return null;
+        }
+        return String.valueOf(getConfiguration().getEntry(modelName).getValue());
+    }
+}
