39663ef437bc0d43083fbf87d194334c5c1fa0d1
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2019-02-02 19:47:59 +0100

    Added models: Autopilot, Supermario, simulatorModel Added EvaluationTest.java as base for evaluation

---
+++ src/test/java/de/monticore/lang/monticar/generator/middleware/EvaluationTest.java
@@ -0,0 +1,29 @@
+package de.monticore.lang.monticar.generator.middleware;
+
+import de.monticore.lang.monticar.generator.middleware.cli.DistributedTargetGeneratorCli;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class EvaluationTest {
+
+    @Test
+    public void testPacman() {
+        DistributedTargetGeneratorCli.main(new String[]{"./src/test/resources/config/evaluation/pacman.json"});
+    }
+
+    @Test
+    public void testAutopilot() {
+        DistributedTargetGeneratorCli.main(new String[]{"./src/test/resources/config/evaluation/autopilot.json"});
+    }
+
+    @Test
+    public void testSupermario() {
+        DistributedTargetGeneratorCli.main(new String[]{"./src/test/resources/config/evaluation/supermario.json"});
+    }
+
+    @Ignore("Fails on Spectral")
+    @Test
+    public void testSimulatorModel() {
+        DistributedTargetGeneratorCli.main(new String[]{"./src/test/resources/config/evaluation/simulatormodel.json"});
+    }
+}
