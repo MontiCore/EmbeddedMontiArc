29c9580ed50872bc7296c46037ba35345bd22f55
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2019-02-03 22:16:28 +0100

    Complete example

--- example/build.sh
+++ example/build.sh
@@ -1,7 +1,10 @@
+rm -r target/; 
+java -jar embedded-montiarc-emadl-generator-0.2.5-SNAPSHOT-jar-with-dependencies.jar -m src/emadl/models/ -r cNNCalculator.Connector -o target -b MXNET -p /home/christopher/anaconda3/bin/python
+
 rm -rf build
 mkdir build && cd build
 
 echo "Building DigitClassifier.."
 cmake ..
-cp -r /home/christopher/Documents/Master/Semester2/Praktikum/mxnet/include/mxnet/ ../target/
+#cp -r /home/christopher/Documents/Master/Semester2/Praktikum/mxnet/include/mxnet/ ../target/
 make
