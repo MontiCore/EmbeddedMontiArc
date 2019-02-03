f1971e9505f7f1803c36616f04cf73f04f6ecc97
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2019-02-03 22:19:03 +0100

    Fixed python path

--- example/build.sh
+++ example/build.sh
@@ -1,5 +1,5 @@
 rm -r target/; 
-java -jar embedded-montiarc-emadl-generator-0.2.5-SNAPSHOT-jar-with-dependencies.jar -m src/emadl/models/ -r cNNCalculator.Connector -o target -b MXNET -p /home/christopher/anaconda3/bin/python
+java -jar embedded-montiarc-emadl-generator-0.2.5-SNAPSHOT-jar-with-dependencies.jar -m src/emadl/models/ -r cNNCalculator.Connector -o target -b MXNET
 
 rm -rf build
 mkdir build && cd build
