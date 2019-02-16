4b2900567fca777bfbdcfc79f35e6cfec339e58f
Carlos Alfredo Yeverino Rodriguez <carlos.alfredo.yeverino@gmail.com>
2019-02-16 21:24:13 +0100

    fix sonarQube code smells

--- src/main/java/de/monticore/lang/monticar/cnnarch/caffe2generator/CNNArch2Caffe2.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/caffe2generator/CNNArch2Caffe2.java
@@ -51,18 +51,18 @@ public class CNNArch2Caffe2 implements CNNArchGenerator{
     private boolean isSupportedLayer(ArchitectureElementSymbol element, LayerSupportChecker layerChecker){
         List<ArchitectureElementSymbol> constructLayerElemList;
 
-        if (!(element instanceof IOSymbol) && (element.getResolvedThis().get() instanceof CompositeElementSymbol))
-        {
+        if (!(element instanceof IOSymbol) && (element.getResolvedThis().get() instanceof CompositeElementSymbol)) {
             constructLayerElemList = ((CompositeElementSymbol)element.getResolvedThis().get()).getElements();
             for (ArchitectureElementSymbol constructedLayerElement : constructLayerElemList) {
-                if (!isSupportedLayer(constructedLayerElement, layerChecker)) return false;
+                if (!isSupportedLayer(constructedLayerElement, layerChecker)) {
+                    return false;
+                }
             }
         }
         if (!layerChecker.isSupported(element.toString())) {
             Log.error("Unsupported layer " + "'" + element.getName() + "'" + " for the backend CAFFE2.");
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
@@ -70,9 +70,16 @@ public class CNNArch2Caffe2 implements CNNArchGenerator{
     private boolean supportCheck(ArchitectureSymbol architecture){
         LayerSupportChecker layerChecker = new LayerSupportChecker();
         for (ArchitectureElementSymbol element : ((CompositeElementSymbol)architecture.getBody()).getElements()){
-            if(!isSupportedLayer(element, layerChecker)) return false;
+            if(!isSupportedLayer(element, layerChecker)) {
+                return false;
+            }
         }
         return true;
+    }
+
+    private static void quitGeneration(){
+        Log.error("Code generation is aborted");
+        System.exit(1);
     }
 
     public CNNArch2Caffe2() {
@@ -105,19 +112,17 @@ public class CNNArch2Caffe2 implements CNNArchGenerator{
         Optional<CNNArchCompilationUnitSymbol> compilationUnit = scope.resolve(rootModelName, CNNArchCompilationUnitSymbol.KIND);
         if (!compilationUnit.isPresent()){
             Log.error("could not resolve architecture " + rootModelName);
-            System.exit(1);
+            quitGeneration();
         }
 
         CNNArchCocos.checkAll(compilationUnit.get());
         if (!supportCheck(compilationUnit.get().getArchitecture())){
-            Log.error("Code generation aborted.");
-            System.exit(1);
+            quitGeneration();
         }
 
         try{
             generateFiles(compilationUnit.get().getArchitecture());
-        }
-        catch (IOException e){
+        } catch (IOException e){
             Log.error(e.toString());
         }
     }
@@ -172,7 +177,7 @@ public class CNNArch2Caffe2 implements CNNArchGenerator{
         try {
             generateFromFilecontentsMap(fileContentMap);
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.error("CMake file could not be generated" + e.getMessage());
         }
     }
 
@@ -195,8 +200,8 @@ public class CNNArch2Caffe2 implements CNNArchGenerator{
         cMakeConfig.addCMakeCommand("set(LIBS ${LIBS} -lprotobuf -lglog -lgflags)");
         cMakeConfig.addCMakeCommand("find_package(CUDA)" + "\n"
                                         + "set(INCLUDE_DIRS ${INCLUDE_DIRS} ${CUDA_INCLUDE_DIRS})" + "\n"
-                                        + "set(LIBS ${LIBS} ${CUDA_LIBRARIES} ${CUDA_curand_LIBRARY})" + "\n"); //Needed since CUDA cannot be found correctly (including CUDA_curand_LIBRARY) and as optional using CMakeFindModule
-
+                                        + "set(LIBS ${LIBS} ${CUDA_LIBRARIES} ${CUDA_curand_LIBRARY})" + "\n");
+                                        //Needed since CUDA cannot be found correctly (including CUDA_curand_LIBRARY)
 
         cMakeConfig.addCMakeCommand("if(CUDA_FOUND)" + "\n" + "  set(LIBS ${LIBS} caffe2 caffe2_gpu)"
                                         + "\n" + "else()" + "\n" + "  set(LIBS ${LIBS} caffe2)" + "\n" + "endif()");
