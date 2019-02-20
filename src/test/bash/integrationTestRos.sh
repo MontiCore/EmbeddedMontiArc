1a34025e461afb5e4d5f2a081f36884889de0ec0
Your Name <you@example.com>
2019-02-20 19:37:27 +0100

    Added Ros2 integration test

--- src/test/bash/integrationTestRos.sh
+++ src/test/bash/integrationTestRos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+baseDir=$(readlink -f `dirname $0`/../../..)
 
-for f in `find . -name compile.sh`
+for f in `find $baseDir/target/generated-sources-cmake/ -name compile.sh`
 do
 	chmod +x $f
 	bash -H $f
