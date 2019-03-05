e95e276aa65866e43b79d029e434043ce10d5f83
Alexander David Hellwig <alexander.hellwig@rwth-aachen.de>
2019-03-05 10:36:44 +0100

    Revert "Add required/optional field name check to StrictJsonDeserializer"
    This reverts commit 94e49c79e431e2b7028b1b9007eab16d210d2004

--- src/main/java/de/monticore/lang/monticar/generator/middleware/cli/StrictJsonDeserializer.java
+++ src/main/java/de/monticore/lang/monticar/generator/middleware/cli/StrictJsonDeserializer.java
@@ -4,19 +4,15 @@ import com.google.gson.*;
 import de.se_rwth.commons.logging.Log;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class StrictJsonDeserializer<T> implements JsonDeserializer<T> {
 
-    private final List<String> optionalFieldNames;
-    private final List<String> requiredFieldNames;
+    private final List<String> fieldNames;
     private final Gson delegate;
 
-    public StrictJsonDeserializer(List<String> optionalFieldNames, List<String> requiredFieldNames, Gson delegate) {
-        this.optionalFieldNames = optionalFieldNames;
-        this.requiredFieldNames = requiredFieldNames;
+    public StrictJsonDeserializer(List<String> fieldNames, Gson delegate) {
+        this.fieldNames = fieldNames;
         this.delegate = delegate;
     }
 
@@ -26,31 +22,12 @@ public class StrictJsonDeserializer<T> implements JsonDeserializer<T> {
             Log.error("Expecting json object!");
         } else {
             JsonObject jsonObj = jsonElement.getAsJsonObject();
-            Set<String> keys = jsonObj.keySet();
-
-            checkRequiredFields(keys);
-            checkOnlyValidFields(keys);
+            for (String s : jsonObj.keySet()) {
+                if (!fieldNames.contains(s)) {
+                    Log.error("0x9DB6E: " + s + " is not a valid field name! Valid options: " + fieldNames);
+                }
+            }
         }
         return delegate.fromJson(jsonElement, type);
-    }
-
-    private void checkOnlyValidFields(Set<String> keys) {
-        List<String> allFieldNames = new ArrayList<>();
-        allFieldNames.addAll(optionalFieldNames);
-        allFieldNames.addAll(requiredFieldNames);
-
-        for (String s : keys) {
-            if (!allFieldNames.contains(s)) {
-                Log.error("0x9DB6E: " + s + " is not a valid field name! Valid options: " + allFieldNames);
-            }
-        }
-    }
-
-    private void checkRequiredFields(Set<String> keys) {
-        for (String requiredFieldName : requiredFieldNames) {
-            if (!keys.contains(requiredFieldName)){
-                Log.error("0x4C433: can not find required field " + requiredFieldName);
-            }
-        }
     }
 }
