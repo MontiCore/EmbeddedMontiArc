66e6c0578b16cb7b6fec210d02700921c018a3b6
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2019-03-09 13:18:54 +0100

    Automatic location of mxnet library

--- example/build.sh
+++ example/build.sh
@@ -1,3 +1,14 @@
+MXNET_PATH=$(python -c "import mxnet; print(mxnet.__file__)")
+MXNET_FOLDER=${MXNET_PATH::-11}
+echo $MXNET_FOLDER
+
+if [ ! -f $MXNET_FOLDER/libmxnet.so ]; then
+    echo "libmxnet.so not found at default location" $MXNET_FOLDER
+    echo "It should be there if the python mxnet package is installed"
+    echo "Either fix the installation, or adapt the ./build.sh script to locate libmxnet.so correctly"
+    exit 1
+fi
+
 rm -rf target
 java -jar ../embedded-montiarc-emadl-generator-0.2.10-jar-with-dependencies.jar -m src/emadl/models/ -r cNNCalculator.Connector -o target -b MXNET
 
@@ -5,6 +16,5 @@ rm -rf build
 mkdir build && cd build
 
 echo "Building DigitClassifier.."
-cmake ..
-#cp -r /home/christopher/Documents/Master/Semester2/Praktikum/mxnet/include/mxnet/ ../target/
+cmake -D MXNET_PATH=$MXNET_FOLDER/libmxnet.so ..
 make
