831c05ed47c59a42418cc9e67c2d470d05a15c91
hengwen <h.w.zhangvic@gmail.com>
2019-04-09 22:40:09 +0200

    Enable the apprx converter to convert xy coordinates back to lon lat coordinates

--- environment/src/test/java/simulation/environment/osm/ApproximateConverterTest.java
+++ environment/src/test/java/simulation/environment/osm/ApproximateConverterTest.java
@@ -92,4 +92,34 @@ public class ApproximateConverterTest extends TestCase {
         //larger error for larger distances
         assertEquals(63950, k3.distance(k4), 70);
     }
+
+    public void testConvertXYToLonLat() {
+        // First try to convert lon lat points into xy points
+        // Then try to convert them back to check if the data are consistent
+        ApproximateConverter converter = new ApproximateConverter(6.06132, 50.78026);
+        ArrayList<Point3D> points = new ArrayList<>();
+
+        points.add(new Point3D(6.06146,50.78059, 0));
+        points.add(new Point3D(6.06132,50.78055, 0));
+        points.add(new Point3D(6.06166, 50.78033,0));
+        points.add(new Point3D(6.06155, 50.78026, 0));
+
+        ArrayList<Point3D> kPoints = new ArrayList<>();
+
+        for(Point3D p : points) {
+            kPoints.add(converter.convertLongLatPoint(p));
+        }
+
+        for(int i = 0; i < points.size(); i++) {
+            Point3D p = points.get(i);
+            Point3D k = kPoints.get(i);
+
+            assertEquals(p.getY(), converter.convertYToLat(k.getY()));
+            assertEquals(p.getX(), converter.convertXToLong(k.getX(), k.getY()));
+
+            Point3D lonLatPointConvertedFromXYPoint = converter.convertXYPoint(k);
+            assertEquals(p.getX(), lonLatPointConvertedFromXYPoint.getX());
+            assertEquals(p.getY(), lonLatPointConvertedFromXYPoint.getY());
+        }
+    }
 }
