487fa6f453f87b84b1471bbcd52432edc3b4cc49
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2019-04-14 21:50:27 +0200

    Delete stacktrace.log when starting test

--- src/test/resources/results/cmake/test/BasicPortsMath/test/tests_main.cpp
+++ src/test/resources/results/cmake/test/BasicPortsMath/test/tests_main.cpp
@@ -2,11 +2,13 @@
 #ifndef TESTS_MAIN
 #define TESTS_MAIN
 #include <iostream>
+#include <stdio.h>
 
 #include "test_basicPortsMath_test.hpp"
 
 int main(){
     std::cout << "=================Start stream testing=================" << std::endl;
+    remove("stacktrace.log");
     int errorCode = 0;
     errorCode += test_basicPortsMath_test::runTest();
     std::cout << "==================End stream testing==================" << std::endl;
--- src/test/resources/results/cmake/test/math/FloatDivisionTest/test/tests_main.cpp
+++ src/test/resources/results/cmake/test/math/FloatDivisionTest/test/tests_main.cpp
@@ -2,11 +2,13 @@
 #ifndef TESTS_MAIN
 #define TESTS_MAIN
 #include <iostream>
+#include <stdio.h>
 
 #include "test_math_floatDivisionTest_test.hpp"
 
 int main(){
     std::cout << "=================Start stream testing=================" << std::endl;
+    remove("stacktrace.log");
     int errorCode = 0;
     errorCode += test_math_floatDivisionTest_test::runTest();
     std::cout << "==================End stream testing==================" << std::endl;
