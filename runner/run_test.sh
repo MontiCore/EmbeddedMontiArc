90ca9c2d3b71d833e31fcc6001d4d9f6bb2f8455
hengwen <h.w.zhangvic@gmail.com>
2019-04-24 00:49:43 +0200

    We are running test by manually bring up and link autopilot server and integration test runner container.

--- runner/run_test.sh
+++ runner/run_test.sh
@@ -1,27 +1,5 @@
 #!/usr/bin/env bash
 #
-# ====
-#
-#     ******************************************************************************
-#      MontiCAR Modeling Family, www.se-rwth.de
-#      Copyright (c) 2017, Software Engineering Group at RWTH Aachen,
-#      All rights reserved.
-#
-#      This project is free software; you can redistribute it and/or
-#      modify it under the terms of the GNU Lesser General Public
-#      License as published by the Free Software Foundation; either
-#      version 3.0 of the License, or (at your option) any later version.
-#      This library is distributed in the hope that it will be useful,
-#      but WITHOUT ANY WARRANTY; without even the implied warranty of
-#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#      Lesser General Public License for more details.
-#
-#      You should have received a copy of the GNU Lesser General Public
-#      License along with this project. If not, see <http://www.gnu.org/licenses/>.
-#     *******************************************************************************
-# ====
-#
-#
 # ******************************************************************************
 #  MontiCAR Modeling Family, www.se-rwth.de
 #  Copyright (c) 2017, Software Engineering Group at RWTH Aachen,
@@ -41,7 +19,34 @@
 # *******************************************************************************
 #
 
-mvn clean install -DskipTests -s settings.xml
+current_dir=$(pwd)
+parent_dir="$(dirname $current_dir)"
+cp -r ../lib/ ./lib
 
-cd runner
-mvn -Dtests=RunnerTest -s ../settings.xml test
+
+docker network create simulation-network
+
+# start autopilot
+docker run \
+    --rm \
+    --network simulation-network \
+    -d \
+    --name=autopilot \
+    registry.git.rwth-aachen.de/monticore/embeddedmontiarc/simulators/rmimodelserver:latest \
+    java -cp rmi-model-server.jar -Djava.rmi.server.codebase=file:rmi-model-server.jar \
+    -Djava.rmi.server.useLocalHostname rwth.rmi.model.server.RMIServer 10101 autopilots --no-zookeeper no_time os_linux
+
+# start integration test
+docker run \
+    --rm \
+    -it \
+    --network simulation-network \
+    -v ${parentdir}:/app \
+    simulation-integration-test:latest \
+    /bin/bash
+
+rm -rf ./lib
+# remove all containers
+docker rm $(docker ps -a -q)
+# remove all images
+docker rmi $(docker images -q)
