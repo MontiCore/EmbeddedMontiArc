ed952c2b156b5b52ffba66e6e8cafd69e2445c49
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2019-05-23 17:25:18 +0200

    Fix generator jar arguments

--- src/stacktraceDebugger/testRunner.ts
+++ src/stacktraceDebugger/testRunner.ts
@@ -4,11 +4,11 @@ import { RuntimeLogger } from "./RuntimeLogger";
 import { window } from 'vscode';
 import { existsSync } from 'fs';
 
-export class EMATestRunner{
-	private targetBasePath:string;
-	private generatorJarPath:string;
-	private modelBasePath:string;
-	private logger:RuntimeLogger;
+export class EMATestRunner {
+	private targetBasePath: string;
+	private generatorJarPath: string;
+	private modelBasePath: string;
+	private logger: RuntimeLogger;
 
 	public getLogger(): RuntimeLogger {
 		return this.logger;
@@ -45,32 +45,45 @@ export class EMATestRunner{
 		this.logger = logger;
 	}
 
-	public getTargetDir(componentName: string){
+	public getTargetDir(componentName: string) {
 		let hash = createHash('md5').update(this.modelBasePath).update(componentName).digest("hex").substr(0, 6);
 		console.log("Hash for '" + componentName + "':" + hash);
 		return this.targetBasePath + "/" + hash;
 	}
 
-	public executeTests(componentName: string):boolean {
+	public executeTests(componentName: string): boolean {
 		this.logger.log("Generating...");
 		let returnCode = this.generateTests(componentName);
-		if(returnCode == 0){
+		if (returnCode == 0) {
 			this.logger.log("Building and running tests...");
 			this.buildAndRunTests(componentName);
 			return true;
-		}else{
+		} else {
 			this.logger.log("The generator returned a non-zero return code. Aborting!");
 			return false;
 		}
 	}
 
-	public generateTests(componentName: string):number {
+	public generateTests(componentName: string): number {
 		const targetDir = this.getTargetDir(componentName);
 		this.execCommand("mkdir", [this.targetBasePath], process.cwd(), false);
 		this.execCommand("mkdir", [targetDir], process.cwd(), false);
-		if(existsSync(this.generatorJarPath)){
-			return this.execCommand("java", ["-jar", this.generatorJarPath, "--models-dir=" + this.modelBasePath, "-o=" + targetDir, "--root-model=" + componentName, "--flag-generate-tests", "--flag-use-armadillo-backend", "--flag-use-exec-logging", "--flag-use-exec-logging", "--flag-use-cmake"], targetDir, true, true);
-		}else{
+		if (existsSync(this.generatorJarPath)) {
+			return this.execCommand("java",
+				[
+					"-jar",
+					this.generatorJarPath,
+					"--models-dir=" + this.modelBasePath,
+					"-o=" + targetDir,
+					"--root-model=" + componentName,
+					"--flag-generate-tests",
+					"--flag-use-armadillo-backend",
+					"--flag-use-exec-logging",
+					"--flag-use-exec-logging",
+					"--flag-generate-cmake"
+				],
+				targetDir, true, true);
+		} else {
 			this.getLogger().log("Can not find generator jar: " + this.generatorJarPath);
 			return 1;
 		}
@@ -83,7 +96,7 @@ export class EMATestRunner{
 		this.execCommand("cmake", ["--build", targetDir + "/build"], process.cwd());
 	}
 
-	public execCommand(command: string, args: string[], cwd, warn:boolean = true, popup:boolean = false): number {
+	public execCommand(command: string, args: string[], cwd, warn: boolean = true, popup: boolean = false): number {
 		console.log("EXEC COMMAND!");
 		const child = spawnSync(command, args,
 			{
@@ -96,12 +109,12 @@ export class EMATestRunner{
 		if (child.status != 0) {
 			const output = child.stdout.toString();
 			const errOutput = child.stderr.toString();
-			if (warn){
+			if (warn) {
 				this.logger.log("Command: " + command + " " + args.join(" "));
 				this.logger.log(output);
 				this.logger.log("finished with error:\n" + errOutput);
 			}
-			if(popup && errOutput.length > 0){
+			if (popup && errOutput.length > 0) {
 				let firstError = errOutput.split("\n")[0];
 				window.showErrorMessage(firstError);
 			}
