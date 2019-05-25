d81064f4bcf059f34996377231162b9f82d4cf18
markus.bendel <markus.bendel@rwth-aachen.de>
2019-05-25 16:04:55 +0200

    added basic maven project structure

---
+++ src/main/java/emam2someipgroup/App.java
@@ -0,0 +1,13 @@
+package emam2someipgroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
