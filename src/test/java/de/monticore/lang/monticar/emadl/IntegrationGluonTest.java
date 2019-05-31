9a6f7b0923c337f57734797655c2f48e5248c22b
Sebastian Nickels <snickels@foxfire.consulting>
2019-05-31 02:42:03 +0200

    Implemented multiple inputs

--- src/test/java/de/monticore/lang/monticar/emadl/IntegrationGluonTest.java
+++ src/test/java/de/monticore/lang/monticar/emadl/IntegrationGluonTest.java
@@ -20,8 +20,22 @@
  */
 package de.monticore.lang.monticar.emadl;
 
+import de.monticore.lang.monticar.emadl.generator.EMADLGeneratorCli;
+import de.se_rwth.commons.logging.Log;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
 public class IntegrationGluonTest extends IntegrationTest {
     public IntegrationGluonTest() {
         super("GLUON", "39253EC049D4A4E5FA0536AD34874B9D#1DBAEE1B1BD83FB7CB5F70AE91B29638#C4C23549E737A759721D6694C75D9771#5AF0CE68E408E8C1F000E49D72AC214A");
+    }
+
+    @Test
+    public void testMultipleInputs() {
+        Log.getFindings().clear();
+        String[] args = {"-m", "src/test/resources/models/", "-r", "MultipleInputs", "-b", "GLUON"};
+        EMADLGeneratorCli.main(args);
+        assertTrue(Log.getFindings().isEmpty());
     }
 }
