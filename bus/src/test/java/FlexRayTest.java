acd78064593c619a6eeb7f83ed4c96b3d98d26bb
Frederik <frederik.basels@rwth-aachen.de>
2019-06-04 21:30:44 +0200

    added FlexRay Test for dynamic and static segment

--- bus/src/test/java/FlexRayTest.java
+++ bus/src/test/java/FlexRayTest.java
@@ -21,14 +21,13 @@
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
-import sensors.CameraSensor;
-import sensors.CompassSensor;
-import sensors.SpeedSensor;
-import simulation.vehicle.ModelicaPhysicalVehicle;
+import commons.controller.commons.BusEntry;
 
 
 public class FlexRayTest {
@@ -45,22 +44,74 @@ public class FlexRayTest {
 		expected = (int)Math.ceil((262 * 8 * 1000)/(double)20);
 		assertEquals(expected, flexRay.getSlotSize());
 	}
-	
+
 	@Test
 	public void testCycleTime() {
-		List<Object> components = new ArrayList<Object>();
-		ModelicaPhysicalVehicle car = new ModelicaPhysicalVehicle();
-		CameraSensor cam = new CameraSensor(car);
-		CompassSensor com = new CompassSensor(car);
-		SpeedSensor speed = new SpeedSensor(car);
-		components.add(cam);
-		components.add(com);
-		components.add(speed);
 		FlexRay flexRay = new FlexRay();
-		long expected = flexRay.getSlotSize() * (3 + 4);
+		long expected = flexRay.getSlotSize() * 4;
 		assertEquals(expected, flexRay.getCycleTime());
-		
 	}
-	
-	
+
+	@Test
+	public void testFillStaticSegment()	{
+		FlexRay flexRay = new FlexRay();
+		List<BusMessage> list1 = new ArrayList<>();
+		List<BusMessage> list2 = new ArrayList<>();
+		List<BusMessage> list3 = new ArrayList<>();
+		Map<Integer, List<BusMessage>> map = new HashMap<>();
+		int data = 6;
+
+		//example data
+		BusMessage message11 = new BusMessage(data, 10, BusEntry.ACTUATOR_BRAKE, 0, 37);
+		BusMessage message12 = new BusMessage(data, 200, BusEntry.ACTUATOR_BRAKE, 0, 37);
+		list1.add(message11);
+		list1.add(message12);
+		map.put(message11.getControllerID(), list1);
+
+		BusMessage message21 = new BusMessage(data, 10, BusEntry.ACTUATOR_GEAR,0,39);
+		BusMessage message22 = new BusMessage(data, Integer.MAX_VALUE, BusEntry.ACTUATOR_GEAR, 0, 39);
+		list2.add(message21);
+		list2.add(message22);
+		map.put(message21.getControllerID(), list2);
+
+		BusMessage message31 = new BusMessage(data, Integer.MAX_VALUE, BusEntry.ACTUATOR_ENGINE, 0, 36);
+		BusMessage message32 = new BusMessage(data, 20, BusEntry.ACTUATOR_ENGINE, 0, 36);
+		list3.add(message31);
+		list3.add(message32);
+		map.put(message31.getControllerID(), list3);
+
+		//expected data
+		List<BusMessage> expected = new ArrayList<>();
+		expected.add(message11);
+		expected.add(message12);
+		expected.add(message21);
+
+		assertEquals(expected, flexRay.fillStaticSegment(map, flexRay.getCycleTime()));
+	}
+
+
+
+	@Test
+	public void testFillDynamicSegment(){
+		FlexRay flexRay = new FlexRay();
+		List<BusMessage> list = new ArrayList<>();
+		List<BusMessage> expected = new ArrayList<>();
+		int data = 6;
+
+		//example data
+		BusMessage message = new BusMessage(data, 500, BusEntry.ACTUATOR_ENGINE, 0, 36 );
+		BusMessage messageTwo = new BusMessage(data, 250, BusEntry.ACTUATOR_GEAR, 0, 39);
+		BusMessage messageThree = new BusMessage(data, 508, BusEntry.ACTUATOR_BRAKE, 0, 37);
+		list.add(message);
+		list.add(messageTwo);
+		list.add(messageThree);
+
+		//expected data
+		expected.add(message);
+		expected.add(messageTwo);
+
+		assertEquals(expected,flexRay.fillDynamicSegment(list, 4*flexRay.getSlotSize()));
+	}
+
+
 }
