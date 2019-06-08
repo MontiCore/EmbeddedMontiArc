8444b574416014d3448734528d03acd4bf188542
Nicola Gatto <nicola.gatto@rwth-aachen.de>
2019-06-08 15:57:34 +0200

    Remove Ignore from IntegrationCaffe2Test

--- src/test/java/de/monticore/lang/monticar/emadl/IntegrationCaffe2Test.java
+++ src/test/java/de/monticore/lang/monticar/emadl/IntegrationCaffe2Test.java
@@ -40,7 +40,6 @@ import java.util.List;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 
-@Ignore
 public class IntegrationCaffe2Test extends IntegrationTest {
     public IntegrationCaffe2Test() {
         super("CAFFE2", "39253EC049D4A4E5FA0536AD34874B9D#1DBAEE1B1BD83FB7CB5F70AE91B29638#13D139510DC5681639AA91D7250288D3#1A42D4842D0664937A9F6B727BD60CEF");
