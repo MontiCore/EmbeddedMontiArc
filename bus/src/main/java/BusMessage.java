1091cd520251204b4b5e45303b71c9f6fba2c370
Frederik <frederik.basels@rwth-aachen.de>
2019-06-18 11:32:07 +0200

    started working on EESimulator

--- bus/src/main/java/BusMessage.java
+++ bus/src/main/java/BusMessage.java
@@ -23,7 +23,7 @@ import commons.controller.commons.BusEntry;
  *  License along with this project. If not, see <http://www.gnu.org/licenses/>.
  * *******************************************************************************
  */
-public class BusMessage{
+public class BusMessage extends DiscreteEvent{
 
 	private Object message;
 
@@ -37,7 +37,7 @@ public class BusMessage{
     /**
      * Time the request to transmit the messages was placed. (in microseconds)
      */
-    private long requestTime;
+    //private long requestTime;
     
     private int controllerID;
     
@@ -46,24 +46,46 @@ public class BusMessage{
     private long finishTime;
     
     private boolean error;
+
+	/**
+	 * -----------Added Type, Target and Priority--------------
+	 * Changed constructor and getters/setters
+	 */
+	//private MessageType type;
+
+    //private EEComponent target;
+
+    private int priority;
+
+
+
     
 	/**
 	 * Random number generator to determine a bit error
 	 */
 	Random bitError = new Random();
     
-    public BusMessage(Object message, int messageLen, BusEntry messageID, int requestTime, int controllerID) {
+    public BusMessage(Object message, int messageLen, BusEntry messageID, int requestTime, int controllerID, int priority, MessageType type, EEComponent target) {
+    	super(type, target, requestTime);
     	this.message = message;
     	this.messageLen = messageLen;
     	this.messageID = messageID;
-    	this.requestTime = requestTime;
     	this.controllerID = controllerID;
+    	this.priority = priority;
     	this.transmittedBytes = 0;
     	this.transmitted = false;
     	this.finishTime = -1l;
     	this.error = false;
     }
     
+
+    public int getPriority(){return priority;}
+
+	//public MessageType getType() { return type;	}
+
+	//public void setType(MessageType type) { this.type = type;	}
+
+	//public EEComponent getTarget() { return target;	}
 
 	public long getFinishTime() {
 		return finishTime;
@@ -90,13 +112,9 @@ public class BusMessage{
 		this.messageLen = messageLen;
 	}
 
-	public long getRequestTime() {
-		return requestTime;
-	}
+	//public long getRequestTime() { return requestTime;	}
 
-	public void setRequestTime(long requestTime) {
-		this.requestTime = requestTime;
-	}
+	//public void setRequestTime(long requestTime) { this.requestTime = requestTime;	}
 
 	public int getControllerID() {
 		return controllerID;
