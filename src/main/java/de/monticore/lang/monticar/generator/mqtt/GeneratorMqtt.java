98bf8658b39b7c66d23fc91d0e0d45d906e12dd8
Georg Vinogradov <georg.vinogradov@rwth-aachen.de>
2019-06-21 11:19:56 +0200

    Start implementing classes/ftl files

--- src/main/java/de/monticore/lang/monticar/generator/mqtt/GeneratorMqtt.java
+++ src/main/java/de/monticore/lang/monticar/generator/mqtt/GeneratorMqtt.java
@@ -1,7 +1,48 @@
 package de.monticore.lang.monticar.generator.mqtt;
+import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.instanceStructure.EMAComponentInstanceSymbol;
+import de.monticore.lang.tagging._symboltable.TaggingResolver;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class GeneratorMqtt {
+
+	private String generationTargetPath;
+    private boolean generateCMake = false;
+    private boolean mqtt2Mode = false;
+
+    public boolean isMqtt2Mode() {
+        return mqtt2Mode;
+    }
+    
+    public void setMqtt2Mode(boolean mqtt2Mode) {
+        this.mqtt2Mode = mqtt2Mode;
+    }
+
+    public void setGenerateCMake(boolean generateCMake) {
+        this.generateCMake = generateCMake;
+    }
+
+    public String getGenerationTargetPath() {
+        return generationTargetPath;
+    }
+
+    public void setGenerationTargetPath(String generationTargetPath) {
+        this.generationTargetPath = generationTargetPath.endsWith("/") ? generationTargetPath : generationTargetPath + "/";
+    }
+
+    
+    // Those methods use ROS specific fileContent and need to be rewritten
+    public List<File> generateFiles(EMAComponentInstanceSymbol component, TaggingResolver symtab) throws IOException {
+        return null;
+    }
+    
+    // public List<FileContent> generateStrings(EMAComponentInstanceSymbol component) {}
+
+    // private List<FileContent> generateMqttAdapter(EMAComponentInstanceSymbol component) {}
+
+    // public List<FileContent> generateCMakeFiles(EMAComponentInstanceSymbol component, List<MqttInterface> mqttInterfaces, boolean mqtt2Mode) {}
 
     
 }
--- src/main/java/de/monticore/lang/monticar/generator/mqtt/template/MqttTemplates.java
+++ src/main/java/de/monticore/lang/monticar/generator/mqtt/template/MqttTemplates.java
@@ -1,7 +1,63 @@
 package de.monticore.lang.monticar.generator.mqtt.template;
 
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+import java.io.IOException;
+import de.se_rwth.commons.logging.Log;
+import java.io.StringWriter;
+import java.util.Map;
+import java.util.HashMap;
+
 public class MqttTemplates {
 
+	private static final Template Mqtt_CMAKELISTS;
+    private static final Template Mqtt_ADAPTER;
+
+    // Choosing ftl files for templates
+    static {
+        Configuration conf = new Configuration(Configuration.VERSION_2_3_23);
+        conf.setDefaultEncoding("UTF-8");
+        conf.setTemplateExceptionHandler(TemplateExceptionHandler.DEBUG_HANDLER);
+        conf.setLogTemplateExceptions(false);
+        conf.setClassForTemplateLoading(MqttTemplates.class, "");
+        try {
+        	Mqtt_CMAKELISTS = conf.getTemplate("CMakeLists.ftl");
+        	Mqtt_ADAPTER = conf.getTemplate("Adapter.ftl");
+        } catch (IOException e) {
+            String msg = "could not load template";
+            Log.error(msg, e);
+            throw new RuntimeException(msg, e);
+        }
+    }
     
+    /*public static String generateMqttCMakeLists(MqttCMakeListsModel model) {
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("model", model);
+        return generate(Mqtt_CMAKELISTS, data);
+    }*/
+    
+    /*public static String generateMqttAdapter(MqttAdapterModel model) {
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("model", model);
+        return generate(Mqtt_ADAPTER, data);
+    }*/
+    
+    private static String generate(Template template, Map dataForTemplate) {
+        Log.errorIfNull(template);
+        Log.errorIfNull(dataForTemplate);
+        StringWriter sw = new StringWriter();
+        try {
+            template.process(dataForTemplate, sw);
+        } catch (TemplateException | IOException e) {
+            Log.error("template generation failed, template: " + template.getName(), e);
+        }
+        return sw.toString();
+    }
+
+
+
 }
