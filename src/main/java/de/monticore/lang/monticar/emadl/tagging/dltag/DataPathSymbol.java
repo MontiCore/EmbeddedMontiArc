a4e5aa30025541415455fa93e0ce233e61f9c3ba
abdallah <abdallah.atouani@rwth-aachen.de>
2019-06-25 13:59:54 +0200

    added the tag symbol DataPathSymbol and the corresponding creator.

---
+++ src/main/java/de/monticore/lang/monticar/emadl/tagging/dltag/DataPathSymbol.java
@@ -0,0 +1,52 @@
+/**
+ *
+ *  ******************************************************************************
+ *  MontiCAR Modeling Family, www.se-rwth.de
+ *  Copyright (c) 2017, Software Engineering Group at RWTH Aachen,
+ *  All rights reserved.
+ *
+ *  This project is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3.0 of the License, or (at your option) any later version.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this project. If not, see <http://www.gnu.org/licenses/>.
+ * *******************************************************************************
+ */
+package de.monticore.lang.monticar.emadl.tagging.dltag;
+
+import de.monticore.lang.tagging._symboltable.TagKind;
+import de.monticore.lang.tagging._symboltable.TagSymbol;
+
+public class DataPathSymbol extends TagSymbol {
+    public static final DataPathKind KIND  = DataPathKind.INSTANCE;
+
+    public DataPathSymbol() {
+        super(KIND, ".");
+    }
+
+    public DataPathSymbol(String path, String type) {
+        this(KIND, path, type);
+    }
+
+    public DataPathSymbol(DataPathKind kind, String path, String type) {
+        super(kind, path, type);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static class DataPathKind extends TagKind {
+        public static final DataPathKind INSTANCE = new DataPathKind();
+
+        protected DataPathKind() {
+        }
+    }
+}
