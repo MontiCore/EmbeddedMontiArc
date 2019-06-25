ba79cd2bdd932f109e98391a6800ff399af3b7d7
jean-marc.ronck <jean-marc.ronck@rwth-aachen.de>
2019-06-25 23:15:03 +0200

    Added logging for "initialize" and "configure".

--- runtime/language/src/main/java/de/monticore/lang/monticar/borealis/runtime/language/ls/AbstractServerLauncher.java
+++ runtime/language/src/main/java/de/monticore/lang/monticar/borealis/runtime/language/ls/AbstractServerLauncher.java
@@ -1,16 +1,22 @@
 package de.monticore.lang.monticar.borealis.runtime.language.ls;
 
+import com.google.common.flogger.FluentLogger;
+
 import java.util.Set;
 
 public abstract class AbstractServerLauncher implements ServerLauncher {
+    protected final FluentLogger logger;
     protected final Set<ServerLauncherContribution> contributions;
 
     protected AbstractServerLauncher(Set<ServerLauncherContribution> contributions) {
+        this.logger = FluentLogger.forEnclosingClass();
         this.contributions = contributions;
     }
 
     @Override
     public void initialize(String[] arguments) throws Exception {
+        this.logger.atInfo().log("Initializing contributions...");
+
         for (ServerLauncherContribution contribution : this.contributions) {
             contribution.onInitialize(arguments);
         }
@@ -18,6 +24,8 @@ public abstract class AbstractServerLauncher implements ServerLauncher {
 
     @Override
     public void configure(String[] arguments) throws Exception {
+        this.logger.atInfo().log("Configuring contributions...");
+
         for (ServerLauncherContribution contribution : this.contributions) {
             contribution.onConfigure(arguments);
         }
