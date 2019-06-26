269743238d5cdc2ca78cc061d67967adeb2c2dd1
jean-marc.ronck <jean-marc.ronck@rwth-aachen.de>
2019-06-26 22:02:34 +0200

    Added "GeneratorPhase" interface.

---
+++ generators/common/src/main/java/de/monticore/lang/monticar/sol/generators/common/GeneratorPhase.java
@@ -0,0 +1,19 @@
+package de.monticore.lang.monticar.sol.generators.common;
+
+public interface GeneratorPhase {
+    /**
+     * @return A label from which a developer can identify the phase.
+     */
+    String getLabel();
+
+    /**
+     * @return At what position this phase should be executed. Higher means sooner.
+     */
+    int getPriority();
+
+    /**
+     * Triggers the execution of this generation phase.
+     * @throws Exception An exception which might occur during this generation phase.
+     */
+    void execute() throws Exception;
+}
