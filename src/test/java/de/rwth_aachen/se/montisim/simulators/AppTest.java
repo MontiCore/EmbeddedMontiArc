6a12c9c837c7fb31b17b62e3972d7bbcca1e57b8
Jean Meurice <jean.meurice@rwth-aachen.de>
2019-07-03 18:20:24 +0200

    Update AppTest.java

--- src/test/java/de/rwth_aachen/se/montisim/simulators/AppTest.java
+++ src/test/java/de/rwth_aachen/se/montisim/simulators/AppTest.java
@@ -16,7 +16,8 @@ public class AppTest
     @Test
     public void basicSetupTest() throws Exception {
 
-        FileSystem fileSystem = new FileSystem(LibraryService.getWorkingDirectory());
+        //FileSystem fileSystem = new FileSystem(LibraryService.getWorkingDirectory());
+        FileSystem fileSystem = new FileSystem(System.getProperty("user.dir") + "/");
         BasicController sim_controller = new BasicController(fileSystem);
         sim_controller.initFromJsonScenario(FileSystem.getJson(fileSystem.getPath(Category.CategoryType.SCENARIOS.id, "straight.json")));
         sim_controller.startSimulation();
