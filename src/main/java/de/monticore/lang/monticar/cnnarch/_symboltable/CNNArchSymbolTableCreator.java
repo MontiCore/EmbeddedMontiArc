e457babefc16ffa4a810ff4dd5540ecb8a255129
Sebastian Nickels <sebastian.nickels@rwth-aachen.de>
2019-07-07 00:32:34 +0200

    Removed unroll related stuff for merge

--- src/main/java/de/monticore/lang/monticar/cnnarch/_symboltable/CNNArchSymbolTableCreator.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/_symboltable/CNNArchSymbolTableCreator.java
@@ -242,28 +242,6 @@ public class CNNArchSymbolTableCreator extends de.monticore.symboltable.CommonSy
     }
 
     @Override
-    public void visit(ASTUnrollDeclaration ast) {
-        UnrollDeclarationSymbol unrollDeclaration = new UnrollDeclarationSymbol(ast.getName());
-        addToScopeAndLinkWithNode(unrollDeclaration, ast);
-    }
-
-    @Override
-    public void endVisit(ASTUnrollDeclaration ast) {
-        UnrollDeclarationSymbol unrollDeclaration = (UnrollDeclarationSymbol) ast.getSymbolOpt().get();
-        unrollDeclaration.setBody((SerialCompositeElementSymbol) ast.getBody().getSymbolOpt().get());
-
-        List<VariableSymbol> parameters = new ArrayList<>(4);
-        for (ASTLayerParameter astParam : ast.getParametersList()){
-            VariableSymbol parameter = (VariableSymbol) astParam.getSymbolOpt().get();
-            parameters.add(parameter);
-        }
-        unrollDeclaration.setParameters(parameters);
-
-
-        removeCurrentScope();
-    }
-
-    @Override
     public void visit(ASTLayerParameter ast) {
         VariableSymbol variable = new VariableSymbol(ast.getName());
         variable.setType(VariableType.LAYER_PARAMETER);
@@ -337,26 +315,6 @@ public class CNNArchSymbolTableCreator extends de.monticore.symboltable.CommonSy
             elements.add(serialElements);
         }
         sym.setElements(elements);
-    }
-
-    @Override
-    public void visit(ASTUnroll ast) {
-        UnrollSymbol layer = new UnrollSymbol("BeamSearchStart");
-        addToScopeAndLinkWithNode(layer, ast);
-    }
-
-    @Override
-    public void endVisit(ASTUnroll ast) {
-        UnrollSymbol layer = (UnrollSymbol) ast.getSymbolOpt().get();
-
-        List<ArgumentSymbol> arguments = new ArrayList<>(6);
-        for (ASTArchArgument astArgument : ast.getArgumentsList()){
-            Optional<ArgumentSymbol> optArgument = astArgument.getSymbolOpt().map(e -> (ArgumentSymbol)e);
-            optArgument.ifPresent(arguments::add);
-        }
-        layer.setArguments(arguments);
-
-        removeCurrentScope();
     }
 
     @Override
