59b43793de79a49e5045e6cfba95c99800779ea6
Lennart Bucher <lennart.bucher@rwth-aachen.de>
2019-07-12 14:12:41 +0200

    add Dockerfile for carla-ros-bridge

---
+++ docker/build2.sh
@@ -0,0 +1,2 @@
+curDir=$(readlink -f `dirname $0`)
+docker build -t registry.git.rwth-aachen.de/monticore/embeddedmontiarc/applications/carlacomponents/emam-carla-ros-bridge:latest -f Dockerfile ./.. "$@"
---
+++ docker/push.sh
@@ -0,0 +1,1 @@
+docker push registry.git.rwth-aachen.de/monticore/embeddedmontiarc/applications/carlacomponents/emam-carla-ros-bridge:latest
