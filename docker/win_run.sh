35383fa82474a5a2fcd5cf4af8a0b3ea735ec5ec
Philipp Helios <philipp.a.t.helios@gmail.com>
2019-07-13 10:06:57 +0200

    windows scripts seem to work now... you still have to edit the ip manually

--- docker/win_run.sh
+++ docker/win_run.sh
@@ -2,6 +2,6 @@ winpty docker run \
 -it \
 --rm \
 --name emam2carla \
---mount type=bind,source=//c/Users/SharedFolder/carlacomponents,target=/usr/src/emam2carla \
+--mount type=bind,source=//c/Users/Public/Documents/SharedFolder/carlacomponents,target=/usr/src/emam2carla \
 registry.git.rwth-aachen.de/monticore/embeddedmontiarc/applications/carlacomponents/emam-carla-ros-bridge \
 bash
