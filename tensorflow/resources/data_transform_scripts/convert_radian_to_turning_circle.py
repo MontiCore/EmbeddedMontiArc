333da3cf0cd6e6f7891b118c1fff4a16c367bbda
Felix Hermsen <felix.hermsen@rwth-aachen.de>
2019-07-23 22:50:05 +0200

    normalization of turining cirle

--- tensorflow/resources/data_transform_scripts/convert_radian_to_turning_circle.py
+++ tensorflow/resources/data_transform_scripts/convert_radian_to_turning_circle.py
@@ -12,13 +12,30 @@ for element in steering_reader:
 #convert angle in degree to turning circle
 #https://de.wikipedia.org/wiki/Wendekreis_(Fahrzeug)
 steering[0][1]="turning_circle"
-for row in steering[1:]:	
+for row in steering[1:]:
 	steering_angle=float(row[1])
 	if(steering_angle==0):
-		turning_circle=32000
+                #magic number
+		turning_circle=0
 	else:
-		turning_circle= (2*wheelbase)/ math.sin(steering_angle)
+		turning_circle= (2*wheelbase)/ math.sin(steering_angle/4)
 	row[1]=turning_circle
+
+max_element = max(abs(element[1]) for element in steering[1:])
+
+
+#normalize
+for row in steering[1:]:
+    if row[1] == 0:
+        row[1] = 1.0
+    else:
+        row[1] = row[1]/max_element
+
+#max_element = max((element[1]) for element in steering[1:])
+#min_element = min((element[1]) for element in steering[1:])
+
+#print max_element
+#print min_element
 
 #Write csv file
 wtr = csv.writer(open ('../test_set/steering_turning_circle.csv', 'w'), delimiter=',', lineterminator='\n')
