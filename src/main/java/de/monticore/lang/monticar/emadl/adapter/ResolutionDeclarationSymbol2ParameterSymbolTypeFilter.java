ce278b427d84c9b0ad9e6743c04a33fbc81f6cec
Sebastian Nickels <sn1c@protonmail.ch>
2019-08-12 01:59:22 +0200

    Renamed VariableSymbol to ParameterSymbol, introduced layer variable declarations and changed IOSymbol to VariableSymbol which now combines IO variables and layer variables

--- src/main/java/de/monticore/lang/monticar/emadl/adapter/ResolutionDeclarationSymbol2ParameterSymbolTypeFilter.java
+++ src/main/java/de/monticore/lang/monticar/emadl/adapter/ResolutionDeclarationSymbol2ParameterSymbolTypeFilter.java
@@ -20,25 +20,25 @@
  */
 package de.monticore.lang.monticar.emadl.adapter;
 
-import de.monticore.lang.monticar.cnnarch._symboltable.VariableSymbol;
+import de.monticore.lang.monticar.cnnarch._symboltable.ParameterSymbol;
 import de.monticore.lang.monticar.si._symboltable.ResolutionDeclarationSymbol;
 import de.monticore.lang.monticar.resolution._ast.ASTUnitNumberResolution;
 import de.monticore.symboltable.Symbol;
 import de.monticore.symboltable.resolving.TransitiveAdaptedResolvingFilter;
 
-public class ResolutionDeclarationSymbol2VariableSymbolTypeFilter extends TransitiveAdaptedResolvingFilter<VariableSymbol> {
+public class ResolutionDeclarationSymbol2ParameterSymbolTypeFilter extends TransitiveAdaptedResolvingFilter<ParameterSymbol> {
 
-    public ResolutionDeclarationSymbol2VariableSymbolTypeFilter() {
+    public ResolutionDeclarationSymbol2ParameterSymbolTypeFilter() {
         super(ResolutionDeclarationSymbol.KIND,
-                VariableSymbol.class,
-                VariableSymbol.KIND);
+                ParameterSymbol.class,
+                ParameterSymbol.KIND);
     }
 
     @Override
     public Symbol translate(Symbol adaptee) {
         assert adaptee instanceof ResolutionDeclarationSymbol;
         if (((ResolutionDeclarationSymbol) adaptee).getASTResolution() instanceof ASTUnitNumberResolution){
-            return new ResolutionDeclarationSymbol2VariableSymbol((ResolutionDeclarationSymbol) adaptee, ((ASTUnitNumberResolution) ((ResolutionDeclarationSymbol) adaptee).getASTResolution()));
+            return new ResolutionDeclarationSymbol2ParameterSymbol((ResolutionDeclarationSymbol) adaptee, ((ASTUnitNumberResolution) ((ResolutionDeclarationSymbol) adaptee).getASTResolution()));
         }
         return null;
     }
