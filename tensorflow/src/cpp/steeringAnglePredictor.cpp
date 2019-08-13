b8a24be578749f3d2bbe2585082f1fd26b05763a
Jonas <jonas.eckhardt@rwth-aachen.de>
2019-08-13 17:59:42 +0200

    Predictor fixed part2

--- tensorflow/src/cpp/steeringAnglePredictor.cpp
+++ tensorflow/src/cpp/steeringAnglePredictor.cpp
@@ -1,6 +1,8 @@
 #include <opencv2/core.hpp>
 #include <opencv2/hdf.hpp>
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "CNNPredictor_endtoend_nvidia.h"
 
 
@@ -12,6 +14,7 @@ inline const char * const BoolToString(bool b);
 
 int main(int argc,char* argv[])
 {
+    //Get information
     if(argc<2){
         cout << "Please give name of h5 file as an argument." << endl;
         return 1;
@@ -23,53 +26,53 @@ int main(int argc,char* argv[])
     cout << "Reads from "+filename << endl;
     Ptr<hdf::HDF5> h5io =hdf::open( filename );
 
-    cout << "Data exists: "<< BoolToString(h5io->hlexists("data")) << endl;
-    //cout << "Softmax_label exists: " << BoolToString(h5io->hlexists("softmax_label")) << endl;
+    cout << "Data exists: "<< h5io->hlexists("data") << endl;   
     
-    std::vector<int> dims;
-    dims = h5io->dsgetsize("data", hdf::HDF5::H5_GETDIMS);    
-    cout << "Dimension of data is: ";
-    cout << dims.at(0) << ",";
-    cout << dims.at(1) << ",";
-    cout << dims.at(2) << ",";
-    cout << dims.at(3) << endl;
-
-
-    //Read data from h5 file
-    Mat M;
-    h5io->dsread(M, "data");
-   
+    
+    //Dimensions for debugging
+    std::vector<int> dims = h5io->dsgetsize("data", hdf::HDF5::H5_GETDIMS);
     int num_pictures=dims.at(0);
     int width=dims.at(3);
     int height=dims.at(2);
     int num_channels=dims.at(1);
-    vector<float> prediction[num_pictures];
+    
+    cout << "Dimension of data is: "; 
+    cout << "N" << num_pictures;
+    cout << "C" << num_channels;
+    cout << "H" << height;
+    cout << "W" << width<< endl;
+    
+    
+    //Read data from h5 file
+    Mat M;
+    h5io->dsread(M, "data");
+    
+    //Open file
+    ofstream myfile;
+    myfile.open ("example.csv");
     
     //Convert to vector and call predictor
-    for(int n=0;n<num_pictures;n++){
-        vector<float> data(num_channels*height*width);
-        prediction[n]=vector<float> (1,42);
-        Mat img(height,width,CV_32F, M.ptr<int>(n));
-        for(int i=0;i<height;i++){
-            for(int j=0;j<width;j++){
-                for(int k=0;k<num_channels;k++){
-                    data[k*height*width + i*height + j] = (float) img.at<int>(i,j);
-                }
-            }
+    vector<float> prediction[num_pictures];
+    vector<float> data(num_channels*height*width);
+    CNNPredictor_endtoend_nvidia_0 predictor;
+    
+    for(int n=0;n<num_pictures;n++){    
+        for(int i=0;i<width*height*num_channels;i++){
+            data[i]=M.at<float>(n*height*width*num_channels+i);
         }
         
         //Prediction
-        CNNPredictor_endtoend_nvidia_0 predictor;
+        prediction[n]=vector<float> (1,42);
         predictor.predict(data,prediction[n]);
         cout << prediction[n].at(0) <<endl;
-    }
-    
-       
+        if(n<num_pictures-1){
+            myfile << prediction[n].at(0) << ",";
+        }
+    }    
+    //Close everything
     h5io->close();
+    myfile << prediction[num_pictures-1].at(0);
+    myfile.close();
+    
     return 0;
-}
-
-inline const char * const BoolToString(bool b)
-{
-  return b ? "true" : "false";
 }
