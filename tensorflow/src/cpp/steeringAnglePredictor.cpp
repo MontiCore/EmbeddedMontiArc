aef06144df9c359583b84b993f5f782d0317726b
Jonas <jonas.eckhardt@rwth-aachen.de>
2019-08-13 18:30:10 +0200

    Refactoring

--- tensorflow/src/cpp/steeringAnglePredictor.cpp
+++ tensorflow/src/cpp/steeringAnglePredictor.cpp
@@ -2,77 +2,64 @@
 #include <opencv2/hdf.hpp>
 #include <iostream>
 #include <fstream>
-#include <string>
 #include "CNNPredictor_endtoend_nvidia.h"
-
 
 using namespace std;
 using namespace cv;
 
-
-inline const char * const BoolToString(bool b);
-
 int main(int argc,char* argv[])
 {
-    //Get information
-    if(argc<2){
-        cout << "Please give name of h5 file as an argument." << endl;
+    //Parse arguments
+    if(argc<3){
+        cout << "Please give name of h5 file and output filename as arguments." << endl;
         return 1;
     }
-    
     String filename = argv[1];
-    
-    //Debug information
-    cout << "Reads from "+filename << endl;
-    Ptr<hdf::HDF5> h5io =hdf::open( filename );
+    String output_filename = argv[2];
+    cout << "Reads from " << filename << endl;
 
-    cout << "Data exists: "<< h5io->hlexists("data") << endl;   
-    
-    
-    //Dimensions for debugging
+    //Initialize variables
+    Ptr<hdf::HDF5> h5io =hdf::open( filename );
     std::vector<int> dims = h5io->dsgetsize("data", hdf::HDF5::H5_GETDIMS);
     int num_pictures=dims.at(0);
-    int width=dims.at(3);
-    int height=dims.at(2);
     int num_channels=dims.at(1);
-    
-    cout << "Dimension of data is: "; 
-    cout << "N" << num_pictures;
-    cout << "C" << num_channels;
-    cout << "H" << height;
-    cout << "W" << width<< endl;
-    
-    
+    int height=dims.at(2);
+    int width=dims.at(3);
+    vector<float> prediction;
+    vector<float> data(num_channels*height*width);
+    CNNPredictor_endtoend_nvidia_0 predictor;
+    int picture_offset=width*height*num_channels;
+
+    //Debug information
+    cout << "Dimension of data is: "
+         << "N" << num_pictures
+         << "C" << num_channels
+         << "H" << height
+         << "W" << width << endl;
+
     //Read data from h5 file
     Mat M;
     h5io->dsread(M, "data");
-    
-    //Open file
-    ofstream myfile;
-    myfile.open ("example.csv");
-    
-    //Convert to vector and call predictor
-    vector<float> prediction[num_pictures];
-    vector<float> data(num_channels*height*width);
-    CNNPredictor_endtoend_nvidia_0 predictor;
-    
-    for(int n=0;n<num_pictures;n++){    
-        for(int i=0;i<width*height*num_channels;i++){
-            data[i]=M.at<float>(n*height*width*num_channels+i);
-        }
-        
+
+    //Open output file
+    ofstream output_file;
+    output_file.open (output_filename);
+
+    for(int n=0;n<num_pictures;n++){
+        //Convert picture to flat vector
+        for(int i=0;i<picture_offset;i++)
+            data[i]=M.at<float>(n*picture_offset+i);
         //Prediction
-        prediction[n]=vector<float> (1,42);
-        predictor.predict(data,prediction[n]);
-        cout << prediction[n].at(0) <<endl;
-        if(n<num_pictures-1){
-            myfile << prediction[n].at(0) << ",";
-        }
-    }    
+        predictor.predict(data,prediction);
+        cout << prediction.at(0) <<endl;
+        if(n<num_pictures-1)
+            output_file << prediction.at(0) << ",";
+    }
+
     //Close everything
     h5io->close();
-    myfile << prediction[num_pictures-1].at(0);
-    myfile.close();
-    
+    output_file << prediction.at(0);
+    output_file.close();
+
     return 0;
 }
