7ccaa87463c0dc44a3daac8ca45d8a8d1f3c443c
Nicola Gatto <nicola.gatto@rwth-aachen.de>
2019-08-14 13:11:48 +0200

    Move repository

---
+++ run_training.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d logs ]
+then
+    mkdir logs
+fi
+
+# Start the training
+cd "target/agent/src/lander_agent_master/cpp"
+python CNNTrainer_lander_agent_master_dqn.py
+
+cp -r ./model/* ${PROJECT_ROOT}/logs/
+cp -r ./model ${PROJECT_ROOT}/target/bin
+cd "${PROJECT_ROOT}"
