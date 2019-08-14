a01e2efd98a8ce90d5a5b588383945ba670c7d62
Nicola Gatto <nicola.gatto@rwth-aachen.de>
2019-08-14 12:06:26 +0200

    Move repository

---
+++ run_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+. config.sh
+
+echo "Start ROSCORE..."
+xterm -title "ROSCORE" -e "roscore; bash" &
+sleep 2
+
+echo "Start up environment..." 
+xterm -title "Gym-Environment" -e "python bin/ros-gym/launcher.py --environment \"Pong-v0\" --quiet; bash" &
+sleep 2
+
+echo "Start up preprocessor..."
+xterm -title "Preprocessor" -e "${BINARY}/preprocessor; bash" &
+sleep 2
