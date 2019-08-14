d9ca97038716dc47c7e4d496f2ec325dc844d021
Nicola Gatto <nicola.gatto@rwth-aachen.de>
2019-08-14 13:12:19 +0200

    Move repository

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ ! -d "target" ];
+then
+    echo "No target folder found..."
+    echo "Generate project..."
+    rm -rf target
+    java -jar bin/embedded-montiarc-math-middleware-generator-0.0.26-SNAPSHOT-jar-with-dependencies.jar config.json
+fi
+echo "Target folder found!"
+echo "Build project..."
+rm -rf target/build
+rm -rf target/bin
+cmake -Btarget/build -Htarget/src
+make -Ctarget/build
+
+mkdir target/bin
+cp target/build/lander_master/coordinator/Coordinator_lander_master target/bin/agent
