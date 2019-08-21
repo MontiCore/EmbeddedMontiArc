ae56205cb0c19ca0b391edf94520b0033167f45b
kusmenko <kusmenko@se.rwth-aachen.de>
2019-08-21 15:43:05 +0200

    debug

--- python/ema-listall/ema.py
+++ python/ema-listall/ema.py
@@ -3,10 +3,10 @@ def print_projects(group, abs):
   subgroups = group.subgroups
   
   abspath = abs + group.path +"/"
-  for g in subgroups.list():
-    print(abspath + g.path + "    " + str(g.id))
+  for g in subgroups.list(all=True):
     current_group = gl.groups.get(g.id)  
-    for p in current_group.projects.list():
+    print(abspath + g.path + "    " + str(g.id) + "    (" +str(len(current_group.projects.list(all=True))) + " projects)")
+    for p in current_group.projects.list(all=True):
       print(abspath + g.path +"/"+ p.path +"    " + str(p.id))
     print_projects(current_group, abspath)
 
