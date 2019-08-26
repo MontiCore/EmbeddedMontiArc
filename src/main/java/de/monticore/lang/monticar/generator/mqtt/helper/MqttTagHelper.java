a0c740de87e535c6652b7cb7ac1a0643e289d414
Mike Grüne <mikegruene@pop-os.localdomain>
2019-08-26 17:56:24 +0200

    Fix MqttTagHelper

--- src/main/java/de/monticore/lang/monticar/generator/mqtt/helper/MqttTagHelper.java
+++ src/main/java/de/monticore/lang/monticar/generator/mqtt/helper/MqttTagHelper.java
@@ -4,15 +4,10 @@ package de.monticore.lang.monticar.generator.mqtt.helper;
 import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.cncModel.EMAPortSymbol;
 import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.instanceStructure.EMAComponentInstanceSymbol;
 import de.monticore.lang.embeddedmontiarc.tagging.middleware.mqtt.MqttConnectionSymbol;
-import de.monticore.lang.monticar.generator.mqtt.GeneratorMqtt;
 import de.monticore.lang.tagging._symboltable.TagSymbol;
 import de.monticore.lang.tagging._symboltable.TaggingResolver;
-import de.se_rwth.commons.logging.Log;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MqttTagHelper {
     private MqttTagHelper() {
@@ -26,7 +21,7 @@ public class MqttTagHelper {
                     mqttConnectionSymbols.put(p, (MqttConnectionSymbol) tmpTags.iterator().next());
                 }
             });
-            componentInstanceSymbol.getSubComponents().forEach(sc -> mqttConnectionSymbols.putAll(TagHelper.resolveTags(taggingResolver, sc)));
+            componentInstanceSymbol.getSubComponents().forEach(sc -> mqttConnectionSymbols.putAll(MqttTagHelper.resolveTags(taggingResolver, sc)));
         return mqttConnectionSymbols;
     }
 }
