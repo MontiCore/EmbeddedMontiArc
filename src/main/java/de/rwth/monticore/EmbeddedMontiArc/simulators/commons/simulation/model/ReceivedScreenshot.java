cdfc865ef22b6852e30db08babaa44b418d8d2ec
Jean Meurice <jean.meurice@rwth-aachen.de>
2019-08-27 15:57:38 +0200

    License Update - Delete commons-1.0.9 artifact from nexus

--- src/main/java/de/rwth/monticore/EmbeddedMontiArc/simulators/commons/simulation/model/ReceivedScreenshot.java
+++ src/main/java/de/rwth/monticore/EmbeddedMontiArc/simulators/commons/simulation/model/ReceivedScreenshot.java
@@ -1,4 +1,9 @@
-/* (c) https://github.com/MontiCore/monticore */
+/**
+ * (c) https://github.com/MontiCore/monticore
+ *
+ * The license generally applicable for this project
+ * can be found under https://github.com/MontiCore/monticore.
+ */
 package de.rwth.monticore.EmbeddedMontiArc.simulators.commons.simulation.model;
 
 /**
