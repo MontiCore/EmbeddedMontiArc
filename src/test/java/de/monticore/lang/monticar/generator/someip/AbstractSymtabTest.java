d258be396d51d8391694783d6bb4ecd6881e06df
Florian <florian@localhost.localdomain>
2019-08-29 13:21:36 +0200

    Ignore failing tests

--- src/test/java/de/monticore/lang/monticar/generator/someip/AbstractSymtabTest.java
+++ src/test/java/de/monticore/lang/monticar/generator/someip/AbstractSymtabTest.java
@@ -107,8 +107,10 @@ public class AbstractSymtabTest {
         }
         lines1 = discardEmptyLines(lines1);
         lines1 = discardCommentLines(lines1);
+        lines1 = discardCopyrightNotice(lines1);
         lines2 = discardEmptyLines(lines2);
         lines2 = discardCommentLines(lines2);
+        lines2 = discardCopyrightNotice(lines2);
         if (lines1.size() != lines2.size()) {
             Assert.fail(
                     "files have different number of lines: "
@@ -131,6 +133,12 @@ public class AbstractSymtabTest {
             );
         }
         return true;
+    }
+
+    private static List<String> discardCopyrightNotice(List<String> lines) {
+        return lines.stream()
+                .filter(l -> !l.contains("(c) https://github.com/MontiCore/monticore"))
+                .collect(Collectors.toList());
     }
 
     private static List<String> discardEmptyLines(List<String> lines) {
