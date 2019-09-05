15a612d3701c0fccfaf0ac25c14ddebb874449ef
Sebastian Nickels <sn1c@protonmail.ch>
2019-09-05 17:19:06 +0200

    Added support for unrolls

--- src/main/java/de/monticore/lang/monticar/cnnarch/gluongenerator/CNNArch2GluonArchitectureSupportChecker.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/gluongenerator/CNNArch2GluonArchitectureSupportChecker.java
@@ -42,4 +42,9 @@ public class CNNArch2GluonArchitectureSupportChecker extends ArchitectureSupport
         return true;
     }
 
+    @Override
+    protected boolean checkUnroll(ArchitectureSymbol architecture) {
+        return true;
+    }
+
 }
