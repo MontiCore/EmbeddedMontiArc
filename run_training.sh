c696489e43b3ea9c56684505f2d6bc74ac0ab449
Nicola Gatto <nicola.gatto@rwth-aachen.de>
2019-09-17 15:24:36 +0200

    Ci pipeline

--- run_training.sh
+++ run_training.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 # (c) https://github.com/MontiCore/monticore  
 
-if [ ! -d logs ]
-then
-    mkdir logs
-fi
+. config.sh
 
 # Start the training
 cd "target/agent/src/atari_agent_master/cpp"
 python CNNTrainer_atari_agent_master_dqn.py
 
-cp -r ./model/* ${PROJECT_ROOT}/logs/
-cp -r ./model ${PROJECT_ROOT}/target/bin
+cp -r ./model ${PROJECT_ROOT}/${BINARY}
 cd "${PROJECT_ROOT}"
