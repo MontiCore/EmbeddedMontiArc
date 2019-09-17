22748e3ba25997e1c1469f42d815bc59bf31d0f1
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2019-09-17 15:30:38 +0200

    Added DebugLog config, compact daimler model

---
+++ src/main/java/de/se_rwth/commons/logging/DebugLog.java
@@ -0,0 +1,16 @@
+package de.se_rwth.commons.logging;
+
+public class DebugLog extends Log {
+
+    public static void init() {
+        Log debugLog = new DebugLog();
+        Log.setLog(debugLog);
+    }
+
+    @Override
+    protected boolean doIsDebugEnabled(String logName) {
+        return true;
+    }
+
+
+}
