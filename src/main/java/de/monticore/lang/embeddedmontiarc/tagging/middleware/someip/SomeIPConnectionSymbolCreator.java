16b827bb0d63c6ec1fbc5d92e54752562fd74b4a
markus.bendel <markus.bendel@rwth-aachen.de>
2019-09-20 18:14:24 +0200

    Change reg expression

--- src/main/java/de/monticore/lang/embeddedmontiarc/tagging/middleware/someip/SomeIPConnectionSymbolCreator.java
+++ src/main/java/de/monticore/lang/embeddedmontiarc/tagging/middleware/someip/SomeIPConnectionSymbolCreator.java
@@ -22,13 +22,13 @@ public class SomeIPConnectionSymbolCreator implements TagSymbolCreator {
 
     /**
      * regular expression pattern:
-     * topic = {({name}, {type}), \( msgField = {msgField} \)\?}
+     * {serviceID = {serviceID}, instanceID = {instanceID}, eventgroupID = {eventgroupID}}
      * to test the pattern just enter:
-     * \s*\{\s*topic\s*=\s*\(\s*([a-z|A-Z|~|/][0-9|a-z|A-Z|_|/]*)\s*,\s*([a-z|A-Z][0-9|a-z|A-Z|_|/]*)\s*\)\s*(s*,\s*msgField\s*=\s*([a-z|A-Z][a-z|A-Z|1-9|_|\.|::|\(|\)]*)\s*)?\s*\}\s*
+     * \s*\{\s*serviceID\s*=\s*[0-9]*,\s*instanceID\s*=\s*[0-9]*,\s*eventgroupID\s*=\s*[0-9]*\}\s*
      * at http://www.regexplanet.com/advanced/java/index.html
      */
 
-    public static final Pattern pattern = Pattern.compile("\\s*\\{\\s*topic\\s*=\\s*([a-z|A-Z|~|/][0-9|a-z|A-Z|_|/]*)\\s*(s*,\\s*msgField\\s*=\\s*([a-z|A-Z][a-z|A-Z|1-9|_|\\.|::|\\(|\\)]*)\\s*)?\\s*\\}\\s*");
+    public static final Pattern pattern = Pattern.compile("\\\s*\\{\\s*serviceID\\s*=\\s*[0-9]*,\\s*instanceID\\s*=\\s*[0-9]*,\\s*eventgroupID\\s*=\\s*[0-9]*\\}\\s*");
 
     public static Scope getGlobalScope(final Scope scope) {
         Scope s = scope;
@@ -124,7 +124,7 @@ public class SomeIPConnectionSymbolCreator implements TagSymbolCreator {
         }
         Log.error(String.format("'%s' does not match the specified regex pattern '%s'",
                 regex,
-                "{topic = ({name}, {type}), msgField = {msgField}}"));
+                "{serviceID = {serviceID}, instanceID = {instanceID}, eventgroupID = {eventgroupID}}"));
         return null;
     }
 
