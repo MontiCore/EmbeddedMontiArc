c63a0e6e3145ed074969ce787a9ce1621b885c94
Philipp Helios <philipp.a.t.helios@gmail.com>
2019-09-28 18:09:26 +0200

    shell scripts should work now

--- docker/Linux/bumpbot.sh
+++ docker/Linux/bumpbot.sh
@@ -1,2 +1,2 @@
-docker exec -it carlacomponents /bin/bash -c 'source /opt/ros/kinetic/setup.bash && cd /usr/src/carlacomponents/Bumpbot/ && rm -rf target && java -jar ../mw-generator.jar Bumpbot/Bumpbot.json && target/compile.sh && target/build/test_bumpBot/coordinator/Coordinator_test_bumpBot'
+docker exec -it carlacomponents /bin/bash -c 'source /opt/ros/kinetic/setup.bash && cd /usr/src/carlacomponents/Bumpbot/ && rm -rf target && mvn clean install -s settings.xml && Bumpbot/target/middleware/bumper_bumpBot/build/bumper_bumpBot/coordinator/Coordinator_bumper_bumpBot'
 
