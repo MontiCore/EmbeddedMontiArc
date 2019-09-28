e7d73737abef5b16d6268e696a587be0f23062c0
Lennart Bucher <lennart.bucher@rwth-aachen.de>
2019-09-28 23:07:56 +0200

    delete files created by automatic merge

--- docker/bumpbot/open_shell.sh
+++
@@ -1,2 +0,0 @@
-docker exec -it emam2carla /bin/bash -c 'source /opt/ros/kinetic/setup.bash && source opt/carla-ros-bridge/devel/setup.bash && /bin/bash'
-# (c) https://github.com/MontiCore/monticore  
