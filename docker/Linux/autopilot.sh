444fa9ab1533539ad252fdb204623c28ece93c9b
Lennart Bucher <lennart.bucher@rwth-aachen.de>
2019-09-29 09:47:25 +0200

    update executable path

--- docker/Linux/autopilot.sh
+++ docker/Linux/autopilot.sh
@@ -1,2 +1,1 @@
-docker exec -it carlacomponents /bin/bash -c 'source /opt/ros/kinetic/setup.bash && cd /usr/src/carlacomponents/ && rm -rf Autopilot/target && mvn clean install -pl Autopilot -s settings.xml && Autopilot/target/middleware/wrapper_wrapper/build/test_autopilot/coordinator/Coordinator_test_autopilot'
-
+docker exec -it carlacomponents /bin/bash -c 'source /opt/ros/kinetic/setup.bash && cd /usr/src/carlacomponents/ && rm -rf Autopilot/target && mvn clean install -pl Autopilot -s settings.xml && Autopilot/target/middleware/wrapper_wrapper/build/wrapper_wrapper/coordinator/Coordinator_wrapper_wrapper'
