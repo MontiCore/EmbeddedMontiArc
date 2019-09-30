0d204bf08469ce6c8b66d45dc9c1c0f831f55a2d
Florian Raeth <Boxroom@users.noreply.github.com>
2019-09-30 11:50:35 +0200

    small test with all supported data-types

--- src/test/java/de/monticore/lang/monticar/generator/middleware/SomeIPGenerationTest.java
+++ src/test/java/de/monticore/lang/monticar/generator/middleware/SomeIPGenerationTest.java
@@ -1,23 +1,16 @@
 package de.monticore.lang.monticar.generator.middleware;
 
-import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.cncModel.EMAPortSymbol;
 import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.instanceStructure.EMAComponentInstanceSymbol;
 import de.monticore.lang.embeddedmontiarc.tagging.middleware.someip.SomeIPToEmamTagSchema;
-import de.monticore.lang.embeddedmontiarc.tagging.middleware.someip.SomeIPConnectionSymbol;
 import de.monticore.lang.monticar.generator.middleware.impls.CPPGenImpl;
-import de.monticore.lang.monticar.generator.someip.GeneratorSomeIP;
 import de.monticore.lang.monticar.generator.middleware.impls.SomeIPGenImpl;
 import de.monticore.lang.monticar.generator.someip.helper.SomeIPTagHelper;
-import de.monticore.lang.tagging._symboltable.TagSymbol;
 import de.monticore.lang.tagging._symboltable.TaggingResolver;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -28,7 +21,7 @@ public class SomeIPGenerationTest extends AbstractSymtabTest {
     @Test
     public void testSomeIPGeneration() throws IOException {
         TaggingResolver taggingResolver = createSymTabAndTaggingResolver(TEST_PATH);
-        EMAComponentInstanceSymbol componentInstanceSymbol = taggingResolver.<EMAComponentInstanceSymbol>resolve("tests.a.addComp", EMAComponentInstanceSymbol.KIND).orElse(null);
+        EMAComponentInstanceSymbol componentInstanceSymbol = taggingResolver.<EMAComponentInstanceSymbol>resolve("tests.someip.testComp", EMAComponentInstanceSymbol.KIND).orElse(null);
         SomeIPToEmamTagSchema.registerTagTypes(taggingResolver);
 
         assertNotNull(componentInstanceSymbol);
@@ -36,7 +29,7 @@ public class SomeIPGenerationTest extends AbstractSymtabTest {
         SomeIPTagHelper.resolveTags(taggingResolver, componentInstanceSymbol);
 
         DistributedTargetGenerator distributedTargetGenerator = new DistributedTargetGenerator();
-        distributedTargetGenerator.setGenerationTargetPath(OUT_BASE + "addComp/src");
+        distributedTargetGenerator.setGenerationTargetPath(OUT_BASE + "compA/src");
         distributedTargetGenerator.add(new CPPGenImpl(TEST_PATH),"cpp");
         distributedTargetGenerator.add(new SomeIPGenImpl(), "someip");
 
