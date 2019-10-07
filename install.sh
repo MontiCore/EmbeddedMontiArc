d0d23bae5b7743fad2a04e4594969b15fd93e7ff
Nicola Gatto <nicola.gatto@rwth-aachen.de>
2019-10-07 14:55:55 +0200

    Update generator

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # (c) https://github.com/MontiCore/monticore  
 . config.sh
 
-GENERATOR_PATH="bin/embedded-montiarc-math-middleware-generator-0.0.26-SNAPSHOT-jar-with-dependencies.jar"
+GENERATOR_PATH="bin/embedded-montiarc-math-middleware-generator-0.0.33-SNAPSHOT-jar-with-dependencies.jar"
 AGENT_BUILD="target/agent/build"
 POSTPROCESSOR_BUILD="target/postprocessor/build"
 
