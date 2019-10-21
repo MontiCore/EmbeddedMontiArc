638ba4f89f27bab7f06aa231b70a0b4298d28c48
Malte Heithoff <malte.heithoff@rwth-aachen.de>
2019-10-21 17:13:51 +0200

    Updated Code

--- python/ema-listall/emaFailedPipelineProjects.py
+++ python/ema-listall/emaFailedPipelineProjects.py
@@ -1,21 +1,16 @@
 def print_FailedPipelineProjects(group, abs):
-  # print("\r\n")
   subgroups = group.subgroups
-
   abspath = abs + group.path +"/"
   for g in subgroups.list(all=True):
     current_group = gl.groups.get(g.id)
-    # print(abspath + g.path + "    " + str(g.id) + "    (" +str(len(current_group.projects.list(all=True))) + " projects)")
     for p in current_group.projects.list(all=True):
-      lastMasterPipelineStatus = "-"
       project = gl.projects.get(p.id)
-      pipelines = project.pipelines.list(all=True)
-      for pipeline in pipelines:
-        if pipeline.ref == "master":
-          lastMasterPipelineStatus = pipeline.status
-          break
-      if(lastMasterPipelineStatus == "failed"):
-        print(abspath + g.path +"/"+ p.path + "    " + str(p.id) + "    " + "Pipelinestatus: " + lastMasterPipelineStatus)
+      pipelines = project.pipelines.list(ref='master')
+      if not pipelines == []:
+        pipeline = pipelines[0]
+        if (pipeline.status == "failed"):
+          print(abspath + g.path + "/" + p.path + "    " + str(
+            p.id) + "    " + "Pipelinestatus: " + pipeline.status)
     print_FailedPipelineProjects(current_group, abspath)
 
 import gitlab
