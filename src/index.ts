97e65c4b222406fdf7418c5c2aca8b535936c0f2
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2019-11-24 14:56:37 +0100

    Export classes in index.ts/js

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { LanguageServerManager } from './LanguageServerManager';
+export { MavenLanguageClient, MavenLanguageClientOptions } from './mavenLanguageClient';
+export { MavenUpdateManager } from './mavenUpdateManager';
+export { MavenUpdater } from './mavenUpdater';
