78b79e906c070a870d8636550c87fca9165af9a8
Julian Treiber <julian.treiber@rwth-aachen.de>
2019-12-03 15:24:25 +0100

    gitignore

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ then
 fi
 
 npm install --unsafe-perm
-echo "y" | vsce package --baseContentUrl "https://git.rwth-aachen.de/monticore/EmbeddedMontiArc/utilities/ema-lsp/tree/treiber/dl" -o "ema-linter.vsix"
+echo "y" | vsce package --baseContentUrl "https://git.rwth-aachen.de/monticore/EmbeddedMontiArc/utilities/vscode-ema-linter/blob/emadl" -o "ema-linter.vsix"
