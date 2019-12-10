0d45b028727e0a0861e9530ac3e4353989cf0759
Julian Treiber <julian.treiber@rwth-aachen.de>
2019-12-10 14:07:16 +0100

    updated ema-lsp dependency to local version 1.3

--- build.sh
+++ build.sh
@@ -8,5 +8,11 @@ then
 	npm install -g vsce
 fi
 
+mvn install:install-file \
+ -Dfile="ema-lsp-1.3-SNAPSHOT.jar" \
+ -DgroupId=de.monticore.lang.monticar \
+ -DartifactId=ema-lsp \
+ -Dversion=1.3-SNAPSHOT \
+
 npm install --unsafe-perm
 echo "y" | vsce package --baseContentUrl "https://git.rwth-aachen.de/monticore/EmbeddedMontiArc/utilities/vscode-ema-linter/blob/emadl" -o "ema-linter.vsix"
