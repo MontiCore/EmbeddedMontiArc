0f8b0cc1a14f0e539d0c51c8a9b0e29500ae8cae
Ahmed <ahmedb.94@hotmail.com>
2019-12-26 20:05:16 +0100

    remove unused imports

--- src/main/java/de/monticore/lang/monticar/generator/cpp/commands/ArgumentReturnMathCommand.java
+++ src/main/java/de/monticore/lang/monticar/generator/cpp/commands/ArgumentReturnMathCommand.java
@@ -1,21 +1,7 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.lang.monticar.generator.cpp.commands;
 
-import de.monticore.lang.math._symboltable.expression.MathExpressionSymbol;
-import de.monticore.lang.math._symboltable.matrix.MathMatrixAccessSymbol;
-import de.monticore.lang.math._symboltable.matrix.MathMatrixNameExpressionSymbol;
-import de.monticore.lang.monticar.generator.BluePrint;
 import de.monticore.lang.monticar.generator.MathCommand;
-import de.monticore.lang.monticar.generator.cpp.BluePrintCPP;
-import de.monticore.lang.monticar.generator.cpp.MathFunctionFixer;
-import de.monticore.lang.monticar.generator.cpp.OctaveHelper;
-import de.monticore.lang.monticar.generator.cpp.converter.ExecuteMethodGenerator;
-import de.monticore.lang.monticar.generator.cpp.converter.MathConverter;
-import de.monticore.lang.monticar.generator.cpp.symbols.MathStringExpression;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 
 /**
  * @author Ahmed Diab.
