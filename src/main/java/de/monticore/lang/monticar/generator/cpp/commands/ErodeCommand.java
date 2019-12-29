ce138e031c0f766b73e6cecee579e569a189015d
Ahmed <ahmedb.94@hotmail.com>
2019-12-29 14:32:05 +0100

    extend the erode and dilate components

--- src/main/java/de/monticore/lang/monticar/generator/cpp/commands/ErodeCommand.java
+++ src/main/java/de/monticore/lang/monticar/generator/cpp/commands/ErodeCommand.java
@@ -3,7 +3,7 @@ package de.monticore.lang.monticar.generator.cpp.commands;
 import de.monticore.lang.math._symboltable.expression.MathExpressionSymbol;
 import de.monticore.lang.math._symboltable.matrix.MathMatrixAccessSymbol;
 import de.monticore.lang.math._symboltable.matrix.MathMatrixNameExpressionSymbol;
-import de.monticore.lang.monticar.generator.BluePrint;
+import de.monticore.lang.monticar.generator.*;
 import de.monticore.lang.monticar.generator.cpp.BluePrintCPP;
 import de.monticore.lang.monticar.generator.cpp.converter.ExecuteMethodGenerator;
 import de.monticore.lang.monticar.generator.cpp.MathFunctionFixer;
@@ -44,14 +44,66 @@ public class ErodeCommand extends ArgumentReturnMathCommand{
         String valueListString = "";
         for (MathMatrixAccessSymbol accessSymbol : mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().getMathMatrixAccessSymbols())
             MathFunctionFixer.fixMathFunctions(accessSymbol, (BluePrintCPP) bluePrint);
+
+        Method erodeHelperMethod = getErodeHelperMethod();
         valueListString += ExecuteMethodGenerator.generateExecuteCode(mathExpressionSymbol, new ArrayList<String>());
         List<MathMatrixAccessSymbol> newMatrixAccessSymbols = new ArrayList<>();
-        MathStringExpression stringExpression = new MathStringExpression("erode" + valueListString,mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().getMathMatrixAccessSymbols());
+        MathStringExpression stringExpression = new MathStringExpression("erodeHelper" + valueListString,mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().getMathMatrixAccessSymbols());
         newMatrixAccessSymbols.add(new MathMatrixAccessSymbol(stringExpression));
 
 
         mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().setMathMatrixAccessSymbols(newMatrixAccessSymbols);
         ((BluePrintCPP) bluePrint).addCVIncludeString("opencv2/imgproc");
+        bluePrint.addMethod(erodeHelperMethod);
 
+    }
+
+    private Method getErodeHelperMethod(){
+        Method method = new Method("erodeHelper", "void");
+
+        //parameter
+        Variable src = new Variable();
+        src.setName("src");
+        src.setVariableType(new VariableType("CommonMatrixType", MathConverter.curBackend.getMatrixTypeName(), MathConverter.curBackend.getIncludeHeaderName()));
+        Variable dst = new Variable();
+        dst.setName("dst");
+        dst.setVariableType(new VariableType("CommonMatrixType", MathConverter.curBackend.getMatrixTypeName(), MathConverter.curBackend.getIncludeHeaderName()));
+        Variable erosion_elem = new Variable();
+        erosion_elem.setName("erosion_elem");
+        erosion_elem.setVariableType(new VariableType("Integer", "int",""));
+        Variable iteration = new Variable();
+        iteration.setName("iteration");
+        iteration.setVariableType(new VariableType("Integer", "int", ""));
+        //add variable to method
+        method.addParameter(src);
+        method.addParameter(dst);
+        method.addParameter(erosion_elem);
+        method.addParameter(iteration);
+        //add an instruction to the method
+        method.addInstruction(methodBody());
+
+        return method;
+    }
+
+    private Instruction methodBody() {
+        return new Instruction() {
+            @Override
+            public String getTargetLanguageInstruction() {
+                return  "    int erosion_type = 0;\n" +
+                        "    if( erosion_elem == 0 ){ erosion_type = MORPH_RECT; }\n" +
+                        "    else if( erosion_elem == 1 ){ erosion_type = MORPH_CROSS; }\n" +
+                        "    else if( erosion_elem == 2) { erosion_type = MORPH_ELLIPSE; }\n" +
+                        "    erosion_size = erosion_elem;\n" +
+                        "    mat element = getStructuringElement( erosion_type,\n" +
+                        "                            Size( 2*erosion_size + 1, 2*erosion_size+1 ),\n" +
+                        "                            Point( erosion_size, erosion_size ) );\n" +
+                        "    erode( src, dst, element, Point(-1,-1), iteration );";
+            }
+
+            @Override
+            public boolean isConnectInstruction() {
+                return false;
+            }
+        };
     }
 }
--- src/test/resources/results/armadillo/testMath/l0/test_math_dilateCommandTest.h
+++ src/test/resources/results/armadillo/testMath/l0/test_math_dilateCommandTest.h
@@ -9,12 +9,29 @@ using namespace arma;
 using namespace cv;
 class test_math_dilateCommandTest{
 public:
+mat src;
+int dilation_elem;
+int iteration;
+mat dst;
 void init()
 {
+src=mat(n,m);
+dst=mat(n,m);
 }
+void dilateHelper(mat src, mat dst, int erosion_elem, int iteration)
+{
+    int dilation_type = 0;
+    if( dilation_elem == 0 ){ dilation_type = MORPH_RECT; }
+    else if( dilation_elem == 1 ){ dilation_type = MORPH_CROSS; }
+    else if( dilation_elem == 2) { dilation_type = MORPH_ELLIPSE; }
+    dilation_size = dilation_elem;
+    mat element = getStructuringElement( dilation_type,
+                            Size( 2*dilation_size + 1, 2*dilation_size+1 ),
+                            Point( dilation_size, dilation_size ) );
+    dilate( src, dst, element, Point(-1,-1), iteration );}
 void execute()
 {
-double a = (dilate(0, 0, 0, 0, 0));
+dst = (dilateHelper(src, dilation_elem, iteration));
 }
 
 };
