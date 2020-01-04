1520f7534e7724f08a8ce1bd9b757ca52bf13909
Ahmed <ahmedb.94@hotmail.com>
2020-01-04 13:44:38 +0100

    add inRange command

--- src/main/java/de/monticore/lang/monticar/generator/cpp/commands/InRangeCommand.java
+++ src/main/java/de/monticore/lang/monticar/generator/cpp/commands/InRangeCommand.java
@@ -3,8 +3,7 @@ package de.monticore.lang.monticar.generator.cpp.commands;
 import de.monticore.lang.math._symboltable.expression.MathExpressionSymbol;
 import de.monticore.lang.math._symboltable.matrix.MathMatrixAccessSymbol;
 import de.monticore.lang.math._symboltable.matrix.MathMatrixNameExpressionSymbol;
-import de.monticore.lang.monticar.generator.BluePrint;
-import de.monticore.lang.monticar.generator.MathCommand;
+import de.monticore.lang.monticar.generator.*;
 import de.monticore.lang.monticar.generator.cpp.BluePrintCPP;
 import de.monticore.lang.monticar.generator.cpp.MathFunctionFixer;
 import de.monticore.lang.monticar.generator.cpp.converter.ExecuteMethodGenerator;
@@ -19,7 +18,7 @@ import java.util.List;
  * @author Ahmed Diab
  */
 
-public class InRangeCommand extends MathCommand{
+public class InRangeCommand extends ArgumentReturnMathCommand{
     public InRangeCommand() {
         setMathCommandName("inRange");
     }
@@ -45,13 +44,49 @@ public class InRangeCommand extends MathCommand{
         String valueListString = "";
         for (MathMatrixAccessSymbol accessSymbol : mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().getMathMatrixAccessSymbols())
             MathFunctionFixer.fixMathFunctions(accessSymbol, (BluePrintCPP) bluePrint);
+
+        Method inRangeHelperMethod = getInRangeHelperMethod();
         valueListString += ExecuteMethodGenerator.generateExecuteCode(mathExpressionSymbol, new ArrayList<String>());
         List<MathMatrixAccessSymbol> newMatrixAccessSymbols = new ArrayList<>();
-        MathStringExpression stringExpression = new MathStringExpression("inRange" + valueListString,mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().getMathMatrixAccessSymbols());
+        MathStringExpression stringExpression = new MathStringExpression("inRangeHelper" + valueListString,mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().getMathMatrixAccessSymbols());
         newMatrixAccessSymbols.add(new MathMatrixAccessSymbol(stringExpression));
 
         mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().setMathMatrixAccessSymbols(newMatrixAccessSymbols);
         ((BluePrintCPP) bluePrint).addCVIncludeString("opencv2/core");
+        bluePrint.addMethod(inRangeHelperMethod);
 
+    }
+
+    private Method getInRangeHelperMethod(){
+        Method method = new Method("inRangeHelper", "void");
+
+        //add parameters
+        Variable src = new Variable();
+        method.addParameter(src, "src", "CommonMatrix","Mat", "");;
+        Variable dst = new Variable();
+        method.addParameter(dst, "dst", "CommonMatrixType", "Mat", "");
+        Variable lowerBoundary = new Variable();
+        method.addParameter(lowerBoundary, "lowerB", "colvec", "colvec", "" );
+        Variable upperBoundary = new Variable();
+        method.addParameter(upperBoundary, "upperB", "colvec", "colvec", "");
+        //add an instruction to the method
+        method.addInstruction(methodBody());
+
+        return method;
+    }
+
+    private Instruction methodBody(){
+        return new Instruction() {
+            @Override
+            public String getTargetLanguageInstruction() {
+                return "    inRange(src, dst, Scalar(lowerB(0), lowerB(1), lowerB(2)),\n" +
+                        "            Scalar(upperB(0), upperB(1), upperB(2)));\n";
+            }
+
+            @Override
+            public boolean isConnectInstruction() {
+                return false;
+            }
+        };
     }
 }
