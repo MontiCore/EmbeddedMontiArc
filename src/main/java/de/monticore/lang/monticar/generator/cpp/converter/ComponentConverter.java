09a921eb96c64495eb7a855d60232e9f1b52319e
Ahmed <ahmedb.94@hotmail.com>
2020-01-09 17:41:33 +0100

    extends some features

--- src/main/java/de/monticore/lang/monticar/generator/cpp/converter/ComponentConverter.java
+++ src/main/java/de/monticore/lang/monticar/generator/cpp/converter/ComponentConverter.java
@@ -76,7 +76,7 @@ public class ComponentConverter {
                     }
                 } else if(mathExpresionSymbol.isValueExpression()) {
                     if (((MathValueSymbol) mathExpresionSymbol).getValue() instanceof MathMatrixNameExpressionSymbol) {
-                        boolean bool = namesOfFunctions.add(((MathMatrixNameExpressionSymbol) ((MathValueSymbol) mathExpresionSymbol).getValue()).getNameToAccess());
+                        namesOfFunctions.add(((MathMatrixNameExpressionSymbol) ((MathValueSymbol) mathExpresionSymbol).getValue()).getNameToAccess());
                     }
                 }
             }
