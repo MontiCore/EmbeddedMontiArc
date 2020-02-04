fba8f6e80122da8e636c92452b8957502990afbd
Julian Treiber <julian.treiber@rwth-aachen.de>
2020-02-04 11:19:32 +0100

    added test images fashion_2x2

--- python-scripts/generate_test_images.py
+++ python-scripts/generate_test_images.py
@@ -5,7 +5,7 @@ import os
 save_dir = '../resources/images_fashion2x2/'
 
 with h5py.File('../resources/fashionmnist_2x2/test.h5', 'r') as fh5:
-    images = fh5['softmax_label']
+    images = fh5['data']
     for i in range(10):
         save_path = os.path.join(save_dir, '{}.png'.format(i))
         print('Saving to... '+ os.path.abspath(save_path))
