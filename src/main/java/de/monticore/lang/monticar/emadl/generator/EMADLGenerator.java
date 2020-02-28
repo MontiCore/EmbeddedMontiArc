e3043f102389492b10b189689e1466e72980e647
julian.treiber <julian.treiber@rwth-aachen.de>
2020-02-28 21:20:40 +0100

    added weightsPath

--- src/main/java/de/monticore/lang/monticar/emadl/generator/EMADLGenerator.java
+++ src/main/java/de/monticore/lang/monticar/emadl/generator/EMADLGenerator.java
@@ -14,6 +14,7 @@ import de.monticore.lang.monticar.cnnarch._symboltable.NetworkInstructionSymbol;
 import de.monticore.lang.monticar.cnnarch.generator.CNNArchGenerator;
 import de.monticore.lang.monticar.cnnarch.generator.CNNTrainGenerator;
 import de.monticore.lang.monticar.cnnarch.generator.DataPathConfigParser;
+import de.monticore.lang.monticar.cnnarch.generator.WeightsPathConfigParser;
 import de.monticore.lang.monticar.cnnarch.gluongenerator.CNNTrain2Gluon;
 import de.monticore.lang.monticar.cnnarch.gluongenerator.annotations.ArchitectureAdapter;
 import de.monticore.lang.monticar.cnnarch.gluongenerator.preprocessing.PreprocessingComponentParameterAdapter;
@@ -246,7 +247,7 @@ public class EMADLGenerator {
             String b = backend.getBackendString(backend);
             String trainingDataHash = "";
             String testDataHash = "";
-			
+
             if (architecture.get().getDataPath() != null) {
                 if (b.equals("CAFFE2")) {
                     trainingDataHash = getChecksumForLargerFile(architecture.get().getDataPath() + "/train_lmdb/data.mdb");
@@ -410,6 +411,21 @@ public class EMADLGenerator {
         return dataPath;
     }
 
+    protected String getWeightsPath(EMAComponentSymbol component, EMAComponentInstanceSymbol instance){
+        String weightsPath;
+
+        Path weightsPathDefinition = Paths.get(getModelsPath(), "weights_paths.txt");
+        if (weightsPathDefinition.toFile().exists()) {
+            WeightsPathConfigParser newParserConfig = new WeightsPathConfigParser(getModelsPath() + "weights_paths.txt");
+            weightsPath = newParserConfig.getWeightsPath(component.getFullName());
+        } else {
+            Log.warn("No weights path definition found in " + weightsPathDefinition + " found: "
+                    + "No pretrained weights will be loaded.");
+            weightsPath = null;
+        }
+        return weightsPath;
+    }
+
     protected void generateComponent(List<FileContent> fileContents,
                                      Set<EMAComponentInstanceSymbol> allInstances,
                                      TaggingResolver taggingResolver,
@@ -431,7 +447,9 @@ public class EMADLGenerator {
         if (architecture.isPresent()){
             cnnArchGenerator.check(architecture.get());
             String dPath = getDataPath(taggingResolver, EMAComponentSymbol, componentInstanceSymbol);
+            String wPath = getWeightsPath(EMAComponentSymbol, componentInstanceSymbol);
             architecture.get().setDataPath(dPath);
+            architecture.get().setWeightsPath(wPath);
             architecture.get().setComponentName(EMAComponentSymbol.getFullName());
             generateCNN(fileContents, taggingResolver, componentInstanceSymbol, architecture.get());
             if (processedArchitecture != null) {
