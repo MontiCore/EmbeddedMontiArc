2ff485fb3bdf02dfc2c4add44c6268681ff9fadb
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2020-03-16 22:52:44 +0100

    Disable Windows path tests on Linux

--- src/test/java/de/monticore/util/lsp/DockerModelFileCacheTest.java
+++ src/test/java/de/monticore/util/lsp/DockerModelFileCacheTest.java
@@ -6,11 +6,14 @@ import java.io.File;
 import java.nio.file.Path;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
 
 public class DockerModelFileCacheTest {
 
     @Test
     public void translatePathSimpleWindows() {
+        assumeTrue(System.getProperty("os.name").toLowerCase().contains("windows"));
+
         DockerModelFileCache cache = new DockerModelFileCache(null, null, new File("C:\\path\\to\\external\\dir").toPath(), new File("/path/to/internal/dir").toPath());
         Path res = cache.translatePath(new File("C:\\path\\to\\external\\dir\\file.txt").toPath());
         assertEquals(new File("/path/to/internal/dir/file.txt").toPath(), res);
@@ -18,6 +21,8 @@ public class DockerModelFileCacheTest {
 
     @Test
     public void translatePathNestedWindows() {
+        assumeTrue(System.getProperty("os.name").toLowerCase().contains("windows"));
+
         DockerModelFileCache cache = new DockerModelFileCache(null, null, new File("C:\\path\\to\\external\\dir").toPath(), new File("/path/to/internal/dir").toPath());
         Path res = cache.translatePath(new File("C:\\path\\to\\external\\dir\\nested\\file.txt").toPath());
         assertEquals(new File("/path/to/internal/dir/nested/file.txt").toPath(), res);
