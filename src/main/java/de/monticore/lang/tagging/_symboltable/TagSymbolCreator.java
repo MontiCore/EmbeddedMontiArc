f8b61359dcf37ce2fc4f582ecbb3fd6adbcdff3d
Bernhard Rumpe <rumpe@se-rwth.de>
2020-03-25 23:53:06 +0100

    BR-sy

--- src/main/java/de/monticore/lang/tagging/_symboltable/TagSymbolCreator.java
+++ src/main/java/de/monticore/lang/tagging/_symboltable/TagSymbolCreator.java
@@ -7,7 +7,6 @@ import de.monticore.symboltable.Scope;
 /**
  * Created by Michael von Wenckstern on 31.05.2016.
  *
- * @author Michael von Wenckstern
  */
 public interface TagSymbolCreator {
   void create(ASTTaggingUnit unit, TaggingResolver scope);
