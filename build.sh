de5460b1232a6e3047b88dc5f08f2c4792af6db9
Ahmed <ahmedb.94@hotmail.com>
2020-04-27 09:58:25 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 rm -rf target
 java -jar embedded-montiarc-math-generator-0.1.16-SNAPSHOT-jar-with-dependencies.jar -m src/main/emam/ -r ba.ballTracking -o target -flag-generate-cmake
-cd target
-sed -i 's/cube/Cube<unsigned char>/g' ba_ballTracking.h
-cd ..
+
 rm -rf build
 mkdir build && cd build
 
