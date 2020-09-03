f89050c1edb6a1cf34ee29280f101a4517d6d7d3
Julian Johannes Steinsberger-Dührßen <johannes.steinsberger-duehrssen@rwth-aachen.de>
2020-09-03 02:44:56 +0200

    Update build.sh

--- tensorflow/build.sh
+++ tensorflow/build.sh
@@ -1,5 +1,5 @@
 rm -rf target
-java -jar ../embedded-montiarc-emadl-generator-0.4.1-jar-with-dependencies.jar -m src/emadl/models/ -r cNNCalculator.Connector -o target -b TENSORFLOW -c n
+java -jar ../embedded-montiarc-emadl-generator-0.3.6-SNAPSHOT-jar-with-dependencies.jar -m src/emadl/models/ -r cNNCalculator.Connector -o target -b TENSORFLOW -c n
 
 rm -rf build
 mkdir build && cd build
