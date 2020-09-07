93d705e0272045aec8447174ba96f6f019c0e8a5
abdallahat <abdallah.atouani@rwth-aachen.de>
2020-09-07 18:11:04 +0200

    remove old directory

--- src/main/java/de/monticore/lang/monticar/utilities/TestMojo.java
+++ src/main/java/de/monticore/lang/monticar/utilities/TestMojo.java
@@ -6,7 +6,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 
-@Mojo(name="ml-process-ex")
+@Mojo(name="build")
 public class TestMojo extends AbstractMojo {
   public void execute() throws MojoExecutionException, MojoFailureException {
     DatasetArtifactCreator creator = new DatasetArtifactCreator();
