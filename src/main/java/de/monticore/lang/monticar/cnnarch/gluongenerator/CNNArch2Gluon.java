6cc8130673c7121896e972608ab7b0e0aa6109f9
Malte Heithoff <h.malte.f@googlemail.com>
2020-09-28 16:01:30 +0200

    Adapt to new generator interface

--- src/main/java/de/monticore/lang/monticar/cnnarch/gluongenerator/CNNArch2Gluon.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/gluongenerator/CNNArch2Gluon.java
@@ -9,7 +9,7 @@ import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
 import de.monticore.lang.monticar.generator.FileContent;
 import de.monticore.lang.monticar.generator.cmake.CMakeConfig;
 import de.monticore.lang.monticar.generator.cmake.CMakeFindModule;
-import de.se_rwth.commons.logging.Log;
+import de.monticore.lang.tagging._symboltable.TaggingResolver;
 
 import java.util.*;
 
@@ -20,99 +20,105 @@ public class CNNArch2Gluon extends CNNArchGenerator {
         layerSupportChecker = new CNNArch2GluonLayerSupportChecker();
     }
 
+    @Override
+    public CMakeConfig getCmakeConfig() {
+        return null;
+    }
+
     //check cocos with CNNArchCocos.checkAll(architecture) before calling this method.
     @Override
-    public Map<String, String> generateStrings(ArchitectureSymbol architecture){
-        Map<String, String> fileContentMap = compileFileContentMap(architecture);
-        return fileContentMap;
+    public List<FileContent> generateStrings(TaggingResolver taggingResolver, ArchitectureSymbol architecture){
+        List<FileContent> fileContents = compileFileContents(architecture);
+        return fileContents;
     }
 
-    public Map<String, String> generateStringsAllowMultipleIO(ArchitectureSymbol architecture, Boolean pythonFilesOnly) {
-        Map<String, String> fileContentMap;
+    public List<FileContent> generateStringsAllowMultipleIO(ArchitectureSymbol architecture, Boolean pythonFilesOnly) {
+        List<FileContent> fileContents;
         if (pythonFilesOnly) {
-            fileContentMap = compilePythonFilesOnlyContentMap(architecture);
+            fileContents = compilePythonFilesOnlyContents(architecture);
         } else {
-            fileContentMap = compileFileContentMap(architecture);
+            fileContents = compileFileContents(architecture);
         }
-        return fileContentMap;
+        return fileContents;
     }
 
-    private Map<String, String> compilePythonFiles(CNNArch2GluonTemplateController controller, ArchitectureSymbol architecture) {
-        Map<String, String> fileContentMap = new HashMap<>();
-        Map.Entry<String, String> temp;
+    private List<FileContent> compilePythonFiles(CNNArch2GluonTemplateController controller, ArchitectureSymbol architecture) {
+        List<FileContent> fileContents = new ArrayList<>();
+        FileContent temp;
 
         temp = controller.process("CNNNet", Target.PYTHON);
-        fileContentMap.put(temp.getKey(), temp.getValue());
+        fileContents.add(temp);
 
         if (architecture.getDataPath() != null) {
             temp = controller.process("CNNDataLoader", Target.PYTHON);
-            fileContentMap.put(temp.getKey(), temp.getValue());
+            fileContents.add(temp);
         }
 
         temp = controller.process("CNNCreator", Target.PYTHON);
-        fileContentMap.put(temp.getKey(), temp.getValue());
+        fileContents.add(temp);
 
-        return fileContentMap;
+        return fileContents;
     }
 
-    private Map<String, String> compileCppFiles(CNNArch2GluonTemplateController controller) {
-        Map<String, String> fileContentMap = new HashMap<>();
-        Map.Entry<String, String> temp;
+    private List<FileContent> compileCppFiles(CNNArch2GluonTemplateController controller) {
+        List<FileContent> fileContents = new ArrayList<>();
+        FileContent temp;
 
         temp = controller.process("CNNPredictor", Target.CPP);
-        fileContentMap.put(temp.getKey(), temp.getValue());
+        fileContents.add(temp);
 
         temp = controller.process("CNNSupervisedTrainer", Target.PYTHON);
-        fileContentMap.put(temp.getKey(), temp.getValue());
+        fileContents.add(temp);
 
         temp = controller.process("CNNGanTrainer", Target.PYTHON);
-        fileContentMap.put(temp.getKey(), temp.getValue());
+        fileContents.add(temp);
 
         temp = controller.process("execute", Target.CPP);
-        fileContentMap.put(temp.getKey().replace(".h", ""), temp.getValue());
+        temp = new FileContent(temp.getFileContent(), temp.getFileName().replace(".h",""));
+        fileContents.add(temp);
 
         temp = controller.process("CNNModelLoader", Target.CPP);
-        fileContentMap.put("CNNModelLoader.h", temp.getValue());
+        fileContents.add(new FileContent(temp.getFileContent(), "CNNModelLoader.h"));
 
-        return fileContentMap;
+        return fileContents;
     }
 
 
-    private Map<String, String> compileFileContentMap(ArchitectureSymbol architecture) {
+    private List<FileContent> compileFileContents(ArchitectureSymbol architecture) {
         TemplateConfiguration templateConfiguration = new GluonTemplateConfiguration();
 
         architecture.processForEpisodicReplayMemory();
 
-        Map<String, String> fileContentMap = new HashMap<>();
+        List<FileContent> fileContents = new ArrayList<>();
         CNNArch2GluonTemplateController archTc = new CNNArch2GluonTemplateController(
                 architecture, templateConfiguration);
 
-        fileContentMap.putAll(compilePythonFiles(archTc, architecture));
-        fileContentMap.putAll(compileCppFiles(archTc));
+        fileContents.addAll(compilePythonFiles(archTc, architecture));
+        fileContents.addAll(compileCppFiles(archTc));
 
-        return fileContentMap;
+        return fileContents;
     }
 
-    private Map<String, String> compilePythonFilesOnlyContentMap(ArchitectureSymbol architecture) {
+    private List<FileContent> compilePythonFilesOnlyContents(ArchitectureSymbol architecture) {
         TemplateConfiguration templateConfiguration = new GluonTemplateConfiguration();
         CNNArch2GluonTemplateController archTc = new CNNArch2GluonTemplateController(
                 architecture, templateConfiguration);
         return compilePythonFiles(archTc, architecture);
     }
 
-    public Map<String, String> generateCMakeContent(String rootModelName) {
+    public List<FileContent> generateCMakeContent(String rootModelName) {
         // model name should start with a lower case letter. If it is a component, replace dot . by _
         rootModelName = rootModelName.replace('.', '_').replace('[', '_').replace(']', '_');
         rootModelName =  rootModelName.substring(0, 1).toLowerCase() + rootModelName.substring(1);
 
         CMakeConfig cMakeConfig = new CMakeConfig(rootModelName);
         cMakeConfig.addModuleDependency(new CMakeFindModule("Armadillo", true));
-        cMakeConfig.addCMakeCommand("set(LIBS ${LIBS} mxnet)");
+        cMakeConfig.addCmakeLibraryLinkage("mxnet");
 
-        Map<String,String> fileContentMap = new HashMap<>();
+        List<FileContent> fileContents = new ArrayList<>();
         for (FileContent fileContent : cMakeConfig.generateCMakeFiles()){
-            fileContentMap.put(fileContent.getFileName(), fileContent.getFileContent());
+            fileContents.add(fileContent);
         }
-        return fileContentMap;
+        return fileContents;
     }
 }
