88034dfaddd9ebb4d30eefcf6c6da65c2e3f64e7
Jean Meurice <jean.meurice@rwth-aachen.de>
2020-10-04 20:42:03 +0200

    Native Cpp exec Fix test

--- scripts/unix/before_script.sh
+++ scripts/unix/before_script.sh
@@ -8,3 +8,9 @@ apt update
 apt --assume-yes install libxext6
 apt --assume-yes install libxrender1
 apt --assume-yes install libxtst6
+
+LIBGCC=$(find /lib -name libgcc* -print -quit)
+echo LIBGCC: $LIBGCC
+LIBGCC_FOLDER=$(dirname ${LIBGCC})
+echo LIBGCC_FOLDER: $LIBGCC_FOLDER
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LIBGCC_FOLDER}
