33372dba7942d31e808fd2fd4aecc2604649674e
Jean Meurice <jean.meurice@rwth-aachen.de>
2020-10-05 14:14:38 +0200

    Settings update

--- install/run.bat
+++ install/run.bat
@@ -2,15 +2,6 @@
 @REM (c) https://github.com/MontiCore/monticore
 @REM
 
-@REM
-@REM (c) https://github.com/MontiCore/monticore
-@REM
-@REM The license generally applicable for this project
-@REM can be found under https://github.com/MontiCore/monticore
-@REM
-
 pushd %~dp0
-
 java -jar basic-simulator.jar %*
-
 popd
