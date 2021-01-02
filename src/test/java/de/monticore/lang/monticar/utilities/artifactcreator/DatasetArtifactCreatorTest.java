acdab72b8c408a446ba741215b5d0d17b5175fe2
abdallahat <abdallah.atouani@rwth-aachen.de>
2021-01-02 13:53:52 +0100

    add test for DatasetArtifactCreator

--- src/test/java/de/monticore/lang/monticar/utilities/artifactcreator/DatasetArtifactCreatorTest.java
+++ src/test/java/de/monticore/lang/monticar/utilities/artifactcreator/DatasetArtifactCreatorTest.java
@@ -1,14 +1,29 @@
 package de.monticore.lang.monticar.utilities.artifactcreator;
 
+import de.monticore.lang.monticar.utilities.models.FileLocation;
 import de.monticore.lang.monticar.utilities.models.StorageInformation;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.powermock.api.mockito.PowerMockito.when;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ArtifactCreator.class)
 public class DatasetArtifactCreatorTest {
+
+  @Rule
+  public TemporaryFolder tmpFolder = new TemporaryFolder();
 
   @Test
   public void testCreateArtifactWithoutGroupId() {
@@ -18,7 +33,7 @@ public class DatasetArtifactCreatorTest {
 
     String expectedMessage = "Group ID of dataset artifact must be specified.";
 
-    assertIllegalArgumentExceptionThrown(new IllegalArgumentException(), storageInformation, expectedMessage);
+    assertThrowsException(new IllegalArgumentException(), storageInformation, expectedMessage);
   }
 
   @Test
@@ -29,7 +44,7 @@ public class DatasetArtifactCreatorTest {
 
     String expectedMessage = "Artifact ID of dataset artifact must be specified.";
 
-    assertIllegalArgumentExceptionThrown(new IllegalArgumentException(), storageInformation, expectedMessage);
+    assertThrowsException(new IllegalArgumentException(), storageInformation, expectedMessage);
   }
 
   @Test
@@ -40,11 +55,37 @@ public class DatasetArtifactCreatorTest {
 
     String expectedMessage = "Path of dataset must be specified.";
 
-    assertIllegalArgumentExceptionThrown(new NullPointerException(), storageInformation, expectedMessage);
+    assertThrowsException(new NullPointerException(), storageInformation, expectedMessage);
+  }
+
+  @Test
+  public void testCreateArtifactWithValidInformation() throws IOException {
+    StorageInformation storageInformation = new StorageInformation();
+    storageInformation.setArtifactId("artifactId");
+    storageInformation.setGroupId("groupId");
+    storageInformation.setVersion(1);
+    storageInformation.setPath(
+        new File(getClass().getClassLoader().getResource("dataset/episodicMemorySimple").getFile())
+    );
+
+    spy(ArtifactCreator.class);
+    when(ArtifactCreator.createJarFileName(anyString(), anyString())).thenReturn(tmpFolder.getRoot().getAbsolutePath() + "/dataset.jar");
+    File file = DatasetArtifactCreator.createArtifact(storageInformation, tmpFolder.getRoot().getAbsolutePath());
+    assertTrue(file.exists());
+  }
+
+  @Test
+  public void testGetDatasetLocations() {
+    File datasetPath = new File(getClass().getClassLoader().getResource("dataset/episodicMemorySimple").getFile());
+    List<FileLocation> datasetLocations = DatasetArtifactCreator.getDatasetLocations(datasetPath);
+
+    assertEquals(2, datasetLocations.size());
+    assertEquals("training_data/train.h5", datasetLocations.get(0).getJarLocation());
+    assertEquals("training_data/test.h5", datasetLocations.get(1).getJarLocation());
   }
 
 
-  private void assertIllegalArgumentExceptionThrown(Exception expectedException, StorageInformation storageInformation, String expectedMessage) {
+  private static void assertThrowsException(Exception expectedException, StorageInformation storageInformation, String expectedMessage) {
     Exception exception = assertThrows(expectedException.getClass(), () ->
         DatasetArtifactCreator.createArtifact(storageInformation, "/tmp")
     );
