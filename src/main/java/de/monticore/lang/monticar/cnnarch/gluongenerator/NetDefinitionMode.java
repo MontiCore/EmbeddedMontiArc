d345d26ba8d6b7eadd80387e9b530e43211aa147
Luis Rickert <rickert.luis@googlemail.com>
2021-03-04 15:55:48 +0100

    [added]: extendet Enum by "Artificial_ARCH_CLASS"

--- src/main/java/de/monticore/lang/monticar/cnnarch/gluongenerator/NetDefinitionMode.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/gluongenerator/NetDefinitionMode.java
@@ -8,7 +8,6 @@ public enum NetDefinitionMode {
     ARCHITECTURE_DEFINITION,
     PREDICTION_PARAMETER,
     FORWARD_FUNCTION,
-    PARAMETER_VALIDATION,
     ARTIFICIAL_ARCH_CLASS;
 
     public static NetDefinitionMode fromString(final String netDefinitionMode) {
@@ -19,11 +18,10 @@ public enum NetDefinitionMode {
                 return FORWARD_FUNCTION;
             case "PREDICTION_PARAMETER":
                 return PREDICTION_PARAMETER;
-            case "PARAMETER_VALIDATION":
-                return PARAMETER_VALIDATION;
             case "ARTIFICICIAL_ARCH_CLASS":
                 return ARTIFICIAL_ARCH_CLASS;
             default:
+
                 throw new IllegalArgumentException("Unknown Net Definition Mode");
         }
     }
