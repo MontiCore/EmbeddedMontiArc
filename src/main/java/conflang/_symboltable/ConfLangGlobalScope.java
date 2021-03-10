cc0b2717799a380c85acd7d5dcb00754e1cce8f3
Cihad Kirhan <cihad.kirhan@rwth-aachen.de>
2021-03-10 14:10:19 +0100

    Added custom ConfLangGlobalScope

---
+++ src/main/java/conflang/_symboltable/ConfLangGlobalScope.java
@@ -0,0 +1,9 @@
+package conflang._symboltable;
+
+public class ConfLangGlobalScope extends ConfLangGlobalScopeTOP {
+
+    @Override
+    public ConfLangGlobalScope getRealThis() {
+        return this;
+    }
+}
