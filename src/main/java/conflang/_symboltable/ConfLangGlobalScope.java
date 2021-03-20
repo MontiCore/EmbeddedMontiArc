8ddb1c49e71b5a9c4fe4aead6059f43458a8387f
Cihad Kirhan <cihad.kirhan@rwth-aachen.de>
2021-03-20 16:11:48 +0100

    Removed custom global scope implementation

--- src/main/java/conflang/_symboltable/ConfLangGlobalScope.java
+++
@@ -1,9 +0,0 @@
-package conflang._symboltable;
-
-public class ConfLangGlobalScope extends ConfLangGlobalScopeTOP {
-
-    @Override
-    public ConfLangGlobalScope getRealThis() {
-        return this;
-    }
-}
