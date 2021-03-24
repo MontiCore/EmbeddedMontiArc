976ee231a6a082c56b1de0e9004652a18445b6f2
Kirhan, Cihad <Cihad.Kirhan@arag.de>
2021-03-24 23:59:11 +0100

    Integration of ConfLang and SchemaLang

---
+++ src/main/java/de/monticore/lang/monticar/cnnarch/generator/training/LearningMethod.java
@@ -0,0 +1,23 @@
+package de.monticore.lang.monticar.cnnarch.generator.training;
+
+public enum LearningMethod {
+
+    SUPERVISED("supervised"),
+    REINFORCEMENT("reinforcement"),
+    GAN("gan");
+
+    String method;
+
+    LearningMethod(String method) {
+        this.method = method;
+    }
+
+    public static LearningMethod learningMethod(String learningMethod) {
+        for (LearningMethod lm : values()) {
+            if (lm.method.equals(learningMethod)) {
+                return lm;
+            }
+        }
+        throw new IllegalArgumentException(String.valueOf(learningMethod));
+    }
+}
