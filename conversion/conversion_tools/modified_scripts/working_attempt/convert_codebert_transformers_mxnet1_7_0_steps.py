a41d7b7d0d7a4212a55b978982236ed3063b50d2
makua.bernal <makua.bernal@rwth-aachen.de>
2021-04-07 20:55:26 +0200

    updated step file for 1.7.0

---
+++ conversion/conversion_tools/modified_scripts/working_attempt/convert_codebert_transformers_mxnet1_7_0_steps.py
@@ -0,0 +1,224 @@
+# RoBERTa Equivalencies
+# found by looking at conversion/conversion_tools/convert_roberta_original_pytorch_checkpoint_to_pytorch.py
+# Huggingface -> Fairseq -> gluonnlp
+#
+# MODEL CONFIG
+#
+# RobertaConfig.vocab_size -> FairseqRobertaModel.model.decoder.sentence_encoder.embed_tokens.num_embeddings,
+# RobertaConfig.hidden_size -> FairseqRobertaModel.args.encoder_embed_dim,
+# RobertaConfig.num_hidden_layers -> FairseqRobertaModel.args.encoder_layers,
+# RobertaConfig.num_attention_heads -> FairseqRobertaModel.args.encoder_attention_heads
+# RobertaConfig.intermediate_size -> FairseqRobertaModel.args.encoder_ffn_embed_dim
+# RobertaConfig.max_position_embeddings -> 512 (514 - 2 for the padding and some other symbol at the beginning)
+# RobertaConfig.type_vocab_size -> 1
+# RobertaConfig.layer_norm_eps -> 1e-5
+#
+# WEIGHTS/PARAMS
+#
+# RobertaForMaskedLM.roberta.embeddings.word_embeddings.weight -> FairseqRobertaModel.model.decoder.sentence_encoder.embed_tokens.weight
+# RobertaForMaskedLM.roberta.embeddings.position_embeddings.weight -> FairseqRobertaModel.model.decoder.sentence_encoder.embed_positions.weight
+# RobertaForMaskedLM.roberta.embeddings.token_type_embeddings.weight.data -> torch.zeros_like(model.roberta.embeddings.token_type_embeddings.weight)  # just zero them out b/c RoBERTa doesn't use them.
+# RobertaForMaskedLM.roberta.embeddings.LayerNorm.weight -> FairseqRobertaModel.model.decoder.sentence_encoder.emb_layer_norm.weight
+# RobertaForMaskedLM.roberta.embeddings.LayerNorm.bias -> FairseqRobertaModel.model.decoder.sentence_encoder.emb_layer_norm.bias
+#
+# FOR EVERY HIDDEN LAYER
+#
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.self.query.weight.data -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn.q_proj.weight
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.self.query.bias.data -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn.q_proj.bias
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.self.key.weight.data -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn.k_proj.weight
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.self.key.bias.data -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn.k_proj.bias
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.self.value.weight.data -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn.v_proj.weight
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.self.value.bias.data -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn.v_proj.bias
+#
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.output.dense.weight -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn.out_proj.weight
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.output.dense.bias -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn.out_proj.bias
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.output.LayerNorm.weight -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn_layer_norm.weight
+# RobertaForMaskedLM.roberta.encoder.layer[i].attention.output.LayerNorm.bias -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].self_attn_layer_norm.bias
+#
+# RobertaForMaskedLM.roberta.encoder.layer[i].intermediate.dense.weight -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].fc1.weight
+# RobertaForMaskedLM.roberta.encoder.layer[i].intermediate.dense.bias -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].fc1.bias
+#
+# RobertaForMaskedLM.roberta.encoder.layer[i].output.dense.weight -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].fc2.weight
+# RobertaForMaskedLM.roberta.encoder.layer[i].output.dense.bias -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].fc2.bias
+# RobertaForMaskedLM.roberta.encoder.layer[i].output.LayerNorm.weight -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].final_layer_norm.weight
+# RobertaForMaskedLM.roberta.encoder.layer[i].output.LayerNorm.bias -> FairseqRobertaModel.model.decoder.sentence_encoder.layers[i].final_layer_norm.bias
+#
+# The script below is an adapted version of the RoBERTA fairseq to gluonnlp script found in their github
+# In addition to the parameter mapping I had to remove mlm parts, zero the token type embeddings weights, and adjust the padding part
+# Library Versions: gluonnlp Version: 0.10.0, mxnet 1.7.0.post2, torch 1.4.0, transformers 4.2.1
+
+# Use BERTEncoder class in gluonnlp 0.10.0 and look at the _load_pretrained_params function at https://nlp.gluon.ai/_modules/gluonnlp/model/bert.html#roberta_12_768_12
+# next steps, look at distilbert and encoder._collect_params_with_prefix() and compare to huggingface
+
+import os
+import re
+import sys
+import json
+import shutil
+import logging
+import argparse
+import pprint as pp
+
+import mxnet as mx
+import numpy as np
+from numpy.testing import assert_allclose
+
+import torch
+import transformers
+import gluonnlp as nlp
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Convert the huggingface CodeBERT Model to Gluon.')
+    parser.add_argument('--save_dir', type=str, default=None,
+                        help='Directory path to save the converted model.')
+    parser.add_argument('--gpu', type=int, default=None,
+                        help='The single gpu to run mxnet, (e.g. --gpu 0) the default device is cpu.')
+    return parser.parse_args()
+
+def convert_params(hf_model, hf_tokenizer, hf_cfg, gluon_model, ctx):
+    print('converting params')
+    gluon_model._output_all_encodings = True
+    gluon_model.encoder._output_all_encodings = True
+    # gluon_model.initialize(ctx=ctx) # unsure if it should be init with normal
+    gluon_model.hybridize()
+    gluon_params = gluon_model.collect_params()
+    hf_params = hf_model.state_dict()
+    num_layers = hf_cfg.num_hidden_layers
+
+    # for layer_id in range(num_layers):
+    #     hf_prefix = 'encoder.layer.{}.'.format(layer_id)
+    #     hf_atten_prefix = hf_prefix + 'attention.self.'
+    #     gl_prefix = 'bertencoder0_transformer{}'.format(layer_id)
+    #     gl_qkv_prefix = gl_prefix + '_dotproductselfattentioncell0_'
+
+    #     for name in [
+    #         'query{}weight', 'key{}weight', 'value{}weight', 
+    #         'query{}bias', 'key{}bias', 'value{}bias'
+    #     ]:
+    #         gl_name = gl_qkv_prefix + name.format('_')
+    #         hf_name = hf_atten_prefix + name.format('.') 
+    #         gluon_params[gl_name].set_data(arr_to_gl(hf_params[hf_name]))
+
+    #     for hf_suffix, gl_suffix in [
+    #         ('attention.output.dense.weight', '_proj_weight'),
+    #         ('attention.output.dense.bias', '_proj_bias'),
+    #         ('attention.output.LayerNorm.weight', '_layernorm0_gamma'),
+    #         ('attention.output.LayerNorm.bias', '_layernorm0_beta'),
+    #         ('intermediate.dense.weight', '_positionwiseffn0_ffn_1_weight'),
+    #         ('intermediate.dense.bias', '_positionwiseffn0_ffn_1_bias'),
+    #         ('output.dense.weight', '_positionwiseffn0_ffn_2_weight'),
+    #         ('output.dense.bias', '_positionwiseffn0_ffn_2_bias'),
+    #         ('output.LayerNorm.weight', '_positionwiseffn0_layernorm0_gamma'),
+    #         ('output.LayerNorm.bias', '_positionwiseffn0_layernorm0_beta')
+    #     ]:
+    #         hf_name = hf_prefix + hf_suffix
+    #         gl_name = gl_prefix + gl_suffix
+    #         gluon_params[gl_name].set_data(arr_to_gl(hf_params[hf_name]))
+
+    # for hf_name, gl_name in [
+    #     ('embeddings.word_embeddings.weight', 'robertamodelwpooler0_word_embed_embedding0_weight'),
+    #     ('embeddings.token_type_embeddings.weight', 'robertamodelwpooler0_token_type_embed_embedding0_weight'),
+    #     ('embeddings.LayerNorm.weight', 'bertencoder0_layernorm0_gamma'),
+    #     ('embeddings.LayerNorm.bias', 'bertencoder0_layernorm0_beta'),
+    #     ('pooler.dense.weight', 'robertamodelwpooler0_pooler_weight'),
+    #     ('pooler.dense.bias', 'robertamodelwpooler0_pooler_bias')
+    # ]:
+    #     gluon_params[gl_name].set_data(arr_to_gl(hf_params[hf_name]))
+
+    # # position embed weight
+    # padding_idx = hf_tokenizer.pad_token_id
+    # hf_pos_embed_name = 'embeddings.position_embeddings.weight'
+    # gl_pos_embed_name = 'bertencoder0_position_weight'
+    # hf_wo_pad = arr_to_gl(hf_params[hf_pos_embed_name])[padding_idx + 1:, :]
+    # gluon_params[gl_pos_embed_name].set_data(hf_wo_pad)
+    
+    # print(gluon_model.collect_params())
+    # pp.pprint(list(zip(list(hf_params.keys()), [hf_params[k].shape for k in hf_params.keys()])))
+    # print(gluon_model)
+    # print(hf_model)
+    # return gluon_model
+
+def arr_to_gl(arr):
+    return mx.nd.array(arr.cpu().numpy())
+
+def test_model(hf_model, hf_tokenizer, gluon_model, gpu):
+    print('testing model')
+    ctx = mx.gpu(gpu) if gpu is not None else mx.cpu()
+    batch_size = 3
+    seq_length = 5
+    vocab_size = hf_model.config.vocab_size
+    padding_id = hf_tokenizer.pad_token_id
+
+    input_ids = [[47499, 39079, 6870, 1, 1], [24802, 37471, 6381, 1, 1], [21942, 26564, 26037, 1, 1]]
+    valid_length = [3, 3, 3]
+
+    gl_input_ids = mx.nd.array(input_ids)
+    gl_valid_length = mx.nd.array(valid_length)
+
+    gl_all_hiddens = gluon_model(
+        gl_input_ids, 
+        valid_length=gl_valid_length
+    )
+
+    input_ids = np.array(input_ids)
+    valid_length = np.array(valid_length)
+
+    hf_input_ids = torch.from_numpy(input_ids).cpu()
+    hf_model.eval()
+
+    # create attention mask for hf model
+    hf_valid_length = np.zeros((batch_size, seq_length))
+    for i in range(batch_size):
+        hf_valid_length[i][:valid_length[i]] = 1
+    hf_valid_length = torch.from_numpy(hf_valid_length)
+
+    hf_outputs = hf_model(hf_input_ids, attention_mask=hf_valid_length, output_hidden_states=True)
+    hf_all_hiddens = hf_outputs['hidden_states']
+
+    # outputting some slices to compare to 2.0.0 script
+    print(len(gl_all_hiddens))
+    print(gl_all_hiddens[0][0][0][300:350])
+    print(gl_all_hiddens[0][1][2][300:350])
+    print(gl_all_hiddens[0][2][4][300:350])
+
+    # checking all_encodings_outputs
+    num_layers = hf_model.config.num_hidden_layers
+    for i in range(num_layers + 1):
+        gl_hidden = gl_all_hiddens[i].asnumpy()
+        hf_hidden = hf_all_hiddens[i]
+        hf_hidden = hf_hidden.detach().cpu().numpy()
+        for j in range(batch_size):
+            assert_allclose(
+                gl_hidden[j, :valid_length[j], :],
+                hf_hidden[j, :valid_length[j], :],
+                1E-4,
+                1E-4
+            )
+
+
+
+def convert_huggingface_model(args):
+    if not args.save_dir:
+        args.save_dir = os.path.basename('./codebert_gluon')
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    # load and save huggingface model
+    hf_tokenizer = transformers.RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+    hf_model = transformers.RobertaModel.from_pretrained("microsoft/codebert-base")
+    
+    hf_model.save_pretrained(args.save_dir)
+    hf_tokenizer.save_pretrained(args.save_dir)
+
+    ctx = mx.gpu(args.gpu) if args.gpu is not None else mx.cpu()
+    gluon_model, _ = nlp.model.get_model('roberta_12_768_12', dataset_name='openwebtext_ccnews_stories_books_cased', use_decoder=False)
+    convert_params(hf_model, hf_tokenizer, hf_model.config, gluon_model, ctx)
+    test_model(hf_model, hf_tokenizer, gluon_model, args.gpu)
+
+    gluon_model.export(os.path.join(args.save_dir, 'codebert'))
+    logging.info('Exported the CodeBERT model to {}'.format(os.path.join(args.save_dir)))
+    logging.info('Conversion finished!')
+
+if __name__ == '__main__':
+    args = parse_args()
+    convert_huggingface_model(args)
