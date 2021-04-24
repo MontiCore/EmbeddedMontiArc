2d7772d39aa6dd2c203a14c30ca52d71787feebe
Alexander Mann <alexander.mann1@rwth-aachen.de>
2021-04-24 10:11:40 +0200

    Adds the 3D up convolution layer class and name in predefinedLayers

--- src/main/java/de/monticore/lang/monticar/cnnarch/predefined/AllPredefinedLayers.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/predefined/AllPredefinedLayers.java
@@ -58,6 +58,7 @@ public class AllPredefinedLayers {
     public static final String DOT_PRODUCT_SELF_ATTENTION_NAME = "DotProductSelfAttention";
     public static final String LOAD_NETWORK_NAME = "LoadNetwork";
     public static final String CONVOLUTION3D_Name = "3DConvolution";
+    public static final String UP_CONVOLUTION3D_Name = "3DConvolution";
 
     //replay layers
     public static final String LARGE_MEMORY_NAME = "LargeMemory";
@@ -195,7 +196,8 @@ public class AllPredefinedLayers {
                 DotProductSelfAttention.create(),
                 LargeMemory.create(),
                 EpisodicMemory.create(),
-                Convolution3D.create());
+                Convolution3D.create()
+                UpConvolution3D.create());
     }
 
     public static List<UnrollDeclarationSymbol> createUnrollList(){
