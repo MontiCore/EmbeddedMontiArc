d7d0c0030e1e90c7ac63fc3abf1a6608aed761e7
Jean Meurice <jean.meurice@rwth-aachen.de>
2021-05-21 16:03:49 +0200

    Load Test

---
+++ samples/load_test/src/lib.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+#if defined(_MSC_VER)
+    //  Microsoft
+    #define EXPORT __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+    #define IMPORT
+#else
+    //  do nothing and hope for the best?
+    #define EXPORT
+    #define IMPORT
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
+extern "C" {
+EXPORT int lib_bar_function();
+}
+
+
+
+struct Foo {
+    int a;
+
+    Foo();
+};
+
+namespace foo_ns {
+    extern Foo global_foo;
+}
