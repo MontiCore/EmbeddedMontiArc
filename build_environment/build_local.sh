955aa65fa20d478c2d6953f7304060461c196b7e
Jean Meurice <jean.meurice@rwth-aachen.de>
2021-06-23 15:10:06 +0200

    Script update & chmod

--- build_environment/build_local.sh
+++ build_environment/build_local.sh
@@ -2,9 +2,10 @@
 
 if [ -z "$1" ] || [ "$1" == "--help" ]; then
     echo "Usage:"
-    echo "    ./build_local.sh 'folder'"
+    echo "    ./build_local.sh 'folder' [Config]"
     echo "Description:"
-    echo "    Builds the project for the local architecture. "
+    echo "    Builds the project for the local architecture."
+    echo "    Config: optional, specifies Release or Debug, defaults to Release"
     exit 0
 fi
 
@@ -14,11 +15,26 @@ pushd $SCRIPT_DIR
 
 . get_config.sh config
 
-SHARED_CPP_PATH=../../externals/shared_cpp
+SHARED_CPP_DIR=$SCRIPT_DIR/$EXTERNALS_DIRECTORY/shared_cpp
+CMAKE_ARGS="-DSHARED_CPP_DIR=${SHARED_CPP_DIR}"
+
+BUILD_CONFIG="${2}"
+if [ -z "$BUILD_CONFIG" ]; then 
+	BUILD_CONFIG=Release; 
+fi
+CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=${BUILD_CONFIG}"
+BUILD_DIR="local_${BUILD_CONFIG}"
+
+if [ -n "$GENERATOR" ]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -G\"${GENERATOR}\""
+fi
+
+echo "CMAKE_ARGS=$CMAKE_ARGS"
+
 
 cd $1
 
-cmake -S . -B local_build
-cmake --build local_build
+cmake ${CMAKE_ARGS} -S . -B ${BUILD_DIR}
+cmake --build ${BUILD_DIR}
 
 popd
