7bb70f37d94704ddf1c2888e8f4e6c9a9872acc1
lr119628 <luis.rickert@rwth-aachen.de>
2021-08-08 16:28:20 +0200

    [update]: comments,typing

--- src/main/resources/AdaNet/AdaNetDefault.py
+++ src/main/resources/AdaNet/AdaNetDefault.py
@@ -2,7 +2,53 @@ import CoreAdaNet
 import mxnet
 from mxnet import gluon, io
 from adaNetUtils import train_candidate
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
+
+
+class CandidateHull(CoreAdaNet.CandidateHull):
+    """
+        the hull which holds the stacked building blocks and is potentially added to the model
+    """
+
+    def __init__(self, stack: int, **kwargs):
+        self.names = []
+        self.stack = stack
+        self.body = None
+        super(CandidateHull, self).__init__(**kwargs)
+
+    def build(self) -> None:
+        """
+            this function builds the candidate function structure and is called by its superclass
+            if there are parameters needed for the construction make sure they are added before the superclasses
+            constructor is called!
+        """
+        body = {self.name_ + f"{i}": CoreAdaNet.BuildingBlock(operation=self.building_block) for i in
+                range(self.stack)}
+        for name, operation in body.items():
+            self.__setattr__(name, operation)
+            self.names.append(name)
+        self.body = body
+
+    def count_nodes(self) -> int:
+        if self.node_count is None:
+            oc = 0
+            for name in self.names:
+                oc += self.__getattribute__(name).count_nodes()
+            self.node_count = oc
+        return self.node_count
+
+    def hybrid_forward(self, F, x, *args, **kwargs):
+        if self.input:
+            x = self.input(x)
+
+        for name in self.names:
+            x = self.__getattribute__(name)(x)
+
+        if self.output:
+            x = self.output(x)
+        x = self.finalOut(x)
+
+        return x
 
 
 class Builder(CoreAdaNet.Builder):
@@ -35,7 +81,7 @@ class Builder(CoreAdaNet.Builder):
         self.optimizer = optimizer
         self.optimizer_params = optimizer_params
 
-    def get_candidates(self) -> Dict:
+    def get_candidates(self) -> Dict[str:Tuple[CandidateHull, List[float]]]:
         """
         :returns a dict of already trained
         """
@@ -44,15 +90,8 @@ class Builder(CoreAdaNet.Builder):
         for i in range(self.candidate_per_round):
             name = f'candidate{i}round{self.round}'
             candidate = CandidateHull(name=name, in_block=self.input, out_block=self.output, stack=i + 1,
-                                      # batch_size=self.batch_size,
                                       model_shape=self.model_shape, building_block=self.build_operation)
-            candidate.initialize(ctx=self.ctx)
-            candidate.hybridize()
-            candidate_loss = train_candidate(candidate, epochs=self.epochs, optimizer=self.optimizer,
-                                             optimizer_params=self.optimizer_params, loss=self.loss,
-                                             batch_size=self.batch_size,
-                                             trainIter=self.train_iterator)
-
+            candidate_loss = self.train(candidate)
             candidates.setdefault(name, (candidate, candidate_loss))
 
         return candidates
@@ -66,44 +105,3 @@ class Builder(CoreAdaNet.Builder):
         """
         self.pre_stack += up
         self.round += 1
-
-
-class CandidateHull(CoreAdaNet.CandidateHull):
-    """
-        the hull which holds the stacked building blocks and is potentially added to the model
-    """
-
-    def __init__(self, stack: int, **kwargs):
-        self.names = []
-        self.stack = stack
-        self.body = None
-        super(CandidateHull, self).__init__(**kwargs)
-
-    def build(self) -> None:
-        body = {self.name_ + f"{i}": CoreAdaNet.BuildingBlock(operation=self.building_block) for i in
-                range(self.stack)}
-        for name, operation in body.items():
-            self.__setattr__(name, operation)
-            self.names.append(name)
-        self.body = body
-
-    def count_nodes(self) -> int:
-        if self.node_count is None:
-            oc = 0
-            for name in self.names:
-                oc += self.__getattribute__(name).count_nodes()
-            self.node_count = oc
-        return self.node_count
-
-    def hybrid_forward(self, F, x, *args, **kwargs):
-        if self.input:
-            x = self.input(x)
-
-        for name in self.names:
-            x = self.__getattribute__(name)(x)
-
-        if self.output:
-            x = self.output(x)
-        x = self.finalOut(x)
-
-        return x
