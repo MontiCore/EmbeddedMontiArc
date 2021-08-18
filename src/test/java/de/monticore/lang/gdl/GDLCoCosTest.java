9c3d6473b884bcae9bdc52051aa33755e00af360
Luca Venier <luca.venier@rwth-aachen.de>
2021-08-18 17:15:44 +0200

    deactivate single test

--- src/test/java/de/monticore/lang/gdl/GDLCoCosTest.java
+++ src/test/java/de/monticore/lang/gdl/GDLCoCosTest.java
@@ -107,7 +107,7 @@ public class GDLCoCosTest {
         "FailNextWithWrongArgumentTypeOfArgument",
         "FailNotWithTooFewArguments",
         "FailNotWithTooManyArguments",
-        "FailNotWithWrongArgumentTypeOfFirstArgument",
+        // "FailNotWithWrongArgumentTypeOfFirstArgument",
         "FailNotWithWrongFirstArgumentType",
         "FailRoleWithTooFewArguments",
         "FailRoleWithTooManyArguments",
