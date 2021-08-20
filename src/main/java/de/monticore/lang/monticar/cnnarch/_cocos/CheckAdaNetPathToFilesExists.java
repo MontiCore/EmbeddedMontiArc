e2abab23728f280b12ab65d78a163bb9f0eed066
lr119628 <luis.rickert@rwth-aachen.de>
2021-08-20 03:08:07 +0200

    [update] coco update to emadl fix and path changes

--- src/main/java/de/monticore/lang/monticar/cnnarch/_cocos/CheckAdaNetPathToFilesExists.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/_cocos/CheckAdaNetPathToFilesExists.java
@@ -1,5 +1,4 @@
 /**
- *
  * (c) https://github.com/MontiCore/monticore
  *
  * The license generally applicable for this project
@@ -14,7 +13,7 @@ import de.se_rwth.commons.logging.Log;
 public class CheckAdaNetPathToFilesExists extends CNNArchSymbolCoCo {
     @Override
     public void check(ArchitectureSymbol architecture) {
-        if (architecture.containsAdaNet() && !architecture.getAdaNetUtils().equals("./src/main/resources/AdaNet/")) {
+        if (architecture.containsAdaNet() && (architecture.getAdaNetUtils().equals("./src/main/resources/AdaNet/") || architecture.getAdaNetUtils().equals("./target/generated-sources-emadl/" + "/AdaNet/"))) {
             // this check is only added for future use it now checks if the path is changed from the default
             String msg = "0" + ErrorCodes.ADANET_NO_PYTHON_FILES_PATH + " the path to the AdaNet python has been changed! if the changed is intended, change this check CheckAdaNetPathToFilesExists" +
                     " expected path: \"./src/main/resources/AdaNet/\" got:" + architecture.getAdaNetUtils();
