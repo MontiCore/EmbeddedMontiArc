130e29e717db9ca5855787eaa3296f3daff5e84c
gospodima <dimasc28@gmail.com>
2021-12-22 16:07:30 +0000

    Add onnx implementation example

---
+++ onnx/build.sh
@@ -0,0 +1,27 @@
+MXNET_PATH=$(python3 -c "import mxnet; print(mxnet.__file__)")
+# (c) https://github.com/MontiCore/monticore  
+MXNET_FOLDER=$(dirname $MXNET_PATH)
+echo $MXNET_FOLDER
+
+if [ ! -f $MXNET_FOLDER/libmxnet.so ]; then
+    echo "libmxnet.so not found at default location" $MXNET_FOLDER
+    echo "It should be there if the python mxnet package is installed"
+    echo "Either fix the installation, or adapt the ./build.sh script to locate libmxnet.so correctly"
+    exit 1
+fi
+
+rm -rf ./target
+java -jar ../embedded-montiarc-emadl-generator-0.5.3-SNAPSHOT-jar-with-dependencies.jar -m ./gluon-pretrained/src/emadl/models/ -r cNNCalculator.Connector -o target -b GLUON -p /usr/bin/python3
+
+cp ./model/cNNCalculator.Network/model_0_newest.onnx ./model_0-0000.onnx
+
+rm -rf ./target
+java -jar ../embedded-montiarc-emadl-generator-0.5.3-SNAPSHOT-jar-with-dependencies.jar -m ./gluon-load/src/emadl/models/ -r cNNCalculator.Connector -o ./gluon-load/target -b GLUON -p /usr/bin/python3
+
+cd ./gluon-load
+rm -rf build
+mkdir build && cd build
+
+echo "Building DigitClassifier.."
+cmake -D MXNET_PATH=$MXNET_FOLDER/libmxnet.so ..
+make
