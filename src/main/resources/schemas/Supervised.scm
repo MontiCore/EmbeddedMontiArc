10c3912b6057474622fefc567e09229dd4623e8e
EMA CI Robot <kusmenko@se-rwth.de>
2022-02-17 22:17:50 +0000

    Squashed 'generators/CNNArch2X/' changes from 65a4d7a1..9cd4352f
    9cd4352f Merge branch 'onnx-dmytro' into 'master'
    a57778c1 Merge branch 'master' into onnx-dmytro
    69e2a60a Update pom.xml
    496a9c4e Add trainable element
    fc7ba116 Update num_epoc from N1 to N
    9567eb92 Update of JAR
    16484042 Add shuffle_data to Supervised.scm
    a62cc4d3 Add onnx_export parameter
    
    git-subtree-dir: generators/CNNArch2X
    git-subtree-split: 9cd4352f07207bf5b28b5af61a05cc8dadc8678e

--- src/main/resources/schemas/Supervised.scm
+++ src/main/resources/schemas/Supervised.scm
@@ -9,7 +9,7 @@ schema Supervised extends General {
     }
 
     batch_size: N1
-    num_epoch: N1
+    num_epoch: N
     normalize: B
     checkpoint_period = 5: N
     preprocessing_name: component
@@ -21,4 +21,6 @@ schema Supervised extends General {
     save_attention_image: B
     eval_metric: eval_metric_type
     loss: loss_type
+    shuffle_data: B
+    onnx_export: B
 }
