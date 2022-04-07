aee79526448e0b037321914891e716c254dee3a0
EMA CI Robot <kusmenko@se-rwth.de>
2022-04-07 08:06:04 +0000

    Squashed 'generators/EMADL2CPP/' changes from e122c7a4..fb30d233
    fb30d233 Merge branch 'ba-baumann' into 'master'
    88c0c13c Updated Tests
    ed4f6938 Merge remote-tracking branch 'origin/master' into ba-baumann
    36334eeb Added Test for GNNs
    e12bfcc3 Tests updated
    dd3b9b75 Small DGL fixes
    8777ccf4 Merge master in ba-baumann
    d45a48f9 DGL Dockerfile
    
    git-subtree-dir: generators/EMADL2CPP
    git-subtree-split: fb30d2336e1b794cc25a8d55554a3649d251d015

---
+++ src/test/resources/models/gnns/coraDgl/DGLNetwork.tag
@@ -0,0 +1,7 @@
+/* (c) https://github.com/MontiCore/monticore */
+package coraDgl;
+conforms to dltag.DataPathTagSchema;
+
+tags NeuronalNetwork {
+ tag DGLNetwork with DataPath = {path = src/test/resources/training_data/GNN/CoraDgl, type = HDF5};
+}
---
+++ src/test/resources/target_code/gluon/gnns/coraDgl/CNNTrainer_coraDgl_dGLNetwork.py
@@ -0,0 +1,43 @@
+# (c) https://github.com/MontiCore/monticore
+import logging
+import mxnet as mx
+
+import CNNCreator_coraDgl_dGLNetwork
+import CNNDataLoader_coraDgl_dGLNetwork
+import CNNSupervisedTrainer_coraDgl_dGLNetwork
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler("train.log", "w", encoding=None, delay="true")
+    logger.addHandler(handler)
+
+    coraDgl_dGLNetwork_creator = CNNCreator_coraDgl_dGLNetwork.CNNCreator_coraDgl_dGLNetwork()
+    coraDgl_dGLNetwork_creator.validate_parameters()
+    coraDgl_dGLNetwork_loader = CNNDataLoader_coraDgl_dGLNetwork.CNNDataLoader_coraDgl_dGLNetwork()
+    coraDgl_dGLNetwork_trainer = CNNSupervisedTrainer_coraDgl_dGLNetwork.CNNSupervisedTrainer_coraDgl_dGLNetwork(
+        coraDgl_dGLNetwork_loader,
+        coraDgl_dGLNetwork_creator
+    )
+
+    coraDgl_dGLNetwork_trainer.train(
+        batch_size=1,
+        num_epoch=20,
+        load_checkpoint=False,
+        context='cpu',
+        preprocessing=False,
+        normalize=False,
+        eval_metric='accuracy_masked',
+        eval_metric_params={
+        },
+        eval_train=False,
+        loss='softmax_cross_entropy',
+            loss_params={
+                'sparse_label': True},
+        optimizer='adam',
+        optimizer_params={
+            'weight_decay': 0.0,
+                    'learning_rate': 0.05,
+                    'learning_rate_decay': 1.0,
+                    'step_size': 1},
+    )
