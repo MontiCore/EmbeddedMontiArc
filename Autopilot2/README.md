deb796866f2a57dfb8e3edb6e401c1617e5a0d0c
Til Valentin Mohr <til.mohr@rwth-aachen.de>
2022-06-23 19:06:48 +0200

    Testing Multiple Vehicles

---
+++ Autopilot2/README.md
@@ -0,0 +1,1 @@
+EMADL agent for 2 vehicles, using the decentralized self-play approach on an intersection
---
+++ Autopilot2/config.sh
@@ -0,0 +1,8 @@
+PROJECT_ROOT="$(pwd)"
+BINARY="target/bin"
+GENERATOR_PATH="bin/embedded-montiarc-math-middleware-generator-0.1.4-SNAPSHOT-jar-with-dependencies.jar"
+AGENT_BUILD="target/agent/build"
+SIMULATOR_PATH="$HOME/dev/basic-simulator/install"
+SCENARIO_PATH="scenarios/rl_autopilot_160.json"
+DIR_NAME="Autopilot2"
+SAVE_FOLDER="$HOME/dev/coopmontisimautopilot/saved_autopilots"
