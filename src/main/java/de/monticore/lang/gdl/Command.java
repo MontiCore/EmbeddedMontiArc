f6dbd5def7479d8a264eae88ced7b34ca50ada6b
Matthias Nadenau <manadenau@web.de>
2022-07-01 13:28:22 +0200

    make translation more robust

--- src/main/java/de/monticore/lang/gdl/Command.java
+++ src/main/java/de/monticore/lang/gdl/Command.java
@@ -1,79 +1,59 @@
 package de.monticore.lang.gdl;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import org.apache.commons.collections4.list.UnmodifiableList;
+import de.monticore.lang.gdl.types.GDLTuple;
+import de.monticore.lang.gdl.types.GDLType;
+import de.monticore.lang.gdl.types.GDLValue;
 
 public class Command {
     
-    private String player;
-    private List<String> arguments;
+    private GDLTuple tuple;
 
-    private Command() {}
-
-    public String getPlayer() {
-        return player;
+    private Command() {
+        tuple = null;
     }
 
-    public List<String> getArguments() {
-        return arguments;
-    }
-
+    @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(this.getPlayer().substring(6) + " ");
-        sb.append("(");
-        for (int i = 0; i < this.getArguments().size(); i++) {
-
-            if (i < (this.getArguments().size() - 1)) {
-                sb.append(Interpreter.convertPLValue2Interpreter(this.getArguments().get(i)) + " ");
-            } else {
-                sb.append(Interpreter.convertPLValue2Interpreter(this.getArguments().get(i)));
-            }
-        }
-        sb.append(")");
-
-        return sb.toString();
+        String tupleString = tuple.toString();
+        return tupleString.substring(1, tupleString.length() - 1);
+    }
+        
+    public String toPlString() {
+        String tuplePlString = tuple.toPlString();
+        return tuplePlString;
     }
 
-    public static Command createMoveFromList(List<String> list) {
-        if (list.size() < 2) {
-            return null;
-        }
-        
-        Command move = new Command();
-        list = list.stream().map(s -> s.startsWith("value_") || s.startsWith("valnn_") ? s : Interpreter.convertInterpreterValue2PL(s)).collect(Collectors.toList());
-
-        move.player = list.get(0);
-        move.arguments = list.stream().skip(1).collect(Collectors.toList());
-
-        return move;
+    public static Command createFromLine(String line) {
+        Command command = new Command();
+        command.tuple = GDLTuple.createFromLine("(" + line + ")");
+        return command;
     }
 
-    public static Command createMoveFromLine(String line) {
-        String pattern = "([a-zA-z0-9]|\\-)+ \\(([a-zA-z0-9]|\\-)+( ([a-zA-z0-9]|\\-)+)*\\)";
-        if (!line.matches(pattern)) {
-            System.out.println("Move format wrong");
-            return null;
-        }
+    public static Command createFromPl(String plLine) {
+        Command command = new Command();
+        command.tuple = GDLTuple.createFromPl("(" + plLine + ")");
+        return command;
+    }
 
-        String[] split = line
-            .replace("(", "")
-            .replace(")", "")
-            .split(" ");
+    public static Command createFromGDLTuple(GDLTuple tuple) {
+        Command command = new Command();
+        command.tuple = tuple;
+        return command;
+    }
 
-        String role = Interpreter.convertInterpreterValue2PL(split[0]);
-        String[] moveArgs = new String[split.length-1];
-        for (int i = 0; i < moveArgs.length; i++) {
-            moveArgs[i] = Interpreter.convertInterpreterValue2PL(split[i+1]);
-        }
-        
-        Command move = new Command();
-        move.player = role;
-        move.arguments = UnmodifiableList.unmodifiableList(List.of(moveArgs));
+    public static Command createFromGDLPlTuple(GDLTuple tuple) {
+        Command command = new Command();
 
-        return move;
+        GDLType[] args = tuple.getElements().stream().skip(1).toArray(i -> new GDLType[i]);
+
+        command.tuple = new GDLTuple(tuple.getElements().get(0), new GDLTuple(args));
+        return command;
     }
 
 }
