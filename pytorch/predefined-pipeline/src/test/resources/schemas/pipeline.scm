1412485a92defcfbaa1767486c163068e13c7231
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-07-05 17:00:13 -0700

    Pipeline configuration through reference models, schemaLang and ConfLang

---
+++ pytorch/predefined-pipeline/src/test/resources/schemas/pipeline.scm
@@ -0,0 +1,11 @@
+schema Pipeline {
+/*
+  equivalent to :
+  data_access : component !
+  training : component !
+  evaluation : component !
+*/
+reference-model = referencemodels.Training_Pipeline.emadl
+
+
+}
