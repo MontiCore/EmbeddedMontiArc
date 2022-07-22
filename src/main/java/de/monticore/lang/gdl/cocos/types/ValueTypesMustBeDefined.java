da305ec769635cad05b9c7529b769599bddd9f01
Matthias Nadenau <manadenau@web.de>
2022-07-22 17:08:00 +0200

    consider mapped types in typedefcheck

--- src/main/java/de/monticore/lang/gdl/cocos/types/ValueTypesMustBeDefined.java
+++ src/main/java/de/monticore/lang/gdl/cocos/types/ValueTypesMustBeDefined.java
@@ -1,11 +1,15 @@
 package de.monticore.lang.gdl.cocos.types;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import de.monticore.lang.gdl._ast.ASTGame;
+import de.monticore.lang.gdl._ast.ASTGameInference;
+import de.monticore.lang.gdl._ast.ASTGameTuple;
 import de.monticore.lang.gdl._ast.ASTGameTypeCombineDef;
 import de.monticore.lang.gdl._ast.ASTGameTypeDef;
+import de.monticore.lang.gdl._ast.ASTGameTypeMapDef;
 import de.monticore.lang.gdl._ast.ASTGameValue;
 import de.monticore.lang.gdl._ast.ASTGameValueType;
 import de.monticore.lang.gdl._cocos.GDLASTGameValueTypeCoCo;
@@ -33,11 +37,24 @@ public class ValueTypesMustBeDefined implements GDLASTGameValueTypeCoCo {
     }
 
     private Set<String> createAllTypes() {
-        return game.getTuplesList().stream()
+        Set<String> staticTypes = game.getTuplesList().stream()
             .filter(t -> t.getElement(0) instanceof ASTGameTypeDef || t.getElement(0) instanceof ASTGameTypeCombineDef)
             .map(t -> t.getElement(1))
             .map(v -> ((ASTGameValue) v).getValue())
             .collect(Collectors.toSet());
+
+        Set<String> mappedTypes = game.getTuplesList().stream()
+            .filter(t -> t.getElement(0) instanceof ASTGameInference && t.getElement(1) instanceof ASTGameTuple)
+            .map(t -> (ASTGameTuple) t.getElement(1))
+            .filter(t -> t.getElement(0) instanceof ASTGameTypeMapDef) 
+            .map(t -> t.getElement(1))
+            .map(v -> ((ASTGameValue) v).getValue())
+            .collect(Collectors.toSet());
+
+        staticTypes =  new HashSet<>(staticTypes);
+        staticTypes.addAll(mappedTypes);
+
+        return staticTypes;
     }
 
 }
