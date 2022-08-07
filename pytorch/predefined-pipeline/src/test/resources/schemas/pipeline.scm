7fb08f7541aa2580e44b3704823c2620fb48683c
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-08-07 16:36:51 -0700

    Add model constructs to model ML pipeline steps

--- pytorch/predefined-pipeline/src/test/resources/schemas/pipeline.scm
+++ pytorch/predefined-pipeline/src/test/resources/schemas/pipeline.scm
@@ -1,11 +1,44 @@
 schema Pipeline {
+
 /*
-  equivalent to :
-  data_access : component !
-  training : component !
-  evaluation : component !
+  implicitly equivalent to following declarations. Schema should therefore avoid redeclaration as this would lead to validation error
+    data_access : component
+    training : component
+    evaluation : component
 */
-reference-model = referencemodels.Training_Pipeline.emadl
 
+reference-model = referencemodels.Training_Pipeline
 
+/*  possible pipeline steps
+    step names should correpsond to those of components in the corresponding reference model(s) to make symbol linking easier
+*/
+
+Step {
+  values:
+	Data_Access,
+	Training;
+
+//determines tep order
+ id:N!
+
+ define Training{
+    implementation = SupervisedTrainer: component!
+    // should correpsond to those of components in the corresponding reference model(s)
+    network: component!
+ }
+
+ define Data_Access{
+    implementation = HDF5DataAccess:component!
+    datasource: component!
+ }
+
+}
+
+// Pipeline steps
+   steps {
+    values: useless_keyword;
+
+    train_step: Step!
+    data_access_step: Step!
+   }
 }
