c01ee1961d2d9eb18f5e7ee844482d422a6b5dba
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-08-08 16:58:36 -0700

    Remove redundant components

--- pytorch/predefined-pipeline/src/test/resources/generated/Data_Access_Executor.py
+++
@@ -1,8 +0,0 @@
-class Data_Access_Executor():
-    def __init__(self, datasource):
-        self.datasource = datasource
-
-    def execute(self):
-        trainData = None
-        testData = None
-        return trainData, testData
--- pytorch/predefined-pipeline/src/test/resources/generated/Evaluation_Executor.py
+++
@@ -1,8 +0,0 @@
-class Evaluation_Executor():
-    def __init__(self, testData, trainedModel):
-        self.testData = testData
-        self.trainedModel = trainedModel
-
-    def execute(self):
-        accuracy = None
-        return accuracy
