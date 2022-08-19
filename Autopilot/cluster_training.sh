84995f401f4c41c754d6f382ba106e56ace7e47e
Til Valentin Mohr <til.mohr@rwth-aachen.de>
2022-08-19 16:03:10 +0200

    Somehow, mxnet still wants a gpu even when not using the gpu

--- Autopilot/cluster_training.sh
+++ Autopilot/cluster_training.sh
@@ -9,13 +9,13 @@
 
 ### Job Specification
 #SBATCH --cpus-per-task=6
-### SBATCH --gres=gpu:volta:2
+#SBATCH --gres=gpu:volta:2
 #SBATCH --mem-per-cpu=4096m
 
 #SBATCH --time=100:00:00
 
-### module load cuda
-### module load cudnn
-### module load nccl
+module load cuda
+module load cudnn
+module load nccl
 
 singularity exec --nv /rwthfs/rz/SW/UTIL.common/singularity/in296142 $(pwd)/install_and_run_training.sh -sp
