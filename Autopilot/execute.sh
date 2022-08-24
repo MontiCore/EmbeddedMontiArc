481d0e64a188fed16dc733f94e21cb4ac4d1d402
Til Valentin Mohr <til.mohr@rwth-aachen.de>
2022-08-24 14:24:07 +0200

    Improved Execute.sh Script: No manual work required anymore

--- Autopilot/execute.sh
+++ Autopilot/execute.sh
@@ -2,9 +2,28 @@
 
 trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
 roscore &
+. ./config.sh
+
 sleep 2
 oldpath=$(pwd)
 . $oldpath/config.sh
+cd "${BINARY}"
+if [ -d "model_old" ]; then
+	/bin/echo "model_old exists"
+	if [ -d "model" ]; then
+		rm -r model
+	fi
+else
+	mv model model_old
+fi
+cp -R ../agent/src/de_rwth_montisim_agent_master/cpp/model/ .
+cd model/de.rwth.montisim.agent.network.AutopilotQNet
+mv model_0_newest-0000.params model_0_newest-0000_OLD.params
+mv model_0_newest-symbol.json model_0_newest-symbol_OLD.json
+mv model_0-0000.params model_0_newest-0000.params
+mv model_0-symbol.json model_0_newest-symbol.json
+
+cd "${oldpath}"
 cd "${BINARY}"
 ./agent -t 100 &
 sleep 2
