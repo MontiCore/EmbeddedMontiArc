af68df6393dfae566149e0ae1eacbcafbf10b8cc
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-08-29 14:48:18 -0700

    Add optimiser to supervised schema

--- src/test/resources/schemas/Supervised.scm
+++ src/test/resources/schemas/Supervised.scm
@@ -30,8 +30,71 @@ schema Supervised {
             }
         }
 
+     optimizer_type {
+             values:
+                 sgd,
+                 adam,
+                 adamw,
+                 rmsprop,
+                 adagrad,
+                 nag,
+                 adadelta;
+
+             /*
+             * Parameters common to all optimizer algorithms.
+             */
+             learning_rate: Q
+             learning_rate_minimum: Q
+             learning_rate_decay: Q
+             weight_decay: Q
+             learning_rate_policy: enum {
+                 fixed, step, exp, inv, poly, sigmoid;
+             }
+             rescale_grad: Q
+             clip_gradient: Q
+             step_size: Z
+
+             define sgd {
+                 momentum: Q
+             }
+
+             define adam {
+                 beta1: Q
+                 beta2: Q
+                 epsilon: Q
+             }
+
+             define adamw {
+                 beta1: Q
+                 beta2: Q
+                 epsilon: Q
+             }
+
+             define rmsprop {
+                 epsilon: Q
+                 gamma1: Q
+                 gamma2: Q
+                 centered: B
+                 clip_weights: Q
+                 rho: Q
+             }
+
+             define adagrad {
+                 epsilon: Q
+             }
+
+             define nag {
+                 momentum: Q
+             }
+
+             define adadelta {
+                 epsilon: Q
+                 rho: Q
+             }
+         }
     // primitive types
     eval_metric: eval_metric_type
+    optimizer: optimizer_type
     batch_size: N1
     num_epoch: N
     checkpoint_period = 5: N
