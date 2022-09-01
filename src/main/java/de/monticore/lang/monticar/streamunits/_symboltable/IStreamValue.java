7b1a5b772126b35464a4a4f4233edf0f7502112e
Yuyuan Liu <lyraymond96@hotmail.com>
2022-09-01 12:51:16 +0200

    Resolve "Extension of streamtest for files"

--- src/main/java/de/monticore/lang/monticar/streamunits/_symboltable/IStreamValue.java
+++ src/main/java/de/monticore/lang/monticar/streamunits/_symboltable/IStreamValue.java
@@ -1,6 +1,8 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.lang.monticar.streamunits._symboltable;
 
+import de.monticore.lang.monticar.streamunits._symboltable.NamedStreamUnitsSymbol;
+
 /**
  */
 public interface IStreamValue {
