a95921cb547f466db5d22759c87e4609998e7063
Anis Abdollahi-Sissan <anis.abdollahi@rwth-aachen.de>
2022-09-13 18:35:19 +0200

    Modify environment call options

--- src/main/cluster/rl/run_environment.sh
+++ src/main/cluster/rl/run_environment.sh
@@ -7,5 +7,5 @@ xterm -title "ROSCORE" -e "roscore; bash" &
 sleep 4
 
 echo "Start up environment..."
-xterm -title "Gym-Environment" -e "python bin/ros-gym/launcher.py --environment \"TopoEnv\" --quiet; bash" &
+xterm -title "Gym-Environment" -e "python bin/ros-gym/launcher.py --environment \"TopoEnv\"; bash" &
 sleep 2
