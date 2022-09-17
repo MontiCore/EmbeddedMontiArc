a348939894d649d6094b213e0b1b4442543a1ffc
Anis Abdollahi-Sissan <anis.abdollahi@rwth-aachen.de>
2022-09-17 22:38:55 +0200

    Fix file transfer waiting loops (between PC and cluster)

--- src/main/cluster/toolchain/files/dyna.py
+++ src/main/cluster/toolchain/files/dyna.py
@@ -4,22 +4,23 @@ import sys
 import pathlib
 import subprocess
 
-time_to_wait = 120 # wait up to 2 minutes
+time_to_wait = 1200 # wait up to 20 minutes
 
 this_directory = pathlib.Path(__file__).parent.resolve()
 project_root = os.path.join(this_directory, '..', '..')
 signal_file = os.path.join(this_directory, 'Lattice_Structures', 'signalfile') # This file comes from the local pc and marks the end of the copy process of 1.k
 final_file = os.path.join(this_directory, 'Lattice_Structures', 'endfile') # This file is created by this script and indicates that lsdyna has finished
 
-time_counter = 0
-while not os.path.exists(signal_file): # Waiting for signalfile that indicates that 1.k has been copied to the cluster folder
-    time.sleep(1)
-    time_counter += 1
-    if time_counter > time_to_wait:
-        sys.exit("Local computer timed out!")
-subprocess.run(['make', 'dyna'], cwd=project_root)
-try:
-    with open(final_file, 'x') as final: # Write 'endfile' as a signal that dyna has finished processing the file from the local pc
-        pass
-except FileExistsError: # This should never happen, but if it happens, it means that the preprocessing did not correctly move and delete the 'endfile' from the previous iteration
-    sys.exit("The preprocessing did not remove toolchain/files/Lattice_Structures/endfile ! (please check copy_preprocessing.py and dyna.py)")
+while True:
+    time_counter = 0
+    while not os.path.exists(signal_file): # Waiting for signalfile that indicates that 1.k has been copied to the cluster folder
+        time.sleep(1)
+        time_counter += 1
+        if time_counter > time_to_wait:
+            sys.exit("Local computer timed out!")
+    subprocess.run(['make', 'dyna'], cwd=project_root)
+    try:
+        with open(final_file, 'x') as final: # Write 'endfile' as a signal that dyna has finished processing the file from the local pc
+            pass
+    except FileExistsError: # This should never happen, but if it happens, it means that the preprocessing did not correctly move and delete the 'endfile' from the previous iteration
+        sys.exit("The preprocessing did not remove toolchain/files/Lattice_Structures/endfile ! (please check copy_preprocessing.py and dyna.py)")
--- src/main/cluster/toolchain/files/dyna.sh
+++ src/main/cluster/toolchain/files/dyna.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mv $HOME/Dokumente/topologyoptimizer/toolchain/files/Input.csv $HOME/Dokumente/topologyoptimizer/toolchain/preprocessing/raw/
+rm $HOME/Dokumente/topologyoptimizer/toolchain/files/signalfile
 cd $HOME/Dokumente/topologyoptimizer/toolchain/files/Lattice_Structures
 
 $MPIEXEC $FLAGS_MPI_BATCH ls-dyna_mpp_s i=main.key
