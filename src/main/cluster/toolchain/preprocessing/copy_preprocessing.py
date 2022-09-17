6567d9e52355a2002f1c8fab762ad88347f9a1f5
Anis Abdollahi-Sissan <anis.abdollahi@rwth-aachen.de>
2022-09-17 18:36:15 +0200

    Remove unnecessary cleanup (already done by preprocessing-script)

--- src/main/cluster/toolchain/preprocessing/copy_preprocessing.py
+++ src/main/cluster/toolchain/preprocessing/copy_preprocessing.py
@@ -12,12 +12,12 @@ destination = os.path.join(cluster_path, "toolchain", "preprocessing", "raw", "r
 filtered_lsdyna = [file for file in lsdyna_files if
                    not file.endswith("main.key") and not file.endswith("1000001.key") and not file.endswith(
                        "Control_cards.key") and not file.endswith("runcommand.bat") and not file.endswith(
-                       "MAT_AA6014-T4_MAT24_kgmmms.key") and not file.endswith("clean.sh")]
+                       "MAT_AA6014-T4_MAT24_kgmmms.key")]
 
 for file in filtered_lsdyna:
     path_to_file = os.path.join(cluster_path, "toolchain", "files", "Lattice_Structures", file)
     shutil.copy(path_to_file, destination)
 
-for file in filtered_lsdyna:
+for file in filtered_lsdyna: # delete files from the origin
     path_to_file = os.path.join(cluster_path, "toolchain", "files", "Lattice_Structures", file)
     os.remove(path_to_file)
