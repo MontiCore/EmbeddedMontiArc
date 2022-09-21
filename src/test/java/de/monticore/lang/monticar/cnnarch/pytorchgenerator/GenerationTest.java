1b0f84935bb0888c431b4911ffa66a519075d3d5
sonam <sonam.chugh@rwth-aachen.de>
2022-09-21 23:56:56 +0200

    Added test cases for DGL and invalid architecture

--- src/test/java/de/monticore/lang/monticar/cnnarch/pytorchgenerator/GenerationTest.java
+++ src/test/java/de/monticore/lang/monticar/cnnarch/pytorchgenerator/GenerationTest.java
@@ -1,8 +1,10 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.lang.monticar.cnnarch.pytorchgenerator;
 
+import de.se_rwth.commons.logging.Finding;
 import de.se_rwth.commons.logging.Log;
 import freemarker.template.TemplateException;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,6 +14,7 @@ import java.io.IOException;
 
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -28,7 +31,7 @@ public class GenerationTest extends AbstractSymtabTest {
     }
 
     @Test
-    public void testLeNet() throws IOException, TemplateException {
+    public void testLeNetGeneration() throws IOException, TemplateException {
         Log.getFindings().clear();
         String[] args = {"-m", "src/test/resources/architectures", "-r", "LeNet", "-o", "./target/generated-sources-cnnarch/"};
         CNNArch2PyTorchCli.main(args);
@@ -41,6 +44,35 @@ public class GenerationTest extends AbstractSymtabTest {
                         "CNNNet_LeNet.py",
                         "CNNPredictor_LeNet.h",
                         "execute_LeNet"));
+    }
+
+    @Test
+    public void testCoraDglGeneration() {
+        Log.getFindings().clear();
+        String[] args = {"-m", "src/test/resources/architectures", "-r", "DGLNetwork", "-o", "./target/generated-sources-cnnarch/"};
+        CNNArch2PyTorchCli.main(args);
+        assertTrue(Log.getFindings().isEmpty());
+
+        checkFilesAreEqual(
+                Paths.get("./target/generated-sources-cnnarch"),
+                Paths.get("./src/test/resources/target_code/coraDgl"),
+                Arrays.asList(
+                        "CNNNet_DGLNetwork.py"
+                ));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testInvalidLayer() {
+        Log.getFindings().clear();
+        String[] args = {"-m", "src/test/resources/architectures", "-r", "LayerInvalid", "-o", "./target/generated-sources-cnnarch/"};
+        CNNArch2PyTorchCli.main(args);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testInvalidArchitecture() {
+        Log.getFindings().clear();
+        String[] args = {"-m", "src/test/resources/architectures", "-r", "ArchitectureInvalid", "-o", "./target/generated-sources-cnnarch/"};
+        CNNArch2PyTorchCli.main(args);
     }
 
 }
