b3aa5563191b196991966a732190cd05a4ea0473
Martin Fitzke <martin.fitzke@rwth-aachen.de>
2022-09-23 13:19:14 +0200

    updated Modular test to remove training hashes before running generator

--- src/test/java/de/monticore/lang/monticar/emadl/ModularTest.java
+++ src/test/java/de/monticore/lang/monticar/emadl/ModularTest.java
@@ -5,6 +5,9 @@ import de.se_rwth.commons.logging.Log;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -15,6 +18,15 @@ public class ModularTest extends AbstractSymtabTest {
         // ensure an empty log
         Log.getFindings().clear();
         Log.enableFailQuick(false);
+
+        /*
+        String[] hashPaths = {"target/singleNetwork","target/emptyNetwork","target/modularNetworkComplex","target/modularNetworkSimple"};
+        try {
+            removeMultipleTrainingHashes(hashPaths);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        */
     }
 
     public void runGenerator(String[] args, int expectedFindings, boolean exceptionAllowed){
@@ -32,44 +44,69 @@ public class ModularTest extends AbstractSymtabTest {
             if (!exceptionAllowed) assertFalse(true);
         }
 
-
     }
 
+
+
     @Test
-    public void testEmtpyNetwork() {
+    public void testEmtpyNetwork() throws IOException {
         Log.getFindings().clear();
+        removeTrainingHash("target/emptyNetwork");
         String[] args = {"-m", "src/test/resources/models/ModularMNIST", "-r", "emptyNetwork.Connector", "-o", "target", "-b", "GLUON", "-c", "y"};
         runGenerator(args,0,true);
 
     }
 
     @Test
-    public void testModularNetworkComplex() {
+    public void testModularNetworkComplex() throws IOException {
         Log.getFindings().clear();
+        removeTrainingHash("target/modularNetworkComplex");
         String[] args = {"-m", "src/test/resources/models/ModularMNIST", "-r", "modularNetworkComplex.Connector", "-o", "target", "-b", "GLUON", "-c", "y"};
         runGenerator(args,6,false);
     }
 
     @Test
-    public void testModularNetworkSimple() {
+    public void testModularNetworkSimple() throws IOException {
         Log.getFindings().clear();
+        removeTrainingHash("target/modularNetworkSimple");
         String[] args = {"-m", "src/test/resources/models/ModularMNIST", "-r", "modularNetworkSimple.Connector", "-o", "target", "-b", "GLUON", "-c", "y"};
         runGenerator(args,6,false);
     }
 
     @Test
-    public void testSingleNetwork() {
+    public void testSingleNetwork() throws IOException {
         Log.getFindings().clear();
+        removeTrainingHash("target/singleNetwork");
         String[] args = {"-m", "src/test/resources/models/ModularMNIST", "-r", "singleNetwork.Connector", "-o", "target", "-b", "GLUON", "-c", "y"};
-        runGenerator(args,6,false);
+        runGenerator(args,12,false);
     }
 
 
-    @Test
-    public void testCustomMNIST() {
-        Log.getFindings().clear();
-        String[] args = {"-m", "src/test/resources/models/customMNISTCalculator", "-r", "cNNCalculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y"};
-        runGenerator(args,6,true);
+    public void removeMultipleTrainingHashes(String[] hashPaths)throws IOException{
+        for (String path:hashPaths){
+            removeTrainingHash(path);
+        }
+
+    }
+
+    public void removeTrainingHash(String path) throws IOException{
+        if (path.equals("")) {
+            Log.info("Path empty","IO_INFO");
+            return;
+        }
+
+        File files = new File(path);
+        if (!files.exists()) {
+            Log.info("Path does not exist","IO_INFO");
+            return;
+        }
+
+        for (File file: files.listFiles()){
+            if (file.isDirectory())removeTrainingHash(file.getPath());
+
+            boolean success = file.delete();
+            if (!success) throw new IOException("File could not be deleted:" + file.toString());
+        }
     }
 
 
