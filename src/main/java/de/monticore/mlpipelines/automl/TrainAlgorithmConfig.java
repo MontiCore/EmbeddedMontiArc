279d7c7f43da640f58826ae32dd93fa525d2716f
Nazish Qamar <nazish.qamar@rwth-aachen.de>
2022-09-28 11:59:02 +0200

    merged TrainAlgorithmConfig

---
+++ src/main/java/de/monticore/mlpipelines/automl/TrainAlgorithmConfig.java
@@ -0,0 +1,11 @@
+package de.monticore.mlpipelines.pipeline.automl;
+
+import de.monticore.mlpipelines.pipeline.Pipeline;
+
+public class TrainAlgorithmConfig{
+    private class TrainAlgorithm;
+    int num_epochs = 1;
+    boolean save_trained_architechture = false;
+    String architecture_save_path = "";
+
+}
