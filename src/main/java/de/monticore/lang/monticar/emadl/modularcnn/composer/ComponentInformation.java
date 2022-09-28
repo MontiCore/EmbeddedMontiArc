62b7b2d10bdad55592c5498bab6761a604f9cde5
Martin Fitzke <martin.fitzke@rwth-aachen.de>
2022-09-29 00:12:56 +0200

    added working json reader -> headers added by maven

--- src/main/java/de/monticore/lang/monticar/emadl/modularcnn/composer/ComponentInformation.java
+++ src/main/java/de/monticore/lang/monticar/emadl/modularcnn/composer/ComponentInformation.java
@@ -1,5 +1,11 @@
 /**
  * (c) https://github.com/MontiCore/monticore
+ *
+ * The license generally applicable for this project
+ * can be found under https://github.com/MontiCore/monticore.
+ */
+/**
+ * (c) https://github.com/MontiCore/monticore
  * <p>
  * The license generally applicable for this project
  * can be found under https://github.com/MontiCore/monticore.
