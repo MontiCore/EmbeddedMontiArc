69cfe977710f2ed9432ddd5394585c07df8e9237
Nazish Qamar <nazish.qamar@rwth-aachen.de>
2022-09-28 12:34:43 +0200

    Update Configuration.java

--- src/main/java/de/monticore/mlpipelines/automl/Configuration.java
+++ src/main/java/de/monticore/mlpipelines/automl/Configuration.java
@@ -1,6 +1,6 @@
-package de.monticore.mlpipelines.pipeline.automl;
-
-import de.monticore.mlpipelines.pipeline.Pipeline;
+package de.monticore.mlpipelines.automl;
+import java.util.Hashtable;
+import de.monticore.mlpipelines.Pipeline;
 
 public class Configuration {
     Hashtable<String, String> preprocessing_config = new Hashtable<String, String>();
@@ -9,5 +9,5 @@ public class Configuration {
     Hashtable<String, String> network_config = new Hashtable<String, String>();
     Hashtable<String, String> initial_hyperparameters = new Hashtable<String, String>();
 
-    TrainAlgorithmConfig train_algorithm_config = new TrainAlgorithmConfig()
+    TrainAlgorithmConfig train_algorithm_config = new TrainAlgorithmConfig();
 }
