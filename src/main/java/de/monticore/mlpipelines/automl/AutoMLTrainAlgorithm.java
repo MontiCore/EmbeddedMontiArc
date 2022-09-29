ec246614bf8471b5a2a078f99a4b1470ea6e2843
Tobias Hörnschemeyer <tobias.hoernschemeyer@rwth-aachen.de>
2022-09-29 14:11:49 +0200

    Add test which loads model as an ArchitectureSsymbol

--- src/main/java/de/monticore/mlpipelines/automl/AutoMLTrainAlgorithm.java
+++ src/main/java/de/monticore/mlpipelines/automl/AutoMLTrainAlgorithm.java
@@ -1,11 +1,12 @@
 package de.monticore.mlpipelines.automl;
 
+import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
 import de.monticore.mlpipelines.Pipeline;
 
 public abstract class AutoMLTrainAlgorithm {
     private Class trainConfiguration;
     private Pipeline trainPipeline;
-    private Class startNetwork;
+    private ArchitectureSymbol startNetwork;
 
     public abstract void train();
 }
---
+++ src/test/java/de/monticore/mlpipelines/ImportModelTest.java
@@ -0,0 +1,31 @@
+package de.monticore.mlpipelines;
+
+import com.google.common.io.Resources;
+import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.instanceStructure.EMAComponentInstanceSymbol;
+import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
+import de.monticore.symboltable.Scope;
+import de.se_rwth.commons.logging.Log;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.net.URL;
+
+import static de.monticore.lang.monticar.emadl.generator.EMADLAbstractSymtab.createSymTab;
+
+public class ImportModelTest extends TestCase {
+
+    @Test
+    public void testLoadModelAsArchitectureSymbol() {
+        Log.getFindings().clear();
+        URL url = Resources.getResource("models/Add.emadl");
+        String s = url.toString();
+        Scope symTab = createSymTab("src/test/resources/models");
+        EMAComponentInstanceSymbol c = symTab.<EMAComponentInstanceSymbol>resolve("vGG16", EMAComponentInstanceSymbol.KIND).orElse(null);
+
+        ArchitectureSymbol arch1 = c.getSpannedScope().<ArchitectureSymbol>
+                resolve("", ArchitectureSymbol.KIND).get();
+
+        assertNotNull(arch1);
+
+    }
+}
