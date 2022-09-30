fcd2e4db8fcf5568ada5ab0e5d71f07508d275ca
Hiroshi Hamano <hiroshihamano1996@gmail.com>
2022-09-30 15:08:20 +0200

    Change attribute names by camelcase; Explicit type argument

--- src/main/java/de/monticore/mlpipelines/automl/Configuration.java
+++ src/main/java/de/monticore/mlpipelines/automl/Configuration.java
@@ -1,12 +1,14 @@
 package de.monticore.mlpipelines.automl;
+
 import java.util.Hashtable;
-import de.monticore.mlpipelines.Pipeline;
 
 public class Configuration {
-    Hashtable<String, String> preprocessing_config = new Hashtable<String, String>();
-    Hashtable<String, String> hyperparameter_optimizer_config = new Hashtable<String, String>();
-    Hashtable<String, String> evaluation_config = new Hashtable<String, String>();
-    Hashtable<String, String> network_config = new Hashtable<String, String>();
-    Hashtable<String, String> initial_hyperparameters = new Hashtable<String, String>();
-    TrainAlgorithmConfig train_algorithm_config = new TrainAlgorithmConfig();
+
+    Hashtable<String, String> preprocessingConfig = new Hashtable<>();
+    Hashtable<String, String> hyperparameterOptimizerConfig = new Hashtable<>();
+    Hashtable<String, String> evaluationConfig = new Hashtable<>();
+    Hashtable<String, String> networkConfig = new Hashtable<>();
+    Hashtable<String, String> initialHyperparameters = new Hashtable<>();
+    TrainAlgorithmConfig trainAlgorithmConfig = new TrainAlgorithmConfig();
+
 }
