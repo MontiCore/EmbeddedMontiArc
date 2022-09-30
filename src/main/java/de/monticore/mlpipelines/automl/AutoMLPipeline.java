7395cfb1bb2ed3a477b854fc44e67ee3c995bc71
Hiroshi Hamano <hiroshihamano1996@gmail.com>
2022-09-30 17:23:14 +0200

    Add example usage of the ConfFile2ConfigurationParser in AutoMLPipeline

--- src/main/java/de/monticore/mlpipelines/automl/AutoMLPipeline.java
+++ src/main/java/de/monticore/mlpipelines/automl/AutoMLPipeline.java
@@ -1,12 +1,20 @@
 package de.monticore.mlpipelines.automl;
 
 import de.monticore.mlpipelines.Pipeline;
+import de.monticore.mlpipelines.parser.ConfFile2ConfigurationParser;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class AutoMLPipeline extends Pipeline {
-    private Class configuration;
+    private Configuration configuration;
     private Pipeline trainPipeline;
 
     public void train(){
-
+        // Example usage of ConfFile2ConfigurationParser to get a Configuration object
+        Path modelPath = Paths.get("src/main/resources");
+        String modelName = "AutoMLExample.conf";
+        ConfFile2ConfigurationParser parser = new ConfFile2ConfigurationParser(modelPath, modelName);
+        this.configuration = parser.getConfiguration();
     }
 }
