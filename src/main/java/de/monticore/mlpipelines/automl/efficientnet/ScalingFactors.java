102f8a6fa16c121aad62e1522a92ea2c15b92039
Tobias Hörnschemeyer <tobias.hoernschemeyer@rwth-aachen.de>
2022-10-03 12:04:13 +0200

    Add ScalingFactors

--- src/main/java/de/monticore/mlpipelines/automl/efficientnet/ScalingFactors.java
+++ src/main/java/de/monticore/mlpipelines/automl/efficientnet/ScalingFactors.java
@@ -1,37 +1,13 @@
 package de.monticore.mlpipelines.automl.efficientnet;
 
 public class ScalingFactors {
-    private float alpha;
-    private float beta;
-    private float gamma;
+    public float alpha;
+    public float beta;
+    public float gamma;
 
     public ScalingFactors(float alpha, float beta, float gamma) {
         this.alpha = alpha;
         this.beta = beta;
-        this.gamma = gamma;
-    }
-
-    public float getAlpha() {
-        return alpha;
-    }
-
-    public void setAlpha(float alpha) {
-        this.alpha = alpha;
-    }
-
-    public float getBeta() {
-        return beta;
-    }
-
-    public void setBeta(float beta) {
-        this.beta = beta;
-    }
-
-    public float getGamma() {
-        return gamma;
-    }
-
-    public void setGamma(float gamma) {
         this.gamma = gamma;
     }
 }
