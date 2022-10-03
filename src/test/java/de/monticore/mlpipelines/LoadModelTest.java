2cd691a7f6c5f1a80e58ba6cb7ff3086cfaa9c0d
Tobias Hörnschemeyer <tobias.hoernschemeyer@rwth-aachen.de>
2022-10-03 10:27:46 +0200

    Add EfficientNetB0 layers

--- src/test/java/de/monticore/mlpipelines/LoadModelTest.java
+++ src/test/java/de/monticore/mlpipelines/LoadModelTest.java
@@ -1,10 +1,14 @@
 package de.monticore.mlpipelines;
 
 import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.instanceStructure.EMAComponentInstanceSymbol;
-import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
+import de.monticore.lang.monticar.cnnarch._symboltable.*;
+import de.monticore.symboltable.CommonScope;
 import de.monticore.symboltable.Scope;
 import junit.framework.TestCase;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static de.monticore.lang.monticar.emadl.generator.EMADLAbstractSymtab.createSymTab;
 
@@ -15,6 +19,12 @@ public class LoadModelTest extends TestCase {
         Scope symTab = createSymTab("src/test/resources/models");
         EMAComponentInstanceSymbol c = symTab.<EMAComponentInstanceSymbol>resolve("efficientNetB0",
                 EMAComponentInstanceSymbol.KIND).orElse(null);
+        List<? extends Scope> subScopes = c.getEnclosingScope().getSubScopes();
+        for (Scope s : subScopes) {
+            int symbolsSize = s.getSubScopes().get(0).getSymbolsSize();
+            System.out.println("Symbols size: " + symbolsSize);
+        }
+
         ArchitectureSymbol arch1 = c.getSpannedScope().<ArchitectureSymbol>resolve("", ArchitectureSymbol.KIND).get();
         assertNotNull(arch1);
     }
