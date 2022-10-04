324a21e125cec9b052ba5d00ccb5b142c9a5b48a
nazish-qamar <nazish-qamar@users.noreply.github.com>
2022-10-04 12:47:47 +0200

    Updating NetworkScaler class

--- src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/efficientnet/NetworkScaler.java
+++ src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/efficientnet/NetworkScaler.java
@@ -1,9 +1,64 @@
 package de.monticore.mlpipelines.automl.trainalgorithms.efficientnet;
 
 import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
+import de.monticore.mlpipelines.automl.configuration.EfficientNetConfig;
 
 public class NetworkScaler {
-    public ArchitectureSymbol scale(ArchitectureSymbol architectureSymbol, ScalingFactors scalingFactors) {
+
+    private float depth;
+    private float width;
+    private float resolution;
+
+    public ArchitectureSymbol scale(ArchitectureSymbol originalArchitectureSymbol, ScalingFactors scalingFactors) {
+        ArchitectureSymbol locArchitecture = originalArchitectureSymbol;
+        setDepth(scalingFactors.alpha);
+        setWidth(scalingFactors.beta);
+        setResolution(scalingFactors.gamma);
+
+
+        locArchitecture = scaleDepth(locArchitecture);
+        locArchitecture = scaleWidth(locArchitecture);
+        locArchitecture = scaleResolution(locArchitecture);
+
+        return locArchitecture;
+    }
+
+
+    private ArchitectureSymbol scaleDepth(ArchitectureSymbol architectureSymbol) {
+
         return architectureSymbol;
+    }
+
+    private ArchitectureSymbol scaleResolution(ArchitectureSymbol architectureSymbol) {
+        return architectureSymbol;
+    }
+
+    private ArchitectureSymbol scaleWidth(ArchitectureSymbol architectureSymbol) {
+
+        return architectureSymbol;
+    }
+
+    public float getDepth() {
+        return depth;
+    }
+
+    public void setDepth(float alpha) {
+        this.depth = (float)Math.pow(alpha, EfficientNetConfig.phi);
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float beta) {
+        this.width = (float)Math.pow(beta, EfficientNetConfig.phi);
+    }
+
+    public float getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(float gamma) {
+        this.resolution = (float)Math.pow(gamma, EfficientNetConfig.phi);
     }
 }
