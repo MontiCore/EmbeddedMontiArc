392cd9570f80b9c2a959231be3f70be0b0dfdb05
Anis Abdollahi-Sissan <anis.abdollahi@rwth-aachen.de>
2022-10-05 19:02:15 +0200

    Fix dyna execution on cluster for agent execution after training

--- src/main/cluster/rl/execute.sh
+++ src/main/cluster/rl/execute.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
 trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
 nohup roscore &
+sleep 2
+
+cd $HOME/Dokumente/topologyoptimizer/rl
+
+nohup python3 ../toolchain/files/dyna.py &
 sleep 2
 
 echo "Start up environment..."
