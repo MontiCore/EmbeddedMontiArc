5048bbd907c7d81937f21e5479bbc0600880676a
Anis Abdollahi-Sissan <anis.abdollahi@rwth-aachen.de>
2022-10-05 17:31:19 +0200

    Secure writing of Input.csv file, add logging

--- src/main/cluster/rl/bin/ros-gym/dynawrapper.py
+++ src/main/cluster/rl/bin/ros-gym/dynawrapper.py
@@ -3,16 +3,20 @@ import numpy as np
 import os
 import time
 import sys
+import shutil
 import pathlib
 import subprocess
 
 this_directory = pathlib.Path(__file__).parent.resolve()
 project_root = os.path.join(this_directory, '..', '..', '..')
 input_file = os.path.join(this_directory, '..', '..', '..', 'toolchain', 'files', 'Input.csv')
+input_preprocessing = os.path.join(this_directory, '..', '..', '..', 'toolchain', 'preprocessing', 'raw', 'Input.csv')
 final_file = os.path.join(this_directory, '..', '..', '..', 'toolchain', 'files', 'Lattice_Structures', 'endfile')
 processed_file = os.path.join(this_directory, '..', '..', '..', 'toolchain', 'preprocessing', 'h5', 'raw', 'train.h5')
 
-time_to_wait = 120 # Wait up to 2 minutes
+log_file = os.path.join(project_root, 'logs', 'dynawrapper.log')
+
+time_to_wait = 1200 # Wait up to 20 minutes
 
 class DynaWrapper(object):
     def __init__(self, material):
@@ -22,12 +26,17 @@ class DynaWrapper(object):
         self.distance = np.zeros(self.limit)
 
     def simulate(self):
+        logging = open(log_file, 'a')
+        logging.write("Starting simulate \n")
         x,y,z,rx,ry,rz = self.material
         stringlist = [str(x),str(y),str(z),str(rx),str(ry),str(rz)]
         materialString = ",".join(stringlist)
-        with open(input_file, 'w') as csvFile:
+        with open(input_preprocessing, 'w') as csvFile:
             csvFile.write(r'1,100,0,' + materialString)
-
+        logging.write("Written to preprocessing: " + materialString + "\n")
+        shutil.copyfile(input_preprocessing, input_file)
+        logging.write("Copied Input.csv to files \n")
+        logging.flush()
         # The csv file will be automatically copied to the local PC, processed by nTopology, and copied to the cluster
 
         # LSDyna will automatically process the file created by nTopology
@@ -39,25 +48,49 @@ class DynaWrapper(object):
             time_counter += 1
             if time_counter > time_to_wait:
                 sys.exit("LSDyna timed out!")
-
+        logging.write("RL received dyna output \n")
+        logging.flush()
         subprocess.run(['make', 'preprocessing'], cwd=project_root)
+        logging.write("Preprocessing finished \n")
         file = h5py.File(processed_file, 'r')
         force_all = file["fd_label"][0][1] # force
         displacement_all = file["fd_label"][0][0] # displacement
         self.force = np.array(force_all[:self.limit])
         self.distance = np.array(displacement_all[:self.limit])
+        logging.write("force is: " + str(self.force) + "\n")
+        logging.write("distance is: " + str(self.distance) + "\n")
         subprocess.run(['make', 'clean'], cwd=project_root)
+        logging.write("Clean finished, continue RL \n")
+        logging.close()
 
     def integrate(self):
+        logging = open(log_file, 'a')
+        logging.write("integrate \n")
+        logging.close()
         return np.trapz(self.force, self.distance)
 
     def getPeak(self):
+        logging = open(log_file, 'a')
+        logging.write("peak \n")
+        logging.close()
         return np.amax(self.force)
 
     def getPeakPos(self): # starts at 0!!!
+        logging = open(log_file, 'a')
+        logging.write("peakpos \n")
+        logging.close()
         return np.argmax(self.force)
 
     def getFinalLevel(self):
+        logging = open(log_file, 'a')
         peak_pos = self.getPeakPos()
+        logging.write("final level with peakpos: " + str(peak_pos) + "\n")
+        logging.close()
         reduced = self.force[peak_pos:]
         return np.average(reduced)
+
+    def updateMaterial(self, material):
+        logging = open(log_file, 'a')
+        logging.write("Update material: " + str(material) + "\n")
+        logging.close()
+        self.material = material
