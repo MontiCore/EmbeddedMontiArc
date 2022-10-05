c9dcafcf56e13de11299530f187a5c8fc7b71711
Anis Abdollahi-Sissan <anis.abdollahi@rwth-aachen.de>
2022-10-05 18:37:55 +0200

    Path fix

--- src/main/cluster/toolchain/files/dyna.sh
+++ src/main/cluster/toolchain/files/dyna.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-mv $HOME/Dokumente/topologyoptimizer/toolchain/files/Input.csv $HOME/Dokumente/topologyoptimizer/toolchain/preprocessing/raw/
-rm $HOME/Dokumente/topologyoptimizer/toolchain/files/signalfile
+rm $HOME/Dokumente/topologyoptimizer/toolchain/files/Lattice_Structures/signalfile
 cd $HOME/Dokumente/topologyoptimizer/toolchain/files/Lattice_Structures
 
 $MPIEXEC $FLAGS_MPI_BATCH ls-dyna_mpp_s i=main.key
