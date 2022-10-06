f7e6f2e9ee743dfd5f3274a5de161e7021744a11
Akash Kumar <Akumar@clinomic.ai>
2022-10-06 18:11:46 +0200

    New classes: hyperband,BOHB,DEHB

---
+++ src/main/java/de/monticore/mlpipelines/automl/hyperparameters/parallel/DEHBAlgorithm.java
@@ -0,0 +1,27 @@
+package de.monticore.mlpipelines.automl.hyperparameters.parallel;
+
+public class DEHBAlgorithm extends ParallelAlgorithm{
+    private double R;
+    private double eta;
+    private double n;
+    private double b;
+
+    public void de_optimizer() {
+        // TODO: Implement method
+    }
+
+    public void de_successive_halving() {
+        // TODO: Implement method
+    }
+    public void dehb() {
+        // TODO: Implement method
+    }
+
+    public void validation_loss() {
+        // TODO: Implement method
+    }
+
+    public void top_configurations() {
+        // TODO: Implement method
+    }
+}
