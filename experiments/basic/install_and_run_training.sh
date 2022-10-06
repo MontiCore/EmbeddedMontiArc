d11d4293f93a205ccf0fe737d76acc34b26facba
Til Valentin Mohr <til.mohr@rwth-aachen.de>
2022-10-06 17:51:05 +0200

    Changed Files to Symbolic Links & Updated Readme

--- experiments/basic/install_and_run_training.sh
+++ experiments/basic/install_and_run_training.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-. /opt/ros/noetic/setup.sh
-
-. ./config.sh
-./install.sh
-
-if [[ "$*" == *"-sp"* ]]
-then
-	/bin/echo "STARTING INITIAL RUN"
-	./initial_run_training.sh "$@"
-else
-	./run_training.sh "$@"
-fi
-
-sleep 1
+../../Autopilot/install_and_run_training.sh
--- experiments/basic/tools/copy_params.sh
+++
@@ -1,5 +0,0 @@
-cd ../target/agent/src/de_rwth_montisim_agent_master/cpp/model/de.rwth.montisim.agent.network.AutopilotQNet/AutopilotAgent
-LATEST=$(ls -t | head -n 1)
-cd ../../../../../../..
-cp agent/src/de_rwth_montisim_agent_master/cpp/model/de.rwth.montisim.agent.network.AutopilotQNet/AutopiletAgent/${LATEST}/best_network-0000.params bin/model/de.rwth.montisim.agent.network.AutopilotQNet/model_0_newest-0000.params
-cp agent/src/de_rwth_montisim_agent_master/cpp/model/de.rwth.montisim.agent.network.AutopilotQNet/AutopiletAgent/${LATEST}/best_network-symbol.json bin/model/de.rwth.montisim.agent.network.AutopilotQNet/model_0_newest-symbol.json 
