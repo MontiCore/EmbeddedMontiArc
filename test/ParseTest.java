7c8591480c788a1e909f8a0b0cefd81106ba473f
Matthias Nadenau <manadenau@web.de>
2022-10-07 16:44:59 +0200

    fix typo

--- test/ParseTest.java
+++ test/ParseTest.java
@@ -15,7 +15,7 @@ public class ParseTest {
     
     @Test
     public void testChessTyped() throws Exception {
-        Interpreter interpreter = Interpreter.fromGDLFile("Chess/ChessTyped.gdl", new InterpreterOptions().withTypes(true));
+        Interpreter interpreter = Interpreter.fromGDLFile("ChessTyped/ChessTyped.gdl", new InterpreterOptions().withTypes(true));
         assertTrue(interpreter.isWithTypes());
         assertTrue(interpreter.getStateSpaceDimension() > 0);
         assertTrue(interpreter.getActionSpaceDimension() > 0);
