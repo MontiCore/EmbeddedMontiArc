5aa45d43ce40cfb7b3a6f67fcb6fa3933f27f12f
Matthias Nadenau <manadenau@web.de>
2022-10-07 16:28:54 +0200

    models w/ ci tests

---
+++ test/ParseTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.monticore.lang.gdl.Interpreter;
+import de.monticore.lang.gdl.InterpreterOptions;
+
+public class ParseTest {
+    
+    @Test
+    public void testDoppelkopf() throws Exception {
+        Interpreter interpreter = Interpreter.fromGDLFile("TicTacToe/TicTacToe.gdl", new InterpreterOptions());
+        interpreter.close();
+    }
+    
+    @Test
+    public void testDoppelkopfTyped() throws Exception {
+        Interpreter interpreter = Interpreter.fromGDLFile("TicTacToeTyped/TicTacToeTyped.gdl", new InterpreterOptions().withTypes(true));
+        assertTrue(interpreter.isWithTypes());
+        assertTrue(interpreter.getStateSpaceDimension() > 0);
+        assertTrue(interpreter.getActionSpaceDimension() > 0);
+        interpreter.close();
+    }
+
+}
