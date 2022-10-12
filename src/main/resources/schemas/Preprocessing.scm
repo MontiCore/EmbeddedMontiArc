80fc33fb95bc85ef8040208610ec36905aca4a4d
Hiroshi Hamano <hiroshihamano1996@gmail.com>
2022-10-12 18:09:23 +0200

    Add scm files for AutoMLPipeline

---
+++ src/main/resources/schemas/Preprocessing.scm
@@ -0,0 +1,10 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+schema Preprocessing {
+
+    train_split: Q
+    norm_method: string
+    grayscale: B
+    data_augmentation: B
+
+}
---
+++ src/main/resources/schemas/TrainAlgorithm.scm
@@ -0,0 +1,10 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+schema TrainAlgorithm {
+
+    save_trained_architecture: B
+    architecture_save_path: string
+    train_algorithm_name: string
+    train_pipeline_name: string
+
+}
