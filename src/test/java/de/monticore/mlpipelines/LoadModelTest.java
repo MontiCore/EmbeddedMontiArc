afb9e4fb84324d4c9cb6255bf65b66d0978cda39
nazish-qamar <nazish-qamar@users.noreply.github.com>
2022-10-14 10:40:51 +0200

    Updating the layer content in network instructions

--- src/test/java/de/monticore/mlpipelines/LoadModelTest.java
+++ src/test/java/de/monticore/mlpipelines/LoadModelTest.java
@@ -1,12 +1,16 @@
 package de.monticore.mlpipelines;
 
+import de.monticore.ast.ASTNode;
 import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.instanceStructure.EMAComponentInstanceSymbol;
+import de.monticore.lang.math._symboltable.expression.MathNumberExpressionSymbol;
 import de.monticore.lang.monticar.cnnarch._symboltable.*;
 import de.monticore.symboltable.Scope;
 import junit.framework.TestCase;
+import org.jscience.mathematics.number.LargeInteger;
+import org.jscience.mathematics.number.Rational;
 import org.junit.Test;
 
-import java.util.List;
+import java.util.*;
 
 import static de.monticore.lang.monticar.emadl.generator.EMADLAbstractSymtab.createSymTab;
 
@@ -17,7 +21,35 @@ public class LoadModelTest extends TestCase {
         Scope symTab = createSymTab("src/test/resources/models");
         EMAComponentInstanceSymbol c = symTab.<EMAComponentInstanceSymbol>resolve("efficientNetB0",
                 EMAComponentInstanceSymbol.KIND).orElse(null);
+
         ArchitectureSymbol arch1 = c.getSpannedScope().<ArchitectureSymbol>resolve("", ArchitectureSymbol.KIND).get();
+
+        List<NetworkInstructionSymbol> networkInstructions = arch1.getNetworkInstructions();
+        for (NetworkInstructionSymbol networkInstruction : networkInstructions) {
+            SerialCompositeElementSymbol networkInstructionBody = networkInstruction.getBody();
+            List<ArchitectureElementSymbol> elements = networkInstructionBody.getElements();
+            for (ArchitectureElementSymbol element : elements) {
+                Optional<ASTNode> nodeOptional = element.getAstNode();
+                ArchitectureElementScope spannedscope  =  elements.get(2).getSpannedScope();
+                ArrayList inner_element = (ArrayList) spannedscope.getLocalSymbols().get("");
+                MathNumberExpressionSymbol oldValue = (MathNumberExpressionSymbol) inner_element.get(3);
+
+                Rational num = oldValue.getValue().getRealNumber();
+
+                if(element.getName().equals("residualBlock")) {
+                    oldValue.getValue().setRealNumber(num.times(2));
+                }
+
+                 /*if(astLayer.getName().equals("residualBlock")){
+                    ASTArchExpression expr_rhs = astLayer.getArguments(0).getRhs();
+                    ArchSimpleExpressionSymbol rhs_symbol = (ArchSimpleExpressionSymbol) expr_rhs.getSymbol();
+                    Optional<MathExpressionSymbol> mathexpression  = rhs_symbol.getMathExpression();
+                    MathNumberExpressionSymbol symbol = (MathNumberExpressionSymbol) mathexpression.get();
+                    System.out.println(symbol.getValue());
+                }*/
+            }
+        }
+
         assertNotNull(arch1);
     }
 }
