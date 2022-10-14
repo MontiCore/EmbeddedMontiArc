6fa050c88357f2035d91d7ea7978045395d51ac3
nazish-qamar <nazish-qamar@users.noreply.github.com>
2022-10-14 12:26:41 +0200

    NetworkScaler and ModelLoader Changes

--- src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/efficientnet/NetworkScaler.java
+++ src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/efficientnet/NetworkScaler.java
@@ -1,7 +1,12 @@
 package de.monticore.mlpipelines.automl.trainalgorithms.efficientnet;
 
-import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
+import de.monticore.lang.math._symboltable.expression.MathNumberExpressionSymbol;
+import de.monticore.lang.monticar.cnnarch._symboltable.*;
 import de.monticore.mlpipelines.automl.configuration.EfficientNetConfig;
+import org.jscience.mathematics.number.Rational;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NetworkScaler {
 
@@ -9,33 +14,57 @@ public class NetworkScaler {
     private float widthFactor = 0;
     private float resolutionFactor = 0;
 
-    private ArchitectureSymbol locArchitectureSymbol = null;
+    private ArchitectureSymbol architecture = null;
 
-    public ArchitectureSymbol scale(ArchitectureSymbol originalArchitectureSymbol, ScalingFactors scalingFactors, int phi) {
-        setLocArchitectureSymbol(originalArchitectureSymbol);
+    public ArchitectureSymbol scale(ArchitectureSymbol originalArchitecture, ScalingFactors scalingFactors, int phi) {
+        setArchitectureSymbol(originalArchitecture);
         setDepth(scalingFactors.alpha);
         setWidth(scalingFactors.beta);
         setResolution(scalingFactors.gamma);
 
+        if(this.architecture.getNetworkInstructions().isEmpty())
+            return originalArchitecture;
+
         scaleDepth();
         scaleWidth();
         scaleResolution();
-
-        return getLocArchitectureSymbol();
+        return getArchitectureSymbol();
     }
 
 
     private void scaleDepth() {
-        //for stem
-        //repeat layers depthFactor times
+        NetworkInstructionSymbol networkInstruction = this.architecture.getNetworkInstructions().get(0);
+        SerialCompositeElementSymbol networkInstructionBody = networkInstruction.getBody();
+        List<ArchitectureElementSymbol> architectureElements = networkInstructionBody.getElements();
 
-        //for other blocks
-        //repeat layers depthFactor times
+        //for residualBlock depth scaling
+        for(ArchitectureElementSymbol architectureElement : architectureElements){
+            if(!architectureElement.getName().equals("residualBlock"))
+                continue;
+
+            ArchitectureElementScope spannedScope  =  architectureElement.getSpannedScope();
+            ArrayList expressions = (ArrayList) spannedScope.getLocalSymbols().get(""); //the MathNumberExpressionSymbol is in the key ""
+
+            MathNumberExpressionSymbol expression = (MathNumberExpressionSymbol) expressions.get(3); //directly fetching the residualBlock
+            Rational oldValue = expression.getValue().getRealNumber();
+            Rational newValue = oldValue.times((long) this.depthFactor);
+            expression.getValue().setRealNumber(newValue);
+        }
     }
 
     private void scaleWidth() {
-        //for only stem
-        //kernel = (widthFactor * filter, widthFactor * filter);
+        NetworkInstructionSymbol networkInstruction = this.architecture.getNetworkInstructions().get(0);
+        SerialCompositeElementSymbol networkInstructionBody = networkInstruction.getBody();
+        List<ArchitectureElementSymbol> architectureElements = networkInstructionBody.getElements();
+
+        //for residualBlock width scaling
+        for(ArchitectureElementSymbol architectureElement : architectureElements){
+            if(!architectureElement.getName().equals("residualBlock"))
+                continue;
+
+            ArchitectureElementScope spannedScope  =  architectureElement.getSpannedScope();
+            ArrayList expressions = (ArrayList) spannedScope.getLocalSymbols().get(""); //the MathNumberExpressionSymbol is in the key ""
+        }
     }
 
 
@@ -47,7 +76,7 @@ public class NetworkScaler {
         return depthFactor;
     }
 
-    public void setDepth(float alpha) {
+    private void setDepth(float alpha) {
         this.depthFactor = (float)Math.pow(alpha, EfficientNetConfig.phi);
     }
 
@@ -55,7 +84,7 @@ public class NetworkScaler {
         return widthFactor;
     }
 
-    public void setWidth(float beta) {
+    private void setWidth(float beta) {
         this.widthFactor = (float)Math.pow(beta, EfficientNetConfig.phi);
     }
 
@@ -63,15 +92,15 @@ public class NetworkScaler {
         return resolutionFactor;
     }
 
-    public void setResolution(float gamma) {
+    private void setResolution(float gamma) {
         this.resolutionFactor = (float)Math.pow(gamma, EfficientNetConfig.phi);
     }
 
-    public ArchitectureSymbol getLocArchitectureSymbol() {
-        return locArchitectureSymbol;
+    public ArchitectureSymbol getArchitectureSymbol() {
+        return architecture;
     }
 
-    public void setLocArchitectureSymbol(ArchitectureSymbol architectureSymbol) {
-        this.locArchitectureSymbol = architectureSymbol;
+    private void setArchitectureSymbol(ArchitectureSymbol architectureSymbol) {
+        this.architecture = architectureSymbol;
     }
 }
