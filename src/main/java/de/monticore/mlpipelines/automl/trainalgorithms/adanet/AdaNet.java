c562d86cbb51e3639394bb8e748c3cd7045bf819
Tobias Hörnschemeyer <tobias.hoernschemeyer@rwth-aachen.de>
2022-10-15 12:47:52 +0200

    Add AdaNet pseudoCode

--- src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/adanet/AdaNet.java
+++ src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/adanet/AdaNet.java
@@ -9,6 +9,16 @@ public class AdaNet extends TrainAlgorithm {
 
     @Override
     public void train(ArchitectureSymbol startNetwork) {
-
+        // For at most n iterations:
+        //    generate possible components (Exactly the same amount of layers as the biggest component in the network or one more)
+        //    for all components:
+        //        add component to network in parallel
+        //        train network
+        //        evaluate network
+        //        remove component from network
+        //    select best component
+        //    add best component to network in parallel
+        //    if network with best component is not better than network without best component:
+        //        stop adaNet and return network without best component
     }
 }
