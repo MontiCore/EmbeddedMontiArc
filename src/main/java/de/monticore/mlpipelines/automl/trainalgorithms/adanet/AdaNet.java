809f045ecd4890fdad35a04a5562677d7fc76ea7
Tobias Hörnschemeyer <tobias.hoernschemeyer@rwth-aachen.de>
2022-10-19 11:16:21 +0200

    Modify AdaNet

--- src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/adanet/AdaNet.java
+++ src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/adanet/AdaNet.java
@@ -4,37 +4,55 @@ import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
 import de.monticore.mlpipelines.automl.configuration.AdaNetConfig;
 import de.monticore.mlpipelines.automl.trainalgorithms.TrainAlgorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// For at most n iterations:
+//    generate possible components (Exactly the same amount of layers as the biggest component in the network or one more)
+//    for all components:
+//        add component to network in parallel
+//        train network
+//        evaluate network
+//        remove component from network
+//    select best component
+//    add best component to network in parallel
+//    if network with best component is not better than network without best component:
+//        stop adaNet and return network without best component
+
 public class AdaNet extends TrainAlgorithm {
+    int lastStepBestComponentsDepth = 1;
     private ArchitectureSymbol scaledArchitecture;
     private CandidateSearch candidateSearch;
-    int lastStepBestComponentsDepth = 1;
 
-    public AdaNet(ScalingFactorsGridSearch gridSearch, NetworkScaler networkScaler) {
+    public AdaNet(CandidateSearch candidateSearch) {
         super();
-        this.candidateSearch = canditateSearch;
+        this.candidateSearch = candidateSearch;
     }
 
 
     @Override
     public void train(ArchitectureSymbol startNetwork) {
-        // For at most n iterations:
-        //    generate possible components (Exactly the same amount of layers as the biggest component in the network or one more)
-        //    for all components:
-        //        add component to network in parallel
-        //        train network
-        //        evaluate network
-        //        remove component from network
-        //    select best component
-        //    add best component to network in parallel
-        //    if network with best component is not better than network without best component:
-        //        stop adaNet and return network without best component
-        for (int i = 0; i < AdaNetConfig.MAX_ITERATIONS; i++) {
+        setStartNetwork(startNetwork);
+        for (int i = 1; i < AdaNetConfig.MAX_ITERATIONS; i++) {
+            AdaNetComponent bestComponent = null;
+            List<AdaNetComponent> adanetComponents = generatePossibleComponents(startNetwork);
+            bestComponent = selectBestComponent(startNetwork, adanetComponents);
+            //call train for all components
 
+            addComponentToNetwork(bestComponent);
         }
     }
 
-    private void generatePossibleComponents(ArchitectureSymbol startNetwork) {
+    private List<AdaNetComponent> generatePossibleComponents(ArchitectureSymbol startNetwork) {
+        List<AdaNetComponent> generatedPossibleComponents = new ArrayList<>();
+        int minDepth = lastStepBestComponentsDepth;
+        int maxDepth = minDepth + 1;
+        for (int i = minDepth; i < maxDepth + 1; i++) {
+            AdaNetComponent component = new AdaNetComponent(i);
+            generatedPossibleComponents.add(component);
+        }
 
+        return generatedPossibleComponents;
     }
 
     private AdaNetComponent selectBestComponent(
@@ -42,6 +60,8 @@ public class AdaNet extends TrainAlgorithm {
             List<AdaNetComponent> adanetComponents) {
         AdaNetComponent bestComponent = adanetComponents.get(0);
 
+        //check the best component
+        return bestComponent;
     }
 
     private void trainAndEvaluateNetwork(ArchitectureSymbol startNetwork) {
