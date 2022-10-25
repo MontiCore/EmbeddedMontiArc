17e568a1909e57540012c5b4e3b8fbafc5b6ecd0
Tobias Hörnschemeyer <tobias.hoernschemeyer@rwth-aachen.de>
2022-10-25 11:48:28 +0200

    Generate emadl string in AdaNetCandidate

--- src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/adanet/AdaNetCandidate.java
+++ src/main/java/de/monticore/mlpipelines/automl/trainalgorithms/adanet/AdaNetCandidate.java
@@ -35,6 +35,35 @@ public class AdaNetCandidate implements EmadlAble {
         this.previousComponents = previousComponents;
     }
 
+    @Override
+    public List<String> getEmadl() {
+        List<String> emadl = new ArrayList<>();
+        return emadl;
+    }
+
+    private void addLayerToCandidateContent(List<String> candidateContent, int layerIndex) {
+        int numberComponentsInLayer = componentsInLayer(layerIndex);
+        if (numberComponentsInLayer > 0) {
+            candidateContent.add("FullyConnected(units=layerWidth)->");
+        } else {
+            candidateContent.add(
+                    String.format("FullyConnected(| = %d, units=layerWidth)->", componentsInLayer(layerIndex)));
+            candidateContent.add("Concatenate() ->");
+        }
+    }
+
+    private int componentsInLayer(int layer) {
+        int layerDepth = layer + 1;
+        int numberComponents = getAllComponents().size();
+        for (int componentIndex = layer; componentIndex < numberComponents; componentIndex++) {
+            if (componentIsInLayer(layerDepth, componentIndex)) {
+                return numberComponents - componentIndex;
+            }
+        }
+
+        return 0;
+    }
+
     public List<AdaNetComponent> getAllComponents() {
         List<AdaNetComponent> allComponents = new ArrayList<>();
         allComponents.addAll(this.previousComponents);
@@ -42,19 +71,8 @@ public class AdaNetCandidate implements EmadlAble {
         return allComponents;
     }
 
-    @Override
-    public List<String> getEmadl() {
-        List<String> emadl = new ArrayList<>();
-        return emadl;
-    }
-
-    public List<String> createCanditateContent(int maxDepth) {
-        List<String> candidateContent = new ArrayList<>();
-        for (int i = 0; i < maxDepth; i++) {
-            candidateContent.add(String.format("        FullyConnected(| = %d, units=layerWidth)->", i + 1));
-            candidateContent.add("        Concatenate() ->");
-        }
-        return candidateContent;
+    private boolean componentIsInLayer(int layerDepth, int componentIndex) {
+        return getAllComponents().get(componentIndex).getDepth() >= layerDepth;
     }
 
     private int getMaxDepth() {
