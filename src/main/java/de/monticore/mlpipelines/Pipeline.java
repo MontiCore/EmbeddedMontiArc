7eaa6413b9b82a6f968833f19c63f9ff81179ff5
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-10-25 13:07:03 -0700

    Keep only master thesis relevant work

--- src/main/java/de/monticore/mlpipelines/Pipeline.java
+++ src/main/java/de/monticore/mlpipelines/Pipeline.java
@@ -1,19 +1,4 @@
 package de.monticore.mlpipelines;
 
-import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
-import de.monticore.mlpipelines.automl.configuration.Configuration;
-import de.monticore.mlpipelines.automl.configuration.TrainAlgorithmConfig;
-
 public abstract class Pipeline {
-    private float trainedAccuracy;
-
-    public float getTrainedAccuracy() {
-        return trainedAccuracy;
-    }
-
-    public void setTrainedAccuracy(float trainedAccuracy) {
-        this.trainedAccuracy = trainedAccuracy;
-    }
-
-    public abstract void train(ArchitectureSymbol architecture, Configuration configuration);
 }
--- src/main/java/de/monticore/mlpipelines/python/PythonPipeline.java
+++ src/main/java/de/monticore/mlpipelines/python/PythonPipeline.java
@@ -1,16 +1,7 @@
 package de.monticore.mlpipelines.python;
 
-import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
 import de.monticore.mlpipelines.Pipeline;
-import de.monticore.mlpipelines.automl.configuration.Configuration;
 
 public class PythonPipeline extends Pipeline {
-    public PythonPipeline() {
-        super();
-    }
 
-    @Override
-    public void train(ArchitectureSymbol architecture, Configuration configuration) {
-
-    }
 }
