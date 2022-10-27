6310937d59e2d598e1cf1c478a2b7d4c7fbab232
Ahmed Diab <ahmed.diab@rwth-aachen.de>
2022-10-27 23:34:19 +0200

    add build scripts

---
+++ make.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#make and install the agent and the preprocessor
+
+dos2unix *.sh
+#source /ros_entrypoint.sh
+#source /gzserver_entrypoint.sh 
+./install.sh
---
+++ run_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "Start the simulator"
+roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch
+
+echo "Start up environment..."
+python bin/roslauncher.py --environment \"gazeboEnv\"
+
+echo "Start up preprocessor..."
+cd "${PROJECT_ROOT}/${BINARY}"
+./preprocessor -executeOnDemand &
