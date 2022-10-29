da138feb596c88710e28433d657426ecede852a6
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-10-28 18:43:30 -0700

    add test class for frontend

---
+++ src/test/java/de/monticore/mlpipelines/workflow/FrontendTest.java
@@ -0,0 +1,22 @@
+package de.monticore.mlpipelines.workflow;
+
+import org.junit.jupiter.api.Test;
+
+class FrontendTest {
+
+    @Test
+    void parseTrainingConfiguration() {
+    }
+
+    @Test
+    void parsePipelineConfiguration() {
+    }
+
+    @Test
+    void parsePipelineReferenceModelToEMAComponent() {
+    }
+
+    @Test
+    void addExecutionSemanticsToEmaComponent() {
+    }
+}
