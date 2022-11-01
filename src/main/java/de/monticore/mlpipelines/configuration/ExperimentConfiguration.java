edc5b21e1b49be5142addc58258473f3d6628663
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-11-01 06:01:01 -0700

    Make experiment configuration accessible

--- src/main/java/de/monticore/mlpipelines/configuration/ExperimentConfiguration.java
+++ src/main/java/de/monticore/mlpipelines/configuration/ExperimentConfiguration.java
@@ -2,13 +2,26 @@ package de.monticore.mlpipelines.configuration;
 
 public class ExperimentConfiguration {
 
-    public final  String pathToTrainingConfiguration;
-    public final  String pathToPipelineImplementations;
-    public final  String pathToExecutionScript;
+    private final  String pathToTrainingConfiguration;
 
+    private final  String pathToPipelineImplementations;
+
+    private final  String pathToExecutionScript;
     public ExperimentConfiguration(final String pathToTrainingConfiguration, final String pathToPipelineImplementations, final String pathToExecutionScript) {
         this.pathToTrainingConfiguration = pathToTrainingConfiguration;
         this.pathToPipelineImplementations = pathToPipelineImplementations;
         this.pathToExecutionScript = pathToExecutionScript;
+    }
+
+    public String getPathToTrainingConfiguration() {
+        return pathToTrainingConfiguration;
+    }
+
+    public String getPathToPipelineImplementations() {
+        return pathToPipelineImplementations;
+    }
+
+    public String getPathToExecutionScript() {
+        return pathToExecutionScript;
     }
 }
--- src/main/java/de/monticore/mlpipelines/configuration/MontiAnnaContext.java
+++ src/main/java/de/monticore/mlpipelines/configuration/MontiAnnaContext.java
@@ -1,6 +1,5 @@
 package de.monticore.mlpipelines.configuration;
 
-import de.monticore.lang.monticar.cnnarch.generator.training.LearningMethod;
 import de.monticore.lang.monticar.emadl.generator.Backend;
 
 public final class MontiAnnaContext {
@@ -10,15 +9,12 @@ public final class MontiAnnaContext {
     private static MontiAnnaContext montiAnnaConfiguration;
     private final String pipelineReferenceModelsPath = "";
 
-    private LearningMethod learningMethod;
-
     private ExperimentConfiguration experimentConfiguration;
 
     private final Backend targetBackend = Backend.PYTORCH;
+
     private MontiAnnaContext() {
     }
-
-    private MontiAnnaContext() {}
 
     public static MontiAnnaContext getInstance() {
         if (montiAnnaConfiguration == null) {
@@ -39,9 +35,14 @@ public final class MontiAnnaContext {
         return targetBackend;
     }
 
-    public void initContext(final String parentModelPath, final String rootModelName) {
+    public ExperimentConfiguration getExperimentConfiguration() {
+        return experimentConfiguration;
+    }
+
+    public void initContext(final String parentModelPath, final String rootModelName, final ExperimentConfiguration experimentConfiguration) {
         getInstance().parentModelPath = parentModelPath;
         getInstance().rootModelName = rootModelName;
+        getInstance().experimentConfiguration = experimentConfiguration;
     }
 
     public String getPipelineReferenceModelsPath() {
