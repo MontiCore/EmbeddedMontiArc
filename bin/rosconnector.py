a6038377d1fe90859d620ad883a32e9eeab36588
Ahmed Diab <ahmed.diab@rwth-aachen.de>
2022-11-07 12:14:32 +0100

    update rosconnecttor

--- bin/rosconnector.py
+++ bin/rosconnector.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from pickle import FALSE
 import rospy
 import time
 import numpy as np
@@ -22,10 +21,14 @@ class RosConnector(object):
     position_topic = '/gazebo/position'
     ros_update_rate = 10
     
-    RANDOM_START_POS = FALSE
+    RANDOM_START_POS = False
     X_INIT = -0.4
     Y_INIT = -0.4
     THETA_INIT = 45.0
+    
+    X_GOAL = 2
+    Y_GOAL = -1
+    THETA_GOAL = -30
     
     current_LaserScan = LaserScan()
     
@@ -35,9 +38,10 @@ class RosConnector(object):
         self.__continuous = continuous
         self.__verbose = verbose
         self.__last_game_score = 0
-        self.__turtleBot_in_position = True
+        self.__turtleBot_in_position = False
         self.__first_action_taken = False
-        self.__crash = 0
+        self.__crash = False
+        self.__goal_reached = False
         self.__prev_lidar_distances = None
         self.__prev_action = None
         
@@ -66,7 +70,7 @@ class RosConnector(object):
         rospy.init_node(env_str, anonymous=True)
         rate = rospy.Rate(10)
         rospy.spin()
-        time.sleep(2)
+        time.sleep(1)
         self.print_if_verbose('ROS node initialized')
 
     @property
@@ -90,21 +94,23 @@ class RosConnector(object):
         return self.__continuous
     
     @property
+    def is_goal_reached(self):
+        return self.__goal_reached
+    
+    @property
     def last_game_score(self):
         pass
 
     def reset(self, msg=Bool(data=True)):
-        #msg_laserScan = LaserScan()
-        #msg_laserScan.ranges = np.zeros(360)
-        
         if msg.data is True and self.is_terminated:
+            rospy.loginfo('reset') # only for logging needed to be delted
             self.__in_reset = True
             self.__score = 0 # ToDo: need to be used
             self.__current_step = 0 # ToDo: need to be used
-            #state = self.reset_turtleBot() #ToDo define this function to set the turtlebot on the starting position. Use the method from the step function.
+            if not self.__turtleBot_in_position:
+                self.positionResetter()
             self.print_if_verbose('TurtleBot started')
-            time.sleep(1)
-            self.__terminate_publisher.publish(Bool(False))
+            time.sleep(0.5)
             msgScan = rospy.wait_for_message('/scan', LaserScan)
             ranges = msgScan.ranges
             ranges_array = Float32MultiArray()
@@ -116,68 +122,73 @@ class RosConnector(object):
             self.__position_publisher.publish(odomMsg_array)
             self.__reward_publisher.publish(Float32(0.0))
             self.__terminated = False
+            self.__terminate_publisher.publish(Bool(False))
+            self.__goal_reached = False
             self.__in_reset = False
+            self.__first_action_taken = False
+            self.__crash = False 
 
-    def step(self, msg):
-        if not rospy.is_shutdown():
-            action = msg.data
-            msgScan = rospy.wait_for_message('/scan', LaserScan) #maybe should be moved under the else case
-
-            if self.is_terminated or self.in_reset or self.is_crash:
-                self.print_if_verbose('Discard action because turtleBot is in reset or terminated or crashed')
-                stopTurtleBot(self.__set_navigation_publisher)
-                self.__crash = 0
-                self.__turtleBot_in_position = False
-                self.__first_action_taken = False
-                return
-            else:
-                if not self.__turtleBot_in_position:
+    def step(self, msg): 
+        try:
+            rospy.loginfo('step') # only for logging needed to be delted
+            if not rospy.is_shutdown():
+                action = msg.data
+                msgScan = rospy.wait_for_message('/scan', LaserScan) #maybe should be moved under the else case
+    
+                if self.is_terminated or self.in_reset or self.is_crash or self.is_goal_reached:
+                    rospy.loginfo('Discard action terminate:'+ str(self.is_terminated) + 'crach:' + str(self.is_crash))
+                    self.print_if_verbose('Discard action because turtleBot is in reset or terminated or crashed or goal reached')
                     stopTurtleBot(self.__set_navigation_publisher)
+                    self.__crash = False
+                    self.__turtleBot_in_position = False
                     self.__first_action_taken = False
-                    if self.RANDOM_START_POS:
-                        (x_init, y_init, theta_init) = setTurtleBotRandomPos(
-                            self.__set_position_publisher)
-                    else:
-                        (x_init, y_init, theta_init) = setTurtleBotPos(
-                            self.__set_position_publisher, 
-                            self.X_INIT, self.Y_INIT, self.THETA_INIT)
-                        odomMsg = rospy.wait_for_message('/odom', Odometry)
-                        (x , y) = getTurtleBotPos(odomMsg)
-                        theta = degrees(getTurtleBotRotation(odomMsg))
-                        if checkDiff(x,x_init, 0.01) and checkDiff(y,y_init,0.01) \
-                            and checkDiff(theta, theta_init, 1):
-                                self.__turtleBot_in_position = True
-                        else:
-                            self.__turtleBot_in_position = False
-                elif not self.__first_action_taken:
-                    lidarDistances = getLidarDist(msgScan)
-                    self.__crash = checkCrash(lidarDistances) 
-                    if not self.__crash:
-                        status_info = doTurtleBotAction(self.__set_navigation_publisher, action)
-                    self.__prev_lidar_distances = lidarDistances
-                    self.__prev_action = action
-                    self.__first_action_taken = True
-                else:
-                    lidarDistances = getLidarDist(msgScan)
-                    self.__crash = checkCrash(lidarDistances) 
-                    if not self.__crash:
-                        status_info = doTurtleBotAction(self.__set_navigation_publisher, action)
-                    (reward, terminal ) = getReward(action, self.__prev_action, lidarDistances, self.__prev_lidar_distances, self.__crash)   
-                    msgScan = rospy.wait_for_message('/scan', LaserScan)
-                    if terminal:
+                    if self.is_terminated or self.is_crash:
                         self.__terminated = True
-                    self.__prev_lidar_distances = lidarDistances
-                    self.__prev_action = action
-                    ranges = msgScan.ranges
-                    ranges_array = Float32MultiArray()
-                    ranges_array.data = ranges 
-                    self.__laser_publisher.publish(ranges_array)
-                    odomMsg = rospy.wait_for_message('/odom', Odometry)
-                    odomMsg_array = Float32MultiArray()
-                    odomMsg_array.data = getPosition(odomMsg)
-                    self.__position_publisher.publish(odomMsg_array)
-                    self.__terminate_publisher.publish(Bool(terminal))
-                    self.__reward_publisher.publish(Float32(reward))        
+                        self.__crash = True
+                        self.__terminate_publisher.publish(Bool(True)) 
+                    return
+                else:
+                    if not self.__turtleBot_in_position:
+                        self.positionResetter()
+                    elif not self.__first_action_taken:
+                        lidarDistances = getLidarDist(msgScan)
+                        self.__crash = checkCrash(lidarDistances)
+                        odomMsg = rospy.wait_for_message('/odom', Odometry)
+                        [x_pos, y_pos]= getPosition(odomMsg)
+                        self.__goal_reached = checkGoalNear(x_pos, y_pos, self.X_GOAL, self.Y_GOAL)
+                        if not self.is_crash and not self.is_goal_reached:
+                            status_info = doTurtleBotAction(self.__set_navigation_publisher, action)
+                        self.__prev_lidar_distances = lidarDistances
+                        self.__prev_action = action
+                        self.__first_action_taken = True
+                    else:
+                        lidarDistances = getLidarDist(msgScan)
+                        self.__crash = checkCrash(lidarDistances)
+                        odomMsg = rospy.wait_for_message('/odom', Odometry)
+                        [x_pos, y_pos]= getPosition(odomMsg)
+                        self.__goal_reached = checkGoalNear(x_pos, y_pos, self.X_GOAL, self.Y_GOAL)
+                        if not self.is_crash and not self.is_goal_reached:
+                            status_info = doTurtleBotAction(self.__set_navigation_publisher, action)
+                        (reward, terminal ) = getReward(action, self.__prev_action, lidarDistances, self.__prev_lidar_distances, self.__crash)   
+                        msgScan = rospy.wait_for_message('/scan', LaserScan)
+                        if terminal:
+                            self.__terminated = True
+                        self.__prev_lidar_distances = lidarDistances
+                        self.__prev_action = action
+                        ranges = msgScan.ranges
+                        ranges_array = Float32MultiArray()
+                        ranges_array.data = ranges 
+                        self.__laser_publisher.publish(ranges_array)
+                        odomMsg = rospy.wait_for_message('/odom', Odometry)
+                        odomMsg_array = Float32MultiArray()
+                        odomMsg_array.data = getPosition(odomMsg)
+                        self.__position_publisher.publish(odomMsg_array)
+                        self.__terminate_publisher.publish(Bool(terminal))
+                        self.__reward_publisher.publish(Float32(reward))
+        except rospy.ROSInterruptException:
+            stopTurtleBot(self.__set_navigation_publisher)
+            print('Simulation terminated!')
+            pass
 
 
     def print_if_verbose(self, massage):
@@ -190,4 +201,24 @@ class RosConnector(object):
     def createActions():
         actions = np.array([0,1,2])
         return actions
+        
+    def positionResetter(self):
+        stopTurtleBot(self.__set_navigation_publisher)
+        self.__first_action_taken = False
+        if self.RANDOM_START_POS:
+            (x_init, y_init, theta_init) = setTurtleBotRandomPos(
+                self.__set_position_publisher)
+        else:
+            (x_init, y_init, theta_init) = setTurtleBotPos(
+                self.__set_position_publisher, 
+                self.X_INIT, self.Y_INIT, self.THETA_INIT)
+            odomMsg = rospy.wait_for_message('/odom', Odometry)
+            (x , y) = getTurtleBotPos(odomMsg)
+            theta = degrees(getTurtleBotRotation(odomMsg))
+            if checkDiff(x,x_init, 0.01) and checkDiff(y,y_init,0.01) \
+                and checkDiff(theta, theta_init, 1):
+                    self.__turtleBot_in_position = True
+            else:
+                self.__turtleBot_in_position = False
+    
     
