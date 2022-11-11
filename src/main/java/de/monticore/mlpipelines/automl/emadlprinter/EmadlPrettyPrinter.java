e4b5525bf745af5ad143d21d76b2142dd09997b0
Tobias Hörnschemeyer <tobias.hoernschemeyer@rwth-aachen.de>
2022-11-11 16:19:07 +0100

    Print tuples

--- src/main/java/de/monticore/mlpipelines/automl/emadlprinter/EmadlPrettyPrinter.java
+++ src/main/java/de/monticore/mlpipelines/automl/emadlprinter/EmadlPrettyPrinter.java
@@ -3,9 +3,6 @@ package de.monticore.mlpipelines.automl.emadlprinter;
 import de.monticore.lang.monticar.cnnarch._ast.*;
 import de.monticore.lang.monticar.cnnarch._symboltable.ArchitectureSymbol;
 import de.monticore.lang.monticar.cnnarch._visitor.CNNArchVisitor;
-import de.monticore.literals.literals._ast.ASTIntLiteral;
-import de.monticore.literals.literals._ast.ASTNumericLiteral;
-import de.monticore.numberunit._ast.ASTNumberWithInf;
 import de.monticore.prettyprint.AstPrettyPrinter;
 import de.monticore.prettyprint.IndentPrinter;
 
@@ -130,26 +127,13 @@ public class EmadlPrettyPrinter implements AstPrettyPrinter<ASTArchitecture>, CN
     public void printASTArchParameterArgument(ASTArchParameterArgument node) {
         String argumentName = node.getName();
         this.printer.print(argumentName + " = ");
-        node.getRhs().accept(getRealThis());
+        NumberPrinter numberPrinter = new NumberPrinter(printer);
+        numberPrinter.printASTArchExpression(node.getRhs());
     }
 
     @Override
     public void visit(ASTStreamInstruction node) {
         printASTStream(node.getBody());
-    }
-
-    @Override
-    public void visit(ASTNumberWithInf node) {
-        if (node.isPresentInf()) {
-            printer.print("inf");
-        } else {
-            ASTNumericLiteral literal = node.getNumber();
-
-            if (literal instanceof ASTIntLiteral) {
-                ASTIntLiteral number = (ASTIntLiteral) literal;
-                printer.print("" + number.getValue());
-            }
-        }
     }
 
 //    @Override
---
+++ src/main/java/de/monticore/mlpipelines/automl/emadlprinter/NumberPrinter.java
@@ -0,0 +1,80 @@
+package de.monticore.mlpipelines.automl.emadlprinter;
+
+import de.monticore.lang.math._ast.ASTNumberExpression;
+import de.monticore.lang.monticar.cnnarch._ast.*;
+import de.monticore.literals.literals._ast.ASTIntLiteral;
+import de.monticore.literals.literals._ast.ASTNumericLiteral;
+import de.monticore.numberunit._ast.ASTNumberWithInf;
+import de.monticore.numberunit._ast.ASTNumberWithUnit;
+import de.monticore.prettyprint.IndentPrinter;
+
+import java.util.List;
+import java.util.Optional;
+
+public class NumberPrinter {
+    private final IndentPrinter printer;
+
+    public NumberPrinter(IndentPrinter printer) {
+        this.printer = printer;
+    }
+
+
+    public void printASTArchExpression(ASTArchExpression rhs) {
+        ASTArchSimpleExpression expression = rhs.getExpression();
+        printASTArchSimpleExpression(expression);
+    }
+
+
+    private void printASTArchSimpleExpression(ASTArchSimpleExpression expression) {
+        Optional<ASTTupleExpression> tupleExpressionOpt = expression.getTupleExpressionOpt();
+        if (tupleExpressionOpt.isPresent())
+            printASTTupleExpression(tupleExpressionOpt.get());
+    }
+
+    private void printASTTupleExpression(ASTTupleExpression astTupleExpression) {
+        List<ASTArchArithmeticExpression> expressions = astTupleExpression.getExpressionsList();
+        printer.print("(");
+        for (int i = 0; i < expressions.size(); i++) {
+            printASTArithmeticExpression(expressions.get(i));
+            if (i < expressions.size() - 1) {
+                printer.print(", ");
+            }
+        }
+        printer.print(")");
+    }
+
+    private void printASTArithmeticExpression(ASTArchArithmeticExpression expression) {
+        if (expression instanceof ASTArchSimpleArithmeticExpression)
+            printASTSimpleArithmeticExpression((ASTArchSimpleArithmeticExpression) expression);
+    }
+
+    private void printASTSimpleArithmeticExpression(ASTArchSimpleArithmeticExpression expression) {
+        Optional<ASTNumberExpression> numberExpressionOpt = expression.getNumberExpressionOpt();
+        if (numberExpressionOpt.isPresent())
+            printASTNumberExpression(numberExpressionOpt.get());
+    }
+
+    private void printASTNumberExpression(ASTNumberExpression expression) {
+        ASTNumberWithUnit numberWithUnit = expression.getNumberWithUnit();
+        printASTNumberWithUnit(numberWithUnit);
+    }
+
+    private void printASTNumberWithUnit(ASTNumberWithUnit numberWithUnit) {
+        Optional<ASTNumberWithInf> numOpt = numberWithUnit.getNumOpt();
+        if (numOpt.isPresent())
+            printASTNumberWithInf(numOpt.get());
+    }
+
+    private void printASTNumberWithInf(ASTNumberWithInf num) {
+        if (num.isPresentInf()) {
+            printer.print("inf");
+        } else {
+            ASTNumericLiteral literal = num.getNumber();
+
+            if (literal instanceof ASTIntLiteral) {
+                ASTIntLiteral number = (ASTIntLiteral) literal;
+                printer.print("" + number.getValue());
+            }
+        }
+    }
+}
