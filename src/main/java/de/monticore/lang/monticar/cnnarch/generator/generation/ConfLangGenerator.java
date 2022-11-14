5e1245e61427db4efb66523c90d268c5ab5d6a92
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-11-14 14:15:29 -0800

    Fix generation path

--- src/main/java/de/monticore/lang/monticar/cnnarch/generator/generation/ConfLangGenerator.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/generator/generation/ConfLangGenerator.java
@@ -10,17 +10,17 @@ import de.monticore.lang.monticar.cnnarch.generator.configuration.Template;
 import de.monticore.lang.monticar.cnnarch.generator.training.TrainingComponentsContainer;
 import de.monticore.lang.monticar.cnnarch.generator.training.TrainingConfiguration;
 import de.monticore.lang.monticar.generator.FileContent;
+import org.apache.commons.lang3.NotImplementedException;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 /***
  * Generator to generate code representation of ConfLang configuration
  */
 public class ConfLangGenerator extends CNNTrainGenerator {
-
-    private final GeneratorSetup generatorSetup = new GeneratorSetup();
 
     private final GeneratorEngine generatorEngine;
 
@@ -30,18 +30,16 @@ public class ConfLangGenerator extends CNNTrainGenerator {
 
     protected ConfLangGenerator(final TrainParamSupportChecker trainParamSupportChecker) {
         super(trainParamSupportChecker);
+        GeneratorSetup generatorSetup = new GeneratorSetup();
         generatorSetup.setTracing(false);
         generatorSetup.setGlex(ConfLangGeneratorConfiguration.getGlobalExtensionManagement());
-        generatorSetup.setOutputDirectory(new File("./target/generated-trainingConfig"));
+        generatorSetup.setOutputDirectory(new File("target/generated-sources/configurations"));
         generatorEngine = new GeneratorEngine(generatorSetup);
     }
 
-    public void setOutputPath(Path outputPath){
-        this.generatorSetup.setOutputDirectory(outputPath.toFile());
-    }
-
     public void generatePythonTrainingConfiguration(final ASTConfLangCompilationUnit configurationModel) {
-        generatorEngine.generate(Template.TRAINING_CONFIGURATION_CLASS.getTemplateName(), generatorSetup.getOutputDirectory().toPath(), configurationModel, configurationModel);
+        final Path fileName = Paths.get("Training_Configuration_" + configurationModel.getConfiguration().getName() + ".py");
+        generatorEngine.generate(Template.TRAINING_CONFIGURATION_CLASS.getTemplateName(), fileName, configurationModel, configurationModel);
 
     }
 
@@ -54,7 +52,7 @@ public class ConfLangGenerator extends CNNTrainGenerator {
     @Deprecated
     @Override
     public List<FileContent> generateStrings(final TrainingConfiguration trainingConfiguration, final TrainingComponentsContainer trainingComponentsContainer, final Path outputPath) {
-        return null;
+       throw new NotImplementedException("This is method is deprecated.");
     }
 
 }
