ed5a81f67450e6af1e4638dea266d59c678217b9
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-11-20 09:24:23 -0800

    Update supervised schema api

--- src/main/resources/experiment/configuration/Supervised_Schema_API.py
+++ src/main/resources/experiment/configuration/Supervised_Schema_API.py
@@ -1,99 +1,97 @@
 class Supervised_Schema_API:
-
     def __init__(self):
         self.training_configuration = None
     def set_training_configuration(self,training_configuration):
         self.training_configuration =  training_configuration
+    
 
-    class Eval_metric_type:
-        def __init__(self,outer):
-            self.outer = outer
-        def get_Eval_metric_type_Value(self):
-            return self.outer.training_configuration.get_Eval_metric_type_Value()
-        #common hyperparameters
-        #value-specific hyperparameter
-        def getaccuracy_ignore_labelaxis(self):
-            return self.outer.training_configuration.getaccuracy_ignore_labelaxis()
-        def getaccuracy_ignore_labelmetric_ignore_label(self):
-            return self.outer.training_configuration.getaccuracy_ignore_labelmetric_ignore_label()
-        def getbleuexclude(self):
-            return self.outer.training_configuration.getbleuexclude()
-    class Optimizer_type:
-        def __init__(self,outer):
-            self.outer = outer
-        def get_Optimizer_type_Value(self):
-            return self.outer.training_configuration.get_Optimizer_type_Value()
-        #common hyperparameters
-        def getoptimizer_typelearning_rate(self):
-            return self.outer.training_configuration.getoptimizer_typelearning_rate()
-        def getoptimizer_typelearning_rate_minimum(self):
-            return self.outer.training_configuration.getoptimizer_typelearning_rate_minimum()
-        def getoptimizer_typelearning_rate_decay(self):
-            return self.outer.training_configuration.getoptimizer_typelearning_rate_decay()
-        def getoptimizer_typeweight_decay(self):
-            return self.outer.training_configuration.getoptimizer_typeweight_decay()
-        def getoptimizer_typelearning_rate_policy(self):
-            return self.training_configuration.optimizer_typelearning_rate_policy()
-        def getoptimizer_typerescale_grad(self):
-            return self.outer.training_configuration.getoptimizer_typerescale_grad()
-        def getoptimizer_typeclip_gradient(self):
-            return self.outer.training_configuration.getoptimizer_typeclip_gradient()
-        def getoptimizer_typestep_size(self):
-            return self.outer.training_configuration.getoptimizer_typestep_size()
-        #value-specific hyperparameter
-        def getsgdmomentum(self):
-            return self.outer.training_configuration.getsgdmomentum()
-        def getadambeta1(self):
-            return self.outer.training_configuration.getadambeta1()
-        def getadambeta2(self):
-            return self.outer.training_configuration.getadambeta2()
-        def getadamepsilon(self):
-            return self.outer.training_configuration.getadamepsilon()
-        def getadamwbeta1(self):
-            return self.outer.training_configuration.getadamwbeta1()
-        def getadamwbeta2(self):
-            return self.outer.training_configuration.getadamwbeta2()
-        def getadamwepsilon(self):
-            return self.outer.training_configuration.getadamwepsilon()
-        def getrmspropepsilon(self):
-            return self.outer.training_configuration.getrmspropepsilon()
-        def getrmspropgamma1(self):
-            return self.outer.training_configuration.getrmspropgamma1()
-        def getrmspropgamma2(self):
-            return self.outer.training_configuration.getrmspropgamma2()
-        def getrmspropcentered(self):
-            return self.outer.training_configuration.getrmspropcentered()
-        def getrmspropclip_weights(self):
-            return self.outer.training_configuration.getrmspropclip_weights()
-        def getrmsproprho(self):
-            return self.outer.training_configuration.getrmsproprho()
-        def getadagradepsilon(self):
-            return self.outer.training_configuration.getadagradepsilon()
-        def getnagmomentum(self):
-            return self.outer.training_configuration.getnagmomentum()
-        def getadadeltaepsilon(self):
-            return self.outer.training_configuration.getadadeltaepsilon()
-        def getadadeltarho(self):
-            return self.outer.training_configuration.getadadeltarho()
+    
 
-    def getEval_metric_type(self):
-        return self.Eval_metric_type(self)
-    def getOptimizer_type(self):
-        return self.Optimizer_type(self)
-    def getBatch_size(self):
-        return self.training_configuration.getBatch_size()
-    def getNum_epoch(self):
-        return self.training_configuration.getNum_epoch()
-    def getCheckpoint_period(self):
-        return self.training_configuration.getCheckpoint_period()
-    def getPreprocessing_name(self):
-        return self.training_configuration.getPreprocessing_name()
-    def getLoad_pretrained(self):
-        return self.training_configuration.getLoad_pretrained()
-    def getLog_period(self):
-        return self.training_configuration.getLog_period()
-    def getShuffle_data(self):
-        return self.training_configuration.getShuffle_data()
+    '''
+        possible values:
+            rmse
+            accuracy
+            accuracy_ignore_label
+    '''
 
-    def getcontext(self):
-        return self.training_configuration.getcontext()
+    def get_eval_metric_value(self):
+        return self.training_configuration.get_eval_metric_value()
+
+    #common hyperparameters
+
+    #value-specific hyperparameters
+    def get_eval_metric_accuracy_ignore_label_axis(self):
+        return self.training_configuration.get_eval_metric_axis()
+    def get_eval_metric_accuracy_ignore_label_metric_ignore_label(self):
+        return self.training_configuration.get_eval_metric_metric_ignore_label()
+
+    
+
+    '''
+        possible values:
+            sgd
+            adam
+            adamw
+    '''
+
+    def get_optimizer_value(self):
+        return self.training_configuration.get_optimizer_value()
+
+    #common hyperparameters
+    def get_optimizer_learning_rate(self):
+        return self.training_configuration.get_optimizer_learning_rate()
+    def get_optimizer_weight_decay(self):
+        return self.training_configuration.get_optimizer_weight_decay()
+    def get_optimizer_learning_rate_policy(self):
+        return self.training_configuration.get_optimizer_learning_rate_policy()
+
+    #value-specific hyperparameters
+    def get_optimizer_sgd_momentum(self):
+        return self.training_configuration.get_optimizer_momentum()
+    def get_optimizer_adam_beta1(self):
+        return self.training_configuration.get_optimizer_beta1()
+    def get_optimizer_adam_beta2(self):
+        return self.training_configuration.get_optimizer_beta2()
+    def get_optimizer_adam_epsilon(self):
+        return self.training_configuration.get_optimizer_epsilon()
+
+    
+
+    '''
+        possible values:
+            cross_entropy
+            kullback_leibler
+    '''
+
+    def get_loss_value(self):
+        return self.training_configuration.get_loss_value()
+
+    #common hyperparameters
+
+    #value-specific hyperparameters
+    def get_loss_huber_rho(self):
+        return self.training_configuration.get_loss_rho()
+    def get_loss_cross_entropy_sparse_label(self):
+        return self.training_configuration.get_loss_sparse_label()
+    def get_loss_cross_entropy_loss_axis(self):
+        return self.training_configuration.get_loss_loss_axis()
+    def get_loss_cross_entropy_batch_axis(self):
+        return self.training_configuration.get_loss_batch_axis()
+
+    def get_batch_size(self):
+        return self.training_configuration.get_batch_size()
+    def get_num_epoch(self):
+        return self.training_configuration.get_num_epoch()
+    def get_checkpoint_period(self):
+        return self.training_configuration.get_checkpoint_period()
+    def get_preprocessing_name(self):
+        return self.training_configuration.get_preprocessing_name()
+    def get_load_pretrained(self):
+        return self.training_configuration.get_load_pretrained()
+    def get_log_period(self):
+        return self.training_configuration.get_log_period()
+    def get_shuffle_data(self):
+        return self.training_configuration.get_shuffle_data()
+
+    def get_context(self):
+        return self.training_configuration.get_context()
