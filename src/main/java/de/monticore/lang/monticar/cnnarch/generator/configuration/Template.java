02b13e7af888294c79ca81134e6bace2b6fc5798
feras <feras.m94.4@gmail.com>
2022-11-20 16:17:36 +0100

    Add test to validate template paths

--- src/main/java/de/monticore/lang/monticar/cnnarch/generator/configuration/Template.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/generator/configuration/Template.java
@@ -1,12 +1,16 @@
 package de.monticore.lang.monticar.cnnarch.generator.configuration;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public enum Template {
     TRAINING_CONFIGURATION_CLASS("templates.confLang.python.TrainingConfigurationClass"),
-    TRAINING_CONFIGURATION_METHOD("templates.confLang.python.Training_Configuration_Method"),
+    EMPTY_CONFIGURATION_METHOD("templates.confLang.python.Empty_Configuration_Method"),
     SIMPLE_CONFIGURATION("templates.confLang.python.SimpleConfiguration"),
     NESTED_CONFIGURATION("templates.confLang.python.NestedConfiguration");
 
     private final String templateName;
+    private final static String templateFileExtension = ".ftl";
 
     Template(final String templateName) {
         this.templateName = templateName;
@@ -14,5 +18,9 @@ public enum Template {
 
     public String getTemplateName() {
         return templateName;
+    }
+
+    public Path toPath(){
+      return Paths.get("src/main/resources/"+ getTemplateName().replace(".","/")+ templateFileExtension);
     }
 }
