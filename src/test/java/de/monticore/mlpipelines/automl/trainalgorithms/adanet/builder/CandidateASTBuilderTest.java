45604d99925d24baf1a30c49106a31ac4d9e7db8
Tobias Hörnschemeyer <tobias.hoernschemeyer@rwth-aachen.de>
2022-11-24 15:43:27 +0100

    Add tests

--- src/test/java/de/monticore/mlpipelines/automl/trainalgorithms/adanet/builder/CandidateASTBuilderTest.java
+++ src/test/java/de/monticore/mlpipelines/automl/trainalgorithms/adanet/builder/CandidateASTBuilderTest.java
@@ -42,4 +42,88 @@ public class CandidateASTBuilderTest extends TestCase {
         ASTParallelBlock adanetAst = candidateASTBuilder.build(adanetSymbol);
         assertNotNull(adanetAst);
     }
+
+    @Test
+    public void testBuildCreatesValidParallelBlock() {
+        CandidateASTBuilder candidateASTBuilder = new CandidateASTBuilder();
+        AdaNetCandidate candidate = new AdaNetCandidate(new AdaNetComponent(3), new ArrayList<>());
+        CandidateSymbolBuilder candidateSymbolBuilder = new CandidateSymbolBuilder();
+        ParallelCompositeElementSymbol adanetSymbol = candidateSymbolBuilder.build(candidate);
+
+        ASTParallelBlock adanetAst = candidateASTBuilder.build(adanetSymbol);
+        assertNotNull(adanetAst);
+        assertTrue(adanetAst.getSymbol() instanceof ParallelCompositeElementSymbol);
+        assertEquals(1, adanetAst.getGroupsList().size());
+    }
+
+    @Test
+    public void testBuildCreatesValidASTStream() {
+        CandidateASTBuilder candidateASTBuilder = new CandidateASTBuilder();
+        AdaNetCandidate candidate = new AdaNetCandidate(new AdaNetComponent(3), new ArrayList<>());
+        CandidateSymbolBuilder candidateSymbolBuilder = new CandidateSymbolBuilder();
+        ParallelCompositeElementSymbol adanetSymbol = candidateSymbolBuilder.build(candidate);
+
+        ASTParallelBlock adanetAst = candidateASTBuilder.build(adanetSymbol);
+        ASTStream stream = adanetAst.getGroupsList().get(0);
+        assertNotNull(stream);
+        assertEquals(6, stream.getElementsList().size());
+    }
+
+    @Test
+    public void testBuildCreatesValidLayer() {
+        CandidateASTBuilder candidateASTBuilder = new CandidateASTBuilder();
+        AdaNetCandidate candidate = new AdaNetCandidate(new AdaNetComponent(3), new ArrayList<>());
+        CandidateSymbolBuilder candidateSymbolBuilder = new CandidateSymbolBuilder();
+        ParallelCompositeElementSymbol adanetSymbol = candidateSymbolBuilder.build(candidate);
+
+        ASTParallelBlock adanetAst = candidateASTBuilder.build(adanetSymbol);
+
+        ASTStream stream = adanetAst.getGroupsList().get(0);
+        ASTLayer layer = (ASTLayer) stream.getElementsList().get(1);
+        assertNotNull(layer);
+        assertEquals("Concatenate", layer.getName());
+    }
+
+    @Test
+    public void testBuildCreatesValidArguments() {
+        CandidateASTBuilder candidateASTBuilder = new CandidateASTBuilder();
+        AdaNetCandidate candidate = new AdaNetCandidate(new AdaNetComponent(3), new ArrayList<>());
+        CandidateSymbolBuilder candidateSymbolBuilder = new CandidateSymbolBuilder();
+        ParallelCompositeElementSymbol adanetSymbol = candidateSymbolBuilder.build(candidate);
+
+        ASTParallelBlock adanetAst = candidateASTBuilder.build(adanetSymbol);
+
+        ASTStream stream = adanetAst.getGroupsList().get(0);
+        ASTParallelBlock parallelLayer = (ASTParallelBlock) stream.getElementsList().get(0);
+        ASTStream stream2 = parallelLayer.getGroups(0);
+        ASTLayer layer = (ASTLayer) stream2.getElementsList().get(0);
+        ASTArchParameterArgument argument = (ASTArchParameterArgument) layer.getArgumentsList().get(0);
+        int number = (int) getValueOfArgument(argument);
+
+
+        assertNotNull(layer);
+        assertEquals("FullyConnected", layer.getName());
+        assertEquals(1, layer.getArgumentsList().size());
+        assertEquals("units", argument.getName());
+        assertEquals(10, number);
+    }
+
+    private static double getValueOfArgument(ASTArchParameterArgument argument) {
+        ASTArchExpression rhs = argument.getRhs();
+        ASTArchSimpleExpression simpleExpression = rhs.getExpression();
+        ASTArchSimpleArithmeticExpression arithmeticExpression = (ASTArchSimpleArithmeticExpression) simpleExpression.getArithmeticExpression();
+        ASTNumberExpression numberExpression = arithmeticExpression.getNumberExpression();
+        ASTNumberWithUnit numberWithUnit = numberExpression.getNumberWithUnit();
+        return numberWithUnit.getNumber().get();
+    }
+
+    @Test
+    public void testAstParallelBlockHasValidArguments() {
+        CandidateASTBuilder candidateASTBuilder = new CandidateASTBuilder();
+        AdaNetCandidate candidate = new AdaNetCandidate(new AdaNetComponent(3), new ArrayList<>());
+        CandidateSymbolBuilder candidateSymbolBuilder = new CandidateSymbolBuilder();
+        ParallelCompositeElementSymbol adanetSymbol = candidateSymbolBuilder.build(candidate);
+        ASTParallelBlock adanetAst = candidateASTBuilder.build(adanetSymbol);
+
+    }
 }
