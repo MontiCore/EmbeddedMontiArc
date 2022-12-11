1baf38244564c81bab5b6671fe72a5566594ab07
Hiroshi Hamano <hiroshihamano1996@gmail.com>
2022-12-11 21:35:06 +0100

    Adapt unittests for getting all keys of entries and changing range entry values

--- src/test/java/de/monticore/mlpipelines/automl/helper/SearchSpacesTest.java
+++ src/test/java/de/monticore/mlpipelines/automl/helper/SearchSpacesTest.java
@@ -29,6 +29,11 @@ public class SearchSpacesTest extends TestCase {
     }
 
     @Test
+    public void testSet() {
+        ASTConfLangCompilationUnitHandler.setValueForKey(compilationUnit, "num_epoch", 10);
+    }
+
+    @Test
     public void testGetNumEpochRange() {
         Map<String, Object> numEpochRange = (Map<String, Object>) ASTConfLangCompilationUnitHandler.getValueByKey(this.compilationUnit, "num_epoch");
         assertEquals(numEpochRange.get("lower"), 8);
@@ -66,5 +71,37 @@ public class SearchSpacesTest extends TestCase {
         assertEquals(weightDecayMap.get("lower"), 0.0);
         assertEquals(weightDecayMap.get("step_size"), 0.01);
         assertEquals(weightDecayMap.get("upper"), 0.9);
+    }
+
+    @Test
+    public void testgetAllKeys() {
+        Map<String, Boolean> keysMap = ASTConfLangCompilationUnitHandler.getAllKeys(compilationUnit);
+
+        assertFalse(keysMap.get("num_epoch"));
+        assertFalse(keysMap.get("batch_size"));
+        assertTrue(keysMap.get("optimizer"));
+    }
+
+    @Test
+    public void testSetNumEpochValue() {
+        ASTConfLangCompilationUnit newCompilationUnit = compilationUnit.deepClone();
+        ASTConfLangCompilationUnit updatedCompilationUnit = ASTConfLangCompilationUnitHandler.setValueForKey(newCompilationUnit, "num_epoch", 20);
+
+        int updatedNumEpoch = (int) ASTConfLangCompilationUnitHandler.getValueByKey(updatedCompilationUnit, "num_epoch");
+        Object numEpochRange = ASTConfLangCompilationUnitHandler.getValueByKey(compilationUnit, "num_epoch");
+
+        assertEquals(updatedNumEpoch, 20);
+        assertFalse(numEpochRange.equals(20));
+    }
+
+    @Test
+    public void testSetNestedValueForKeys() {
+        ASTConfLangCompilationUnit newCompilationUnit = compilationUnit.deepClone();
+        ASTConfLangCompilationUnit updatedCompilationUnit = ASTConfLangCompilationUnitHandler.setNestedValueForKeys(newCompilationUnit, "optimizer", "learning_rate", 0.005);
+        Map<String, Object> updatedConfigMap = ASTConfLangCompilationUnitHandler.getValuesFromNestedConfiguration(updatedCompilationUnit, "optimizer");
+        Map<String, Object> updatedNestedMap = (Map<String, Object>) updatedConfigMap.get("nestedMap");
+        double updatedLearningRate = (double) updatedNestedMap.get("learning_rate");
+
+        assertEquals(updatedLearningRate, 0.005);
     }
 }
