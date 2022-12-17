5cbea3472808c3b586744bb71b7e32a00090d079
Hiroshi Hamano <hiroshihamano1996@gmail.com>
2022-12-17 14:07:22 +0100

    in progress

--- src/main/java/de/monticore/mlpipelines/automl/hyperparameters/parallel/GeneticAlgorithm.java
+++ src/main/java/de/monticore/mlpipelines/automl/hyperparameters/parallel/GeneticAlgorithm.java
@@ -1,6 +1,11 @@
 package de.monticore.mlpipelines.automl.hyperparameters.parallel;
 
 import conflang._ast.ASTConfLangCompilationUnit;
+import groovy.util.MapEntry;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class GeneticAlgorithm extends ParallelAlgorithm {
 
@@ -8,8 +13,33 @@ public class GeneticAlgorithm extends ParallelAlgorithm {
 
     private double crossoverConfig;
 
-    public void executeSelection() {
-        // TODO: Implement method
+    private double selectionRate;
+
+    public List<ASTConfLangCompilationUnit> executeSelection(List<ASTConfLangCompilationUnit> hyperParamsPopulation, List<Double> evalValues, String metricType) {
+        int selectNum = (int) (hyperParamsPopulation.size() * this.selectionRate);
+        selectNum = Math.max(selectNum, 2);
+
+        List<ASTConfLangCompilationUnit> nBestConfigs = this.selectBestConfigs(hyperParamsPopulation, evalValues, selectNum, metricType);
+
+        return nBestConfigs;
+    }
+
+    private List<ASTConfLangCompilationUnit> selectBestConfigs(List<ASTConfLangCompilationUnit> population, List<Double> evalValues, int numConfigs, String metricType) {
+        List<ASTConfLangCompilationUnit> nBestConfigs = new ArrayList<>();
+        List<Double> sortedEvalValues = new ArrayList<>(evalValues);
+        Collections.sort(sortedEvalValues);
+
+        if (metricType.equals("Accuracy")) {
+            Collections.reverse(sortedEvalValues);
+        }
+
+        for (Double value : sortedEvalValues.subList(0, numConfigs)) {
+            int index = evalValues.indexOf(value);
+            ASTConfLangCompilationUnit config = population.get(index);
+            nBestConfigs.add(config);
+        }
+
+        return nBestConfigs;
     }
 
     @Override
@@ -18,7 +48,18 @@ public class GeneticAlgorithm extends ParallelAlgorithm {
     }
 
     @Override
+    public void executeOptimizationStep(List<ASTConfLangCompilationUnit> hyperParamsPopulation, ASTConfLangCompilationUnit searchSpace, List<Double> evalValues, String metricType) {
+        this.setCurrentPopulation(hyperParamsPopulation);
+        this.setEvalValues(evalValues);
+    }
+
+    @Override
     public ASTConfLangCompilationUnit getNewHyperparamsCandidate(ASTConfLangCompilationUnit searchSpace) {
+        return null;
+    }
+
+    @Override
+    public List<ASTConfLangCompilationUnit> getNewPopulation() {
         return null;
     }
 }
--- src/main/java/de/monticore/mlpipelines/automl/hyperparameters/parallel/ParallelAlgorithm.java
+++ src/main/java/de/monticore/mlpipelines/automl/hyperparameters/parallel/ParallelAlgorithm.java
@@ -1,23 +1,56 @@
 package de.monticore.mlpipelines.automl.hyperparameters.parallel;
 
+import conflang._ast.ASTConfLangCompilationUnit;
 import de.monticore.mlpipelines.automl.hyperparameters.AbstractHyperparameterAlgorithm;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class ParallelAlgorithm extends AbstractHyperparameterAlgorithm {
 
-    // TODO: Specify datatype
-    private Object currentPopulation;
+    private List<ASTConfLangCompilationUnit> currentPopulation;
 
-    public void initializePopulation() {
-        // TODO: Implement method
+    private List<Double> evalValues;
+
+    public List<ASTConfLangCompilationUnit> initializePopulation(ASTConfLangCompilationUnit searchSpace, int populationSize) {
+        List<ASTConfLangCompilationUnit> hyperparamsPopulation = new ArrayList<>();
+
+        while (hyperparamsPopulation.size() < populationSize) {
+            ASTConfLangCompilationUnit hyperparams = this.getInitialHyperparams(searchSpace);
+            if (!this.checkHyperparamsInPopulation(hyperparams, hyperparamsPopulation)) {
+                hyperparamsPopulation.add(hyperparams);
+            }
+        }
+
+        return hyperparamsPopulation;
     }
 
-    // TODO: Specify datatype
-    public Object updatePopulation() {
-        Object updatedPopulation = null;
-        return updatedPopulation;
+    private boolean checkHyperparamsInPopulation(ASTConfLangCompilationUnit hyperparams, List<ASTConfLangCompilationUnit> population) {
+        for (ASTConfLangCompilationUnit populationEntry : population) {
+            if (populationEntry.deepEquals(hyperparams)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public Object getCurrentPopulation() {
+    public List<ASTConfLangCompilationUnit> getCurrentPopulation() {
         return this.currentPopulation;
     }
+
+    public void setCurrentPopulation(List<ASTConfLangCompilationUnit> currentPopulation) {
+        this.currentPopulation = currentPopulation;
+    }
+
+    public List<Double> getEvalValues() {
+        return evalValues;
+    }
+
+    public void setEvalValues(List<Double> evalValues) {
+        this.evalValues = evalValues;
+    }
+
+    public abstract List<ASTConfLangCompilationUnit> getNewPopulation();
+
+    public abstract void executeOptimizationStep(List<ASTConfLangCompilationUnit> hyperParamsPopulation, ASTConfLangCompilationUnit searchSpace, List<Double> evalValues, String metricType);
 }
