e853c533322110d3a64405965d15ee369b751ac3
Ahmed Diab <ahmed.diab@rwth-aachen.de>
2022-12-21 10:41:44 +0100

    add new pow command

---
+++ src/main/java/de/monticore/lang/monticar/generator/cpp/commands/MathPowCommand.java
@@ -0,0 +1,57 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.lang.monticar.generator.cpp.commands;
+
+import de.monticore.lang.embeddedmontiarc.embeddedmontiarcmath._symboltable.math.symbols.MathStringExpression;
+import de.monticore.lang.math._symboltable.expression.MathExpressionSymbol;
+import de.monticore.lang.math._symboltable.matrix.MathMatrixAccessSymbol;
+import de.monticore.lang.math._symboltable.matrix.MathMatrixNameExpressionSymbol;
+import de.monticore.lang.monticar.generator.EMAMBluePrint;
+import de.monticore.lang.monticar.generator.MathCommand;
+import de.monticore.lang.monticar.generator.cpp.EMAMBluePrintCPP;
+import de.monticore.lang.monticar.generator.cpp.MathFunctionFixer;
+import de.monticore.lang.monticar.generator.cpp.converter.ExecuteMethodGenerator;
+import de.monticore.lang.monticar.generator.cpp.converter.MathConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ahmed Diab
+ */
+public class MathPowCommand extends MathCommand {
+    public MathPowCommand(){
+        setMathCommandName("pow");
+    }
+    @Override
+    public void convert(MathExpressionSymbol mathExpressionSymbol, EMAMBluePrint bluePrint) {
+        String backendName = MathConverter.curBackend.getBackendName();
+        if (backendName.equals("OctaveBackend")) {
+            convertUsingOctaveBackend(mathExpressionSymbol, bluePrint);
+        } else if (backendName.equals("ArmadilloBackend")) {
+            convertUsingArmadilloBackend(mathExpressionSymbol, bluePrint);
+        }
+    }
+
+    public void convertUsingOctaveBackend(MathExpressionSymbol mathExpressionSymbol, EMAMBluePrint bluePrint) {
+        Log.error("No implementation for Octave Backend");
+    }
+    
+    public void convertUsingArmadilloBackend(MathExpressionSymbol mathExpressionSymbol, EMAMBluePrint bluePrint) {
+        MathMatrixNameExpressionSymbol mathMatrixNameExpressionSymbol = (MathMatrixNameExpressionSymbol) mathExpressionSymbol;
+
+        mathMatrixNameExpressionSymbol.setNameToAccess("");
+
+        String valueListString = "";
+        for (MathMatrixAccessSymbol accessSymbol : mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().
+                getMathMatrixAccessSymbols())
+            MathFunctionFixer.fixMathFunctions(accessSymbol, (EMAMBluePrintCPP) bluePrint);
+        valueListString += ExecuteMethodGenerator.generateExecuteCode(mathExpressionSymbol, new ArrayList<String>());
+
+        List<MathMatrixAccessSymbol> newMatrixAccessSymbols = new ArrayList<>();
+        MathStringExpression stringExpression = new MathStringExpression("std::pow"+valueListString,
+                mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().getMathMatrixAccessSymbols());
+        newMatrixAccessSymbols.add(new MathMatrixAccessSymbol(stringExpression));
+
+        mathMatrixNameExpressionSymbol.getMathMatrixAccessOperatorSymbol().setMathMatrixAccessSymbols(newMatrixAccessSymbols);
+    }
+}
