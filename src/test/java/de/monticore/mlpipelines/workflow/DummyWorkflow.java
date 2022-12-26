31b5b92a18f8be95c4ed642438241b9f000039f2
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-12-26 12:22:21 -0800

    Update tests

--- src/test/java/de/monticore/mlpipelines/workflow/DummyWorkflow.java
+++ src/test/java/de/monticore/mlpipelines/workflow/DummyWorkflow.java
@@ -1,8 +1,17 @@
 package de.monticore.mlpipelines.workflow;
 
 import de.monticore.lang.monticar.cnnarch.generator.training.LearningMethod;
+import de.monticore.mlpipelines.configuration.MontiAnnaContext;
 
 public class DummyWorkflow extends AbstractWorkflow {
+    public DummyWorkflow(final MontiAnnaContext applicationContext) {
+        super(applicationContext);
+    }
+
+    public DummyWorkflow() {
+        super();
+    }
+
     @Override
     public void createPipeline(final LearningMethod learningMethod) {
     }
