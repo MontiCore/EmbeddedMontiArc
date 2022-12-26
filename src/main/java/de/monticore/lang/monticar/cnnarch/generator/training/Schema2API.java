008879ef2c6717bf08926f8cb8f57c2ee86bac41
feras.m94.4 <feras.mulhem@rwth-aachen.de>
2022-12-25 17:26:09 -0800

    Code clean up

--- src/main/java/de/monticore/lang/monticar/cnnarch/generator/training/Schema2API.java
+++ src/main/java/de/monticore/lang/monticar/cnnarch/generator/training/Schema2API.java
@@ -7,28 +7,20 @@ import de.monticore.io.paths.ModelPath;
 import de.monticore.lang.monticar.cnnarch.generator.FreeMarkerTemplate;
 import de.monticore.lang.monticar.cnnarch.generator.util.SchemaUtil;
 import schemalang._ast.ASTSchemaDefinition;
-import schemalang._ast.ASTSchemaLangCompilationUnit;
-import schemalang._parser.SchemaLangParser;
-import schemalang._symboltable.SchemaDefinitionSymbol;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Schema2API {
 
     private File outputDirectory = new File("target/generated-sources/schemaAPI");
-    private ModelPath schemasModelPath = new ModelPath(Paths.get("src/main/resources/schemas"));
 
     public void setOutputDirectory(File outputDirectory) {
         this.outputDirectory = outputDirectory;
     }
-
-    public void setSchemasModelPath(ModelPath schemasModelPath) {
-        this.schemasModelPath = schemasModelPath;
-    }
-
 
     public static void main(String[] args) {
         final String pathToSchemas = args[0];
@@ -58,40 +50,9 @@ public class Schema2API {
     }
 
     private void generateForSchema(final GeneratorEngine generatorEngine, final ASTSchemaDefinition schemaDefinition) {
-        final ASTSchemaDefinition astSchemaDefinition = schemaDefinition;
-        final ASTSchemaDefinition astSuperSchemaDefinition = astSchemaDefinition.getSuperSchemaDefinitions().stream().findFirst().orElse(null);
-        generatorEngine.generate(FreeMarkerTemplate.SCHEMA_CLASS.getTemplateName(), Paths.get(schemaDefinition.getName() + "_Schema_API.py"), astSchemaDefinition,
-                astSchemaDefinition, astSuperSchemaDefinition, astSchemaDefinition.getSchemaMemberList());
+        final ASTSchemaDefinition astSuperSchemaDefinition = schemaDefinition.getSuperSchemaDefinitions().stream().findFirst().orElse(null);
+        generatorEngine.generate(FreeMarkerTemplate.SCHEMA_CLASS.getTemplateName(), Paths.get(schemaDefinition.getName() + "_Schema_API.py"), schemaDefinition,
+                schemaDefinition, astSuperSchemaDefinition, schemaDefinition.getSchemaMemberList());
     }
 
-
-    protected List<SchemaDefinitionSymbol> resolveSchemas() {
-        List<SchemaDefinitionSymbol> schemas = new ArrayList<>();
-        schemas.add(resolveSchema("Supervised"));
-        return schemas;
-
-    }
-
-    protected SchemaDefinitionSymbol resolveSchema(final String schemaName) {
-        return SchemaUtil.resolveSchemaDefinition(schemaName, schemasModelPath);
-
-    }
-
-    protected List<ASTSchemaLangCompilationUnit> parseSchemas() {
-        List<ASTSchemaLangCompilationUnit> schemas = new ArrayList<>();
-        schemas.add(parseSchema("General.scm"));
-        schemas.add(parseSchema("Supervised.scm"));
-        return schemas;
-    }
-
-    private ASTSchemaLangCompilationUnit parseSchema(final String schemaName) {
-        SchemaLangParser schemaLangParser = new SchemaLangParser();
-        final Optional<ASTSchemaLangCompilationUnit> astSchemaLangCompilationUnit;
-        try {
-            astSchemaLangCompilationUnit = schemaLangParser.parse("src/main/resources/schemas/" + schemaName);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return astSchemaLangCompilationUnit.orElseThrow(IllegalStateException::new);
-    }
 }
