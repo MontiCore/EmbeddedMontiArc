95e55f0e8a3860febede1ea4d68255a774a51d47
EMA CI Robot <kusmenko@se-rwth.de>
2023-01-19 12:43:20 +0000

    Squashed 'generators/CNNArch2X/' changes from f7baa133..034e05be
    034e05be Merge branch 'dev' into 'master'
    fc5eb4b6 Update version for SchemaLang dependency
    e81c9a27 Increase maven version
    3c66117e Merge branch '3-schema2python-api' into dev
    43b0a3d4 Merge branch 'dev' into 4-python-training-configuration
    a593a96d Merge branch 'dev' into 3-schema2python-api
    008879ef Code clean up
    aa07f083 Add resolving of complex property definitions in imported and inherited schemas
    f30bc6ac Add maven execution plugin
    25128a72 Fix hook point linking
    9f9e602c Merge branch '4-python-training-configuration' into 'dev'
    aa57085c Merge branch 'dev' into 4-python-training-configuration
    2c9eafc6 Merge remote-tracking branch 'origin/4-python-training-configuration' into 4-python-training-configuration
    2fa753d4 Make generated configuration name configurable
    fadbc328 Merge branch '3-schema2python-api' into 'dev'
    02b13e7a Add test to validate template paths
    d2e348de Add test case for simplified supervised schema
    d5e7df2e Fix tool interface
    423a5834 Fix generated method names for all schema declarations
    1f4214d2 Fix test case for object declarations
    d4ccb7e8 Adapt templates to integrate handling of object declarations
    33552e74 Handle object types without inner classes
    35c75798 Add method to create getter method name
    2dea00e1 Add method to get complex property definition for object member
    caa10275 Make generated method names compatible with training configuration
    0da65060 Update target path
    a739d235 Merge branch 'dev' into 3-schema2python-api
    a60a023b Merge branch '4-python-training-configuration' into 'dev'
    42376d0c Add test case for LeNet network training configuration
    5e1245e6 Fix generation path
    67f93300 Add template hook points
    61c9879e Add template infrastructure
    230d5bb9 Add parameters to HyperparameterOpt with default values for SA, GA and PSO
    ef2c0584 scm File renamed to NeuralArchitectureSearch; add parameter max_episodes
    e16a1bb6 Add generator for ConfLang
    81d69150 Add testing resources
    af68df63 Add optimiser to supervised schema
    da38ab08 Add support for common hyperparameters in complex property definitions
    804580e9 Mvn Groovy plugin
    84bd258e Reference models tests
    b78a9bce Junit methods to use implemented functionality
    b8522be3 Make schema api configurable
    2fce6068 Correct calls on delegation object
    ac3101a2 Schema enums supported in api generation
    307749c0 Test schemas
    ceebccac New schema member types supported in generation: object type, complex property definitions
    a583a115 SchemaAPI: getters for primitive schema entries
    3766a675 Junit Maven dependency
    28f90c18 Freemarker templates for SchemaAPI
    1226df3c Utilities for schema parsing and schema types
    
    git-subtree-dir: generators/CNNArch2X
    git-subtree-split: 034e05be830681fd64fe773fd93c750ba741484e

--- src/main/resources/schemas/HyperparameterOpt.scm
+++ src/main/resources/schemas/HyperparameterOpt.scm
@@ -2,8 +2,30 @@
 
 schema HyperparameterOpt {
 
-    optimizer: enum {
-        SA, BO, WeightedRS, GA, PSO, Hyperband, BOHB, DEHB;
+    optimizer_algorithm {
+        values:
+            SA,
+            BO,
+            WeightedRS,
+            GA,
+            PSO,
+            Hyperband,
+            BOHB,
+            DEHB;
+
+            define SA {
+                initial_temperature = 50.0: Q
+            }
+
+            define GA {
+                crossover_rate = 0.8: Q
+                mutation_rate = 0.3: Q
+            }
+
+            define PSO {
+                c1 = 2.0: Q
+                c2 = 2.0: Q
+            }
     }
 
 }
