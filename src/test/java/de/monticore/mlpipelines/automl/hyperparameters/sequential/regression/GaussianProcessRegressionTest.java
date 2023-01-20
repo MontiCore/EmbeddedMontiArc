3ac04ce79ca40d80ee8c38c17d45e3843d7584a1
Hiroshi Hamano <hiroshihamano1996@gmail.com>
2023-01-20 16:48:42 +0100

    Add unittests for GaussianProcessRegression

--- src/test/java/de/monticore/mlpipelines/automl/hyperparameters/sequential/regression/GaussianProcessRegressionTest.java
+++ src/test/java/de/monticore/mlpipelines/automl/hyperparameters/sequential/regression/GaussianProcessRegressionTest.java
@@ -7,44 +7,50 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.Assert.assertArrayEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GaussianProcessRegressionTest extends TestCase {
 
+    private double[][] xTrain;
+    private double[] yTrain;
+
     private double[] func(double[][] x) {
         double[] y = new double[x.length];
         for (int i=0; i < x.length; i++) {
-            y[i] = Math.pow(x[i][0], 2);
+            y[i] = Math.pow(x[i][0], 2) + Math.pow(x[i][1], 3);
         }
         return y;
     }
 
     private double[][] getXTrain() {
-        double[][] xTrain = new double[10][1];
+        double[][] xTrain = new double[10][2];
         for (int i=0; i < xTrain.length; i++) {
             xTrain[i][0] = i - 5;
+            xTrain[i][1] = i - 5;
         }
         return xTrain;
     }
 
     @Before
     public void setup() throws IOException {
-        double[][] xTrain = this.getXTrain();
-        double[] yTrain = this.func(xTrain);
-
-        GaussianProcessRegression gpr = new GaussianProcessRegression();
-        gpr.fit(xTrain, yTrain);
-
-        double[][] xTest = {
-                {2.5}
-        };
-        gpr.predict(xTest);
-
-        System.out.println("test");
+        xTrain = this.getXTrain();
+        yTrain = this.func(xTrain);
     }
 
     @Test
-    public void testGetInitialNumEpoch() {
-        System.out.println("test");
+    public void testFitPredict() {
+        GaussianProcessRegression gpr = new GaussianProcessRegression(1);
+        gpr.fit(xTrain, yTrain);
+
+        double[][] xTest = {
+                {2.5, 2.5}
+        };
+        Map<String, Object> prediction = gpr.predict(xTest);
+
+        assertArrayEquals((double[]) prediction.get("mean"), new double[]{14.959492721793671}, 0.0);
+        assertArrayEquals(((double[][]) prediction.get("cov"))[0], new double[]{0.48721873563147955}, 0.0);
     }
 }
