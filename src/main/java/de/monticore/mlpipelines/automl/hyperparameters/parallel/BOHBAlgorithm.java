9b2b147b387eac8476b86930afcdbb3ab1b3aa2d
Tobias Hörnschemeyer <tobias.hoernschemeyer@rwth-aachen.de>
2023-01-23 11:45:49 +0100

    Readd missing files

---
+++ src/main/java/de/monticore/mlpipelines/automl/hyperparameters/parallel/BOHBAlgorithm.java
@@ -0,0 +1,24 @@
+package de.monticore.mlpipelines.automl.hyperparameters.parallel;
+
+public class BOHBAlgorithm extends ParallelAlgorithm {
+    private double R;
+    private double eta;
+    private double n;
+    private double b;
+
+    public void bo_sample() {
+        // TODO: Implement method
+    }
+
+    public void successive_halving() {
+        // TODO: Implement method
+    }
+
+    public void validation_loss() {
+        // TODO: Implement method
+    }
+
+    public void top_configurations() {
+        // TODO: Implement method
+    }
+}
---
+++ src/main/java/de/monticore/mlpipelines/automl/hyperparameters/sequential/WeightedRS.java
@@ -0,0 +1,13 @@
+package de.monticore.mlpipelines.automl.hyperparameters.sequential;
+
+import java.util.List;
+
+public class WeightedRS extends SequentialAlgorithm {
+
+    private List weightsList;
+
+    public List determineWeights() {
+        List determinedWeights = null;
+        return determinedWeights;
+    }
+}
