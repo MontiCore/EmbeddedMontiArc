00a02486ac214aea82f02a09eae33fd3f041ce6c
Martin Fitzke <martin.fitzke@rwth-aachen.de>
2023-01-27 04:25:09 +0100

    update

---
+++ src/test/java/de/monticore/lang/monticar/emadl/modularity/LetterTest.java
@@ -0,0 +1,19 @@
+package de.monticore.lang.monticar.emadl.modularity;
+
+import de.se_rwth.commons.logging.Log;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class LetterTest extends ModularTestSetup {
+
+    String[] hashPaths = {"target/letterPredictor"};
+
+    @Test
+    public void testLetterPredictor() throws IOException {
+        Log.getFindings().clear();
+        removeDirectory("target/modularNetworkSimpleMultiNet");
+        String[] args = {"-m", "src/test/resources/models/ModularCNN/letterPredictor", "-r", "letterpred.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-ad", "-dn", "Network,Net1,Net2"};
+        runGenerator(args, hashPaths,14,false);
+    }
+}
