b3c0ebf968ebe811782028ad1daf800ec4cb956b
Akash Kumar <Akumar@clinomic.ai>
2023-01-30 11:15:34 +0100

    Hyperband algo

--- src/main/java/de/monticore/mlpipelines/automl/hyperparameters/sequential/HyperbandAlgorithm.java
+++ src/main/java/de/monticore/mlpipelines/automl/hyperparameters/sequential/HyperbandAlgorithm.java
@@ -1,13 +1,13 @@
 package de.monticore.mlpipelines.automl.hyperparameters.sequential;
 
 import conflang._ast.ASTConfLangCompilationUnit;
-import conflang._symboltable.ConfigurationScope;
+import de.monticore.mlpipelines.automl.helper.ASTConfLangCompilationUnitHandler;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.lang.Math.log;
+
 
 public class HyperbandAlgorithm extends SequentialAlgorithm {
 
@@ -17,7 +17,8 @@ public class HyperbandAlgorithm extends SequentialAlgorithm {
     // private double
     private int s_max;
     private double B ;
-    private double results[];
+
+    ArrayList<Map<String, Object>> results = new ArrayList<>();
     private int counter;
     private double best_loss;
     private double best_counter ;
@@ -36,67 +37,148 @@ public class HyperbandAlgorithm extends SequentialAlgorithm {
         counter = 0;
         best_loss = Double.POSITIVE_INFINITY;
         best_counter = -1;
-    }
-
-    @Override
-    public ASTConfLangCompilationUnit getNewHyperparamsCandidate(ASTConfLangCompilationUnit searchSpace) {
-        return null;
-    }
-
-    public double[] run(int skip_last, boolean dry_run){
 
         for (int s = this.s_max + 1; s >0; s--) {
             // initial number of configurations
             int n = (int) Math.ceil( this.B / this.max_iter / ( s + 1 ) * Math.pow(this.eta,s ));
             //initial number of iterations per config
             int r = (int) (this.max_iter * Math.pow(this.eta,( -s )));
-            // n random configurations
-            //configurations T  = [ self.get_params() for i in range( n )]
 
-            //call searchspace for random config
+            Collection<ASTConfLangCompilationUnit> nConfigurations = getFullSetOfNewHyperparamsCandidate(searchSpace, n);
+            Double valLoss;
+            Map<ASTConfLangCompilationUnit, Double> map = new HashMap<>();
+            Map<String,Object> result = new HashMap<>();
 
-            //call successive halving
+            for (int i = 0; i<s; i++) {
+                int n_configs = (int) (n * Math.pow(this.eta, ( -i )));
+                int n_iterations = (int) (r * Math.pow(this.eta, i));
 
+                Iterator<ASTConfLangCompilationUnit> iterator = nConfigurations.iterator();
+                while (iterator.hasNext()) {
+                    this.counter=+1;
+                    ASTConfLangCompilationUnit element = iterator.next();
+                    valLoss = validation_loss(element,n_iterations);
+                    if (valLoss < this.best_loss){
+                        this.best_loss = valLoss;
+                        this.best_counter = this.counter;
+                    }
+                    result.put("counter",this.counter);
+                    result.put("params",element);
+                    result.put("interations",n_iterations);
+                    result.put("loss",valLoss);
+                    map.put(element,valLoss);
+                    this.results.add( result);
+                }
+
+                nConfigurations = top_configurations(map,n_configs,eta);
+            }
+           
         }
-        return this.results;
+
+        this.currBestHyperparams = bestPerformingConfiguration(results);
+    }
+
+
+    private ASTConfLangCompilationUnit bestPerformingConfiguration(ArrayList<Map<String, Object>> results) {
+
+        Collections.sort(results, Comparator.comparing(map -> (Comparable) map.get("loss")));
+        Map<String, Object> firstMap = results.get(0);
+
+        // Get the value for the specific key in the first HashMap
+        currBestHyperparams = (ASTConfLangCompilationUnit) firstMap.get("params");
+        return currBestHyperparams;
+    }
+
+
+    private double validation_loss(ASTConfLangCompilationUnit element, int nIterations) {
+        double loss = 0.0;
+        //dry run the training pipeline
+        return loss;
+    }
+
+    public Set<ASTConfLangCompilationUnit> top_configurations(Map<ASTConfLangCompilationUnit, Double> map, int n_configs, double eta) {
+        Map<ASTConfLangCompilationUnit, Double> sortedMap = map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        int v = (int) (n_configs / eta);
+        Set<ASTConfLangCompilationUnit> nConfigurations = new HashSet<>();
+
+        nConfigurations= sortedMap.entrySet()
+                .stream()
+                .limit(v)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+
+        return nConfigurations ;
+    }
+
+
+    public Collection<ASTConfLangCompilationUnit> getFullSetOfNewHyperparamsCandidate(ASTConfLangCompilationUnit searchSpace, int n) {
+        Set<ASTConfLangCompilationUnit> nConfigurations = new HashSet<>();
+        ASTConfLangCompilationUnit currentHyperparams;
+        for(int i=0;i<n;i++){
+            currentHyperparams= getNewHyperparamsCandidate(searchSpace);
+            nConfigurations.add(currentHyperparams);
+        }
+        return nConfigurations;
+    }
+    @Override
+    public ASTConfLangCompilationUnit getNewHyperparamsCandidate(ASTConfLangCompilationUnit searchSpace) {
+        ASTConfLangCompilationUnit currentHyperparams = this.getCurrentHyperparameters().deepClone();
+        Map<String, Boolean> params = ASTConfLangCompilationUnitHandler.getAllKeys(searchSpace);
+
+        for (Map.Entry<String, Boolean> entry : params.entrySet()) {
+            String key = entry.getKey();
+            Boolean isNested = entry.getValue();
+
+            if (isNested) {
+                currentHyperparams = this.updateNestedHyperparamsValue(searchSpace, currentHyperparams, key);
+            } else {
+                currentHyperparams = this.updateHyperparamsValue(searchSpace, currentHyperparams, key);
+            }
+        }
+
+        return currentHyperparams;
+    }
+
+    private ASTConfLangCompilationUnit updateHyperparamsValue(ASTConfLangCompilationUnit searchSpace, ASTConfLangCompilationUnit currentHyperparams, String key) {
+        Object searchSpaceValue = ASTConfLangCompilationUnitHandler.getValueByKey(searchSpace, key);
+        if (searchSpaceValue instanceof Map) {
+            Map<String, Object> valueMap = (Map<String, Object>) searchSpaceValue;
+            Object currentValue = ASTConfLangCompilationUnitHandler.getValueByKey(currentHyperparams, key);
+            Object newValue;
+            newValue = this.createValueFromRange(valueMap);
+            currentHyperparams = ASTConfLangCompilationUnitHandler.setValueForKey(currentHyperparams, key, newValue);
+        }
+
+        return currentHyperparams;
+    }
+
+    private ASTConfLangCompilationUnit updateNestedHyperparamsValue(ASTConfLangCompilationUnit searchSpace, ASTConfLangCompilationUnit currentHyperparams, String key) {
+        Map<String, Object> configMap = ASTConfLangCompilationUnitHandler.getValuesFromNestedConfiguration(searchSpace, key);
+        Map<String, Object> nestedMap = (Map<String, Object>) configMap.get("nestedMap");
+        for (Map.Entry<String, Object> nestedEntry : nestedMap.entrySet()) {
+            String nestedKey = nestedEntry.getKey();
+            Object nestedValue = nestedEntry.getValue();
+            if (nestedValue instanceof Map) {
+                Map<String, Object> currentValueMap = ASTConfLangCompilationUnitHandler.getValuesFromNestedConfiguration(currentHyperparams, key);
+                Map<String, Object> currentNestedMap = (Map<String, Object>) currentValueMap.get("nestedMap");
+                Object currentValue = currentNestedMap.get(nestedKey);
+                Object newValue;
+                Map<String, Object> nestedValueMap = (Map<String, Object>) nestedValue;
+                newValue = this.createValueFromRange(nestedValueMap);
+                currentHyperparams = ASTConfLangCompilationUnitHandler.setNestedValueForKeys(currentHyperparams, key, nestedKey, newValue);
+            }
+        }
+
+        return currentHyperparams;
     }
 
     private double logeta(double x, double eta) {
         return log(eta) / log(x);
-    }
-
-
-    public void random_sample(int n,ConfigurationScope hyperconfopt) {
-        Random r = new Random();
-
-
-        // TODO: Implement method
-    }
-
-    public void successive_halving(ConfigurationScope T, int s, int n, int r) {
-        for(int i=0;i<s;i++){
-            int n_configs = (int) (n * Math.pow(this.eta, -i));
-            int n_iterations = (int) (r * Math.pow(this.eta, i));
-
-
-
-
-        }
-        // TODO: Implement method
-    }
-    public void validation_loss() {
-        // TODO: Implement method
-    }
-
-    public void top_configurations() {
-        // TODO: Implement method
-    }
-
-
-
-    public void setval_loss(double validation_loss) {
-
-        this.val_loss.add(validation_loss);
     }
 
 }
