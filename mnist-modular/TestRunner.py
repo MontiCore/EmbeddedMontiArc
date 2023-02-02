1a0035d00312f5144034336a4d5e5fc17befe7f3
Martin Fitzke <martin.fitzke@rwth-aachen.de>
2023-02-02 11:56:04 +0100

    push

--- mnist-modular/TestRunner.py
+++ mnist-modular/TestRunner.py
@@ -15,7 +15,7 @@ def runner():
 
 
     if len(sys.argv) == 5:
-        limit = sys.argv[4]
+        limit = int(sys.argv[4])
 
     print("Limit is", str(limit))
 
@@ -33,13 +33,33 @@ def runner():
 
     if commands is not None and len(commands) > 0:
         k = 0
-        #for c, e in commands, expected:
+        posCounter = 0
+        negCounter = 0
+
         while k < len(commands) and k < limit:
             commands[k].insert(0, execPath)
-            print("Command: ", commands[k], " | Expected: ", expected[k])
-            #process = subprocess.run(c, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True, shell=True)
-        #   output, error = process.communicate()
+            print("Command: ", str(commands[k]), " | Expected: ", str(expected[k]) + "\n")
+            process = subprocess.Popen(["ls", "-al"], text=True,  stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
+            if len(output) > 0:
+                print("Output:", str(output))
+
+            if len(error) > 0:
+                print("Error:", str(error))
+
+            lines = str(output).split("\n")
+            lastLine = lines[len(lines)-1]
+            secondLastLine = lines[len(lines)-2]
+
+            print("Last:", lastLine)
+            print("SecondLast", secondLastLine)
+
+            if scenario == "digits":
+                pass
+
             k += 1
+
 
     else:
         print("Commands are None. Error happened. Aborting.")
@@ -53,20 +73,43 @@ def digitScenario(picture_path, limit):
         number_dirs.append(picture_path + "/" + str(k) + "/")
         k += 1
 
-    numbers = []
-    k = 0
-    while k < 6:
-        numbers.append(random.randint(0, 9))
-        k += 1
+    commands = []
+    expected = []
 
-    print("Numbers:", str(numbers))
-    result = (numbers[2] + numbers[1] * 10 + numbers[0] * 100) + (numbers[5] + numbers[4] * 10 + numbers[3] * 100)
-    print("Result: ", str(result))
-    numbers.append(numbers)
-    print(str(result))
-    print(str(number_dirs))
+    i = 0
+    while i < limit:
+        numbers = []
+        k = 0
+        while k < 6:
+            numbers.append(random.randint(0, 9))
+            k += 1
+
+        com = []
+
+        for n in numbers:
+            numPath = picture_path + "/" + str(n) + "/"
+            com.append(getRandomPictureForDigit(numPath))
+
+        result = (numbers[2] + numbers[1] * 10 + numbers[0] * 100) + (numbers[5] + numbers[4] * 10 + numbers[3] * 100)
+        numbers.append(result)
+
+        expected.append(numbers)
+        commands.append(com)
+
+        #print("Com:", com, " | Exp:", expected)
+
+        i += 1
 
 
+    return commands, expected
+
+
+def getRandomPictureForDigit(path):
+    allPics = os.listdir(path)
+    randPicIndex = random.randint(0, len(allPics)-1)
+    picName = allPics[randPicIndex]
+    fullPath = path + picName
+    return fullPath
 
 def letterScenario(picture_path, limit):
     commands = []
