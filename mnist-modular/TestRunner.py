c80b3e6dba0bcb44a53ffb8f8ce419dc68e9a353
Martin Fitzke <martin.fitzke@rwth-aachen.de>
2023-02-02 13:50:30 +0100

    updated script

--- mnist-modular/TestRunner.py
+++ mnist-modular/TestRunner.py
@@ -4,42 +4,75 @@ import sys, os
 
 
 def runner():
-    if len(sys.argv) <= 4:
-        print(str("No enough args. Example: \"python {} <execPath> <picturePath> <letters/digits> <limit?>\". Aborting.").format( sys.argv[0]))
-        exit(1)
 
-    execPath = sys.argv[1]
-    scenario = sys.argv[3]
-    picture_set_path = sys.argv[2]
+    execPath = None
+    scenario = None
+    picture_set_path = None
+    staticPath = None
+    staticMode = False
     limit = 100
+
+
+    if len(sys.argv) == 3:
+        print("Running static mode")
+        staticMode = True
+        staticPath = sys.argv[1]
+        scenario = sys.argv[2]
+    elif len(sys.argv) <= 2:
+        print(
+            str("Not enough args. Example: \"python {} <execPath> <picturePath> <letters/digits> <limit?>\". Aborting.").format(
+                sys.argv[0]))
+        exit(1)
+    elif len(sys.argv) >= 4:
+        execPath = sys.argv[1]
+        scenario = sys.argv[3]
+        picture_set_path = sys.argv[2]
+
+
+
 
 
     if len(sys.argv) == 5:
         limit = int(sys.argv[4])
 
-    print("Limit is", str(limit))
+
 
     if scenario != "digits" and scenario != "letters":
-        print("Test scenario not digits or letters. Aborting.")
+        print("Test scenario not digits or letters. Aborting. ->", scenario)
         exit(1)
 
     commands = []
     expected = []
 
-    if scenario == "digits":
+    if staticMode:
+        commands, expected = readListFromFile(staticPath)
+        limit = len(commands)
+    elif scenario == "digits":
         commands, expected = digitScenario(picture_set_path, limit)
     elif scenario == "letters":
         commands, expected = letterScenario(picture_set_path, limit)
+
+    print("Limit is", str(limit))
+
+    if not staticMode:
+        for c in commands:
+            c.insert(0, execPath)
+
+        if len(commands) > 0 and len(expected) > 0 :
+            writeCommandsToFile(commands, expected, scenario+"-static-run.txt")
 
     if commands is not None and len(commands) > 0:
         k = 0
         posCounter = 0
         negCounter = 0
 
+        falseStorage = []
+        results = []
+
         while k < len(commands) and k < limit:
-            commands[k].insert(0, execPath)
-            print("Command: ", str(commands[k]), " | Expected: ", str(expected[k]) + "\n")
-            process = subprocess.Popen(["ls", "-al"], text=True,  stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+            #print("Command: ", str(commands[k]), " | Expected: ", str(expected[k]) + "\n")
+            process = subprocess.Popen(commands[k], text=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
             output, error = process.communicate()
 
             if len(output) > 0:
@@ -49,28 +82,47 @@ def runner():
                 print("Error:", str(error))
 
             lines = str(output).split("\n")
-            lastLine = lines[len(lines)-1]
-            secondLastLine = lines[len(lines)-2]
+            lastLine = lines[len(lines) - 2]
+            # secondLastLine = lines[len(lines)-2]
 
-            print("Last:", lastLine)
-            print("SecondLast", secondLastLine)
+            # print("Last:", lastLine)
+            # print("SecondLast", secondLastLine)
 
-            if scenario == "digits":
-                resNumStr = lastLine.split(" ")[1]
+
+            try:
+                resNumStr = lastLine.replace(" ", "").split(":")[1]
                 resNumStr = resNumStr.replace("\"", "")
                 resNum = int(resNumStr)
+                results.append(resNum)
 
-                if resNum == expected[k][6]:
-                    posCounter += 1
-                else:
-                    negCounter += 1
+                if scenario == "digits":
+                    if resNum == expected[k][6]:
+                        posCounter += 1
+                    else:
+                        negCounter += 1
+                        expected[k].append(resNum)
+                        falseStorage.append(expected[k])
+            except:
+                print("result parsing exception")
 
             k += 1
 
         if scenario == "digits":
             print("Correct Results: ", str(posCounter), " | Wrong Results: ", str(negCounter))
+            if negCounter > 0:
+                print("Wrong results:\n")
+                for f in falseStorage:
+                    print(str(f))
 
-        print(scenario, " scenario done")
+                print("Correct Results: ", str(posCounter), " | Wrong Results: ", str(negCounter))
+
+        print("Results:")
+        for r in results:
+            print(r)
+
+        print("Correct Results: ", str(posCounter), " | Wrong Results: ", str(negCounter))
+        print(scenario, "scenario done. Writing results")
+        writeResultsToFile(scenario+"-results.txt", results)
 
     else:
         print("Commands are None. Error happened. Aborting.")
@@ -107,20 +159,20 @@ def digitScenario(picture_path, limit):
         expected.append(numbers)
         commands.append(com)
 
-        #print("Com:", com, " | Exp:", expected)
+        # print("Com:", com, " | Exp:", expected)
 
         i += 1
-
 
     return commands, expected
 
 
 def getRandomPictureForDigit(path):
     allPics = os.listdir(path)
-    randPicIndex = random.randint(0, len(allPics)-1)
+    randPicIndex = random.randint(0, len(allPics) - 1)
     picName = allPics[randPicIndex]
     fullPath = path + picName
     return fullPath
+
 
 def letterScenario(picture_path, limit):
     commands = []
@@ -137,5 +189,66 @@ def letterScenario(picture_path, limit):
 
     return commands, expect
 
+def writeCommandsToFile(commands, expected, path):
+    clearFile(path)
+
+    f = open(path, "w")
+    for c in commands:
+        f.write(str(c) + "\n")
+
+    f.write("-|-|-\n")
+
+    for e in expected:
+        f.write(str(e) + "\n")
+
+    f.close()
+
+def readListFromFile(path):
+    commands = []
+    expected = []
+
+    cDone = False
+
+    f = open(path, "r")
+    for line in f:
+        line = line[:-1]
+        print(line)
+        if line == "-|-|-":
+            cDone = True
+            continue
+
+        comArr = line.replace("[", "").replace("]", "").replace(" ", "").replace("'", "").split(",")
+
+        if cDone:
+            expected.append(comArr)
+        else:
+            commands.append(comArr)
+
+    f.close()
+
+
+
+    if len(commands) == 0 or len(expected) == 0:
+        print("Commands:", str(len(commands)), " | Expected: ", str(len(expected)))
+        print("No commands or expected results loaded from file. Aborting")
+        exit(1)
+
+    return commands, expected
+
+def writeResultsToFile(path,results):
+    clearFile(path)
+
+    f = open(path, "w")
+    f.write("Corr")
+    for c in results:
+        f.write(str(c) + "\n")
+
+    f.close()
+
+def clearFile(path):
+    try:
+        os.remove(path)
+    except:
+        print("could not remove:", path)
 
 runner()
