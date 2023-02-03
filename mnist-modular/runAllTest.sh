9df5200923d532029acf80ed0f574afee51fad01
Martin Fitzke <martin.fitzke@rwth-aachen.de>
2023-02-03 01:21:10 +0100

    3rd test run

--- mnist-modular/runAllTest.sh
+++ mnist-modular/runAllTest.sh
@@ -1,49 +1,57 @@
 #!/bin/bash
 
+
+BUILD=""
+
+if [ "$1" == "b" ]; then
+    BUILD=./build.sh && 
+    echo "using small test runs"
+fi
+
 #### Digits
 
 # Mono
 
 cd gluon-cpp-modular-digits-mono-small
-./build.sh && ./executeStaticTest.sh d s
+$BUILD ./executeStaticTest.sh d s
 cd ..
 
 cd gluon-cpp-modular-digits-mono-default
-./build.sh && ./executeStaticTest.sh d s
+$BUILD ./executeStaticTest.sh d s
 cd ..
 
 cd gluon-cpp-modular-digits-mono-big
-./build.sh && ./executeStaticTest.sh d s
+$BUILD ./executeStaticTest.sh d s
 cd ..
 
 
 # Simple
 
 cd gluon-cpp-modular-digits-simple-small
-./build.sh && ./executeStaticTest.sh d s
+$BUILD ./executeStaticTest.sh d s
 cd ..
 
 cd gluon-cpp-modular-digits-simple-default
-./build.sh && ./executeStaticTest.sh d s
+$BUILD ./executeStaticTest.sh d s
 cd ..
 
 cd gluon-cpp-modular-digits-simple-big
-./build.sh && ./executeStaticTest.sh d s
+$BUILD ./executeStaticTest.sh d s
 cd ..
 
 
 # Complex
 
 cd gluon-cpp-modular-digits-complex-small
-./build.sh && ./executeStaticTest.sh d s
+$BUILD ./executeStaticTest.sh d s
 cd ..
 
 cd gluon-cpp-modular-digits-complex-default
-./build.sh && ./executeStaticTest.sh d s
+$BUILD ./executeStaticTest.sh d s
 cd ..
 
 cd gluon-cpp-modular-digits-complex-big
-./build.sh && ./executeStaticTest.sh d s
+$BUILD ./executeStaticTest.sh d s
 cd ..
 
 
@@ -51,14 +59,14 @@ cd ..
 #### Letters
 
 cd gluon-cpp-modular-letters-mono
-./build.sh && ./executeStaticTest.sh l s
+$BUILD ./executeStaticTest.sh l s
 cd ..
 
 cd gluon-cpp-modular-letters-simple
-./build.sh && ./executeStaticTest.sh l s
+$BUILD ./executeStaticTest.sh l s
 cd ..
 
 cd gluon-cpp-modular-letters-complex
-./build.sh && ./executeStaticTest.sh l s
+$BUILD ./executeStaticTest.sh l s
 cd ..
 
