7c3c77882a38a65665d492ed0becb4724b969193
Ahmed Diab <ahmed.diab@rwth-aachen.de>
2023-02-13 19:35:14 +0100

    update CI

--- turtlebot_real/run_check.sh
+++ turtlebot_real/run_check.sh
@@ -1,3 +1,9 @@
+. config.sh
+
+cd "${PROJECT_ROOT}/${BINARY_GAZEBO}"
+echo "Calculate the RMSE for the experiment..."
 python calc_rmse.py exp1
 
+
+echo "Generate a pdf file for the trajectory..."
 python plot_traj.py exp1
