b703bae834a20f4612a7cc0f9f8ade334d41b6c7
Martin Fitzke <martin.fitzke@rwth-aachen.de>
2023-02-15 19:05:35 +0100

    modular test change

--- src/test/java/de/monticore/lang/monticar/emadl/modularity/ModularBasicTest.java
+++ src/test/java/de/monticore/lang/monticar/emadl/modularity/ModularBasicTest.java
@@ -16,7 +16,7 @@ public class ModularBasicTest extends ModularTestSetup {
     @Test
     public void testEmtpyNetwork() throws IOException {
         Log.getFindings().clear();
-        removeDirectory("target/emptyNetwork");
+        //removeDirectory("target/emptyNetwork");
         String[] args = {"-m", "src/test/resources/models/ModularCNN/emptyNetwork", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-ad"};
         runGenerator(args, hashPaths,0,true);
 
@@ -25,7 +25,7 @@ public class ModularBasicTest extends ModularTestSetup {
     @Test
     public void testModularNetworkSimpleMultiNet() throws IOException {
         Log.getFindings().clear();
-        removeDirectory("target/modularNetworkSimpleMultiNet");
+        //removeDirectory("target/modularNetworkSimpleMultiNet");
         String[] args = {"-m", "src/test/resources/models/ModularCNN/modularNetworkSimpleMultiNet", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-ad", "-dn", "Network,Net2"};
         runGenerator(args, hashPaths,7,false);
     }
@@ -33,7 +33,7 @@ public class ModularBasicTest extends ModularTestSetup {
     @Test
     public void testModularNetworkComplex() throws IOException {
         Log.getFindings().clear();
-        removeDirectory("target/modularNetworkComplex");
+        //removeDirectory("target/modularNetworkComplex");
         String[] args = {"-m", "src/test/resources/models/ModularCNN/modularNetworkComplex", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-ad", "-dn", "Network,Net2"};
         runGenerator(args, hashPaths,6,false);
     }
@@ -41,7 +41,7 @@ public class ModularBasicTest extends ModularTestSetup {
     @Test
     public void testModularNetworkSimple() throws IOException {
         Log.getFindings().clear();
-        removeDirectory("target/modularNetworkSimple");
+        //removeDirectory("target/modularNetworkSimple");
         String[] args = {"-m", "src/test/resources/models/ModularCNN/modularNetworkSimple", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-ad", "-dn", "Network"};
         runGenerator(args, hashPaths,6,false);
     }
@@ -49,7 +49,7 @@ public class ModularBasicTest extends ModularTestSetup {
     @Test
     public void testSingleNetwork() throws IOException {
         Log.getFindings().clear();
-        removeDirectory("target/singleNetwork");
+        //removeDirectory("target/singleNetwork");
         String[] args = {"-m", "src/test/resources/models/ModularCNN/singleNetwork", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-ad"};
         runGenerator(args, hashPaths,6,false);
     }
