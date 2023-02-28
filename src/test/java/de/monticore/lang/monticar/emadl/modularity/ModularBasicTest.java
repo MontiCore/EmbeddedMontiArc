b838ba102bb510497f07649f5af1eae7246b2ab6
EMA CI Robot <kusmenko@se-rwth.de>
2023-02-28 09:50:13 +0000

    Squashed 'generators/EMADL2CPP/' changes from 6bfb7d74..bc84b345
    bc84b345 Merge branch 'ma_fitzke3' into 'master'
    d23bf02b changed model tagging to local dummy file
    d3bbb232 removed pom dep
    
    git-subtree-dir: generators/EMADL2CPP
    git-subtree-split: bc84b34503d54cffce9adab4bf8f7a033e16a10c

--- src/test/java/de/monticore/lang/monticar/emadl/modularity/ModularBasicTest.java
+++ src/test/java/de/monticore/lang/monticar/emadl/modularity/ModularBasicTest.java
@@ -17,34 +17,34 @@ public class ModularBasicTest extends ModularTestSetup {
     public void testEmtpyNetwork() throws IOException {
         Log.getFindings().clear();
         String[] args = {"-m", "src/test/resources/models/ModularCNN/emptyNetwork", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-f", "n"};
-        runGenerator(args, hashPaths,0,true);
+        runGenerator(args, hashPaths,6,true);
     }
 
     @Test
     public void testModularNetworkSimpleMultiNet() throws IOException {
         Log.getFindings().clear();
         String[] args = {"-m", "src/test/resources/models/ModularCNN/modularNetworkSimpleMultiNet", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-f", "n"};
-        runGenerator(args, hashPaths,0,false);
+        runGenerator(args, hashPaths,7,false);
     }
 
     @Test
     public void testModularNetworkComplex() throws IOException {
         Log.getFindings().clear();
         String[] args = {"-m", "src/test/resources/models/ModularCNN/modularNetworkComplex", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-f", "n"};
-        runGenerator(args, hashPaths,0,false);
+        runGenerator(args, hashPaths,6,false);
     }
 
     @Test
     public void testModularNetworkSimple() throws IOException {
         Log.getFindings().clear();
         String[] args = {"-m", "src/test/resources/models/ModularCNN/modularNetworkSimple", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-f", "n"};
-        runGenerator(args, hashPaths,0,false);
+        runGenerator(args, hashPaths,6,false);
     }
 
     @Test
     public void testSingleNetwork() throws IOException {
         Log.getFindings().clear();
         String[] args = {"-m", "src/test/resources/models/ModularCNN/singleNetwork", "-r", "calculator.Connector", "-o", "target", "-b", "GLUON", "-c", "y", "-f", "n"};
-        runGenerator(args, hashPaths,0,false);
+        runGenerator(args, hashPaths,6,false);
     }
 }
