c0f33b3a575a505549c9dc5eeca8c2e85473d071
Martin Fitzke <martin.fitzke@rwth-aachen.de>
2023-03-01 23:41:29 +0100

    decomp test

--- mnist-modular/runLocal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf runLog.txt
-java -jar embedded-montiarc-emadl-generator-0.5.6-SNAPSHOT-jar-with-dependencies.jar -m /home/martin/Development/Masterarbeit/mnistcalculator/mnist-calculator/src/main/ema/emadl/models -r cNNCalculator.Connector -o mnist-calculator/target -b gluon >> runLog.txt
