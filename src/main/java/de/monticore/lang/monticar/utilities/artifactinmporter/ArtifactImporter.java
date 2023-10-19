94a8f999ad87f52e0c60377ceb99b0ca23b231cc
antonio <antonio@getnavis.com>
2023-10-19 22:57:23 +0200

    add test pipeline stage

--- src/main/java/de/monticore/lang/monticar/utilities/artifactinmporter/ArtifactImporter.java
+++ src/main/java/de/monticore/lang/monticar/utilities/artifactinmporter/ArtifactImporter.java
@@ -12,8 +12,16 @@ public class ArtifactImporter {
   public static void importArtifact(Dependency dependency, File targetPath) throws MavenInvocationException {
     Properties properties = new Properties();
     properties.setProperty("outputDirectory", targetPath.getAbsolutePath());
-    properties.setProperty("artifact",String.format("%s:%s:%s:jar:%s",
-        dependency.getGroupId(), dependency.getArtifactId(), dependency.getVersion(), dependency.getClassifier()));
+
+    // config-check doesn't require classifier
+    if (dependency.getGroupId().equals("config-check")) {
+      System.out.println("[importArtifact] config-check");
+      properties.setProperty("artifact",String.format("%s:%s:%s:jar",
+              dependency.getGroupId(), dependency.getArtifactId(), dependency.getVersion()));
+    } else {
+      properties.setProperty("artifact",String.format("%s:%s:%s:jar:%s",
+              dependency.getGroupId(), dependency.getArtifactId(), dependency.getVersion(), dependency.getClassifier()));
+    }
 
     InvocationRequest request = new DefaultInvocationRequest().setGoals(Collections.singletonList("dependency:unpack"));
     request.setProperties(properties);
