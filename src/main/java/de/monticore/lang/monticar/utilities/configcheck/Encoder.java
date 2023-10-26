235e709598e0ce9b957d1d27d6c0ff6489a1a726
antonio <antonio@getnavis.com>
2023-10-26 19:12:02 +0200

    fix encoder

--- src/main/java/de/monticore/lang/monticar/utilities/configcheck/Encoder.java
+++ src/main/java/de/monticore/lang/monticar/utilities/configcheck/Encoder.java
@@ -1,15 +1,22 @@
 package de.monticore.lang.monticar.utilities.configcheck;
 
-
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class Encoder {
     public static String encode(String input) {
-        return Base64.getEncoder().encodeToString(input.getBytes());
+        byte[] encodedBytes = Base64.getEncoder().encode(input.getBytes());
+        return new String(encodedBytes, StandardCharsets.UTF_8).replaceAll("[^a-zA-Z0-9]", "");
     }
 
-    public static String decode(String encoded) {
-        byte[] decodedBytes = Base64.getDecoder().decode(encoded);
-        return new String(decodedBytes);
+    public static String decode(String encodedString) {
+        // Add padding if necessary to ensure it's a valid Base64 string
+        int padding = (4 - (encodedString.length() % 4)) % 4;
+        StringBuilder paddedEncodedString = new StringBuilder(encodedString);
+        for (int i = 0; i < padding; i++) {
+            paddedEncodedString.append("=");
+        }
+        byte[] originalBytes = Base64.getDecoder().decode(paddedEncodedString.toString());
+        return new String(originalBytes, StandardCharsets.UTF_8);
     }
 }
