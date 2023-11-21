99d85009fdee384006c080533f1afd28c6319ed1
Jonas Djurevci <jonas.djurevci@protonmail.com>
2023-11-21 15:41:22 +0100

    Enable support for cli arguments

--- src/test/java/de/monticore/montipipes/generators/PipelineGeneratorTest.java
+++ src/test/java/de/monticore/montipipes/generators/PipelineGeneratorTest.java
@@ -32,11 +32,12 @@ class PipelineGeneratorTest {
         new ExecutionSemantics(symTab, pipelineExecutionSemantic).addExecutionSemantics();
         final List<ASTConfigurationEntry> pipelineConfigurationEntryList = pipelineConfiguration.getConfiguration().getConfigurationEntryList();
         final List<String> importStatements = Lists.newArrayList("Supervised_Schema_API", "Training_Configuration_LeNetNet", "CNNCreator_LeNet");
+        final List<String> cliArguments = Lists.newArrayList("tracking_backends", "mlflow_run_id", "mlflow_tracking_uri");
         final PipelineGenerator pipelineGenerator = new PipelineGenerator();
         pipelineGenerator.setSchemaAPIName("Supervised_Schema_API");
         pipelineGenerator.setTrainingConfigurationName("Training_Configuration_LeNetNet");
         pipelineGenerator.addScriptConfiguration(ExecutionScriptConfiguration.MODEL_DIRECTORY, "./model/mnist/");
-        pipelineGenerator.generatePipelineExecutor(pipelineConfigurationEntryList, pipelineExecutionSemantic, importStatements);
+        pipelineGenerator.generatePipelineExecutor(pipelineConfigurationEntryList, pipelineExecutionSemantic, importStatements, cliArguments);
         final String expected = String.join("\n", Files.readAllLines(Paths.get("src/test/resources/pipeline_scripts/Pipeline_Executor.py")));
         final String actual = String.join("\n", Files.readAllLines(Paths.get(pipelineGenerator.outputPath.toString(), "Pipeline_Executor.py")));
         assertEquals(expected, actual);
