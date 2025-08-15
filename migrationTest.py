6238fa6fee25da894d6e65bee151dd7e74e4f342
David Blum <david.blum@rwth-aachen.de>
2025-08-15 10:59:39 +0200

    Create empty branch

--- migrationTest.py
+++
@@ -1,5 +0,0 @@
-from pipelineMigration import GitlabToGithub
-
-gitlabFilePath = ".gitlab-ci.yml"
-githubFilePath = ".main.yml"
-GitlabToGithub(gitlabFilePath, githubFilePath, "pipeline", ["GitlabToken", "URL", "ID"])
